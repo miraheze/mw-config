2543fa16d35c78f64d0176ca06515308aee3fc95
John F. Lewis <johnflewis93@gmail.com>
2015-07-25 16:49:40 +0100

    Session cache to use Redis

--- RedisConfig.php
+++ RedisConfig.php
@@ -1,12 +1,12 @@
 <?php
-/* $wgObjectCaches['redis'] = array(
+$wgObjectCaches['redis'] = array(
     'class'             => 'RedisBagOStuff',
     'servers'           => array( '185.52.1.76:6379' ),
     'password'          => $wmgRedisPassword,
-); */
+);
 
 $wgMainCacheType = CACHE_DB;
-// $wgSessionCacheType = 'redis';
+$wgSessionCacheType = 'redis';
 
 # Disabled because they make the wiki not faster, but slower --SPF
 // $wgMessageCacheType = 'redis';
