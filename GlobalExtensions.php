37917632cfe26577af3dc191b6def7eafd05973e
Southparkfan <Southparkfan@users.noreply.github.com>
2015-07-26 01:29:07 +0200

    Enable CreateWiki extension

--- GlobalExtensions.php
+++ GlobalExtensions.php
@@ -1,6 +1,7 @@
 <?php
 require_once( "$IP/extensions/AbuseFilter/AbuseFilter.php" );
 require_once( "$IP/extensions/CentralAuth/CentralAuth.php" );
+require_once( "$IP/extensions/CreateWiki/CreateWiki.php" );
 wfLoadExtension( 'CheckUser' );
 wfLoadExtension( 'TitleBlacklist' );
 wfLoadExtension( 'ParserFunctions' );
