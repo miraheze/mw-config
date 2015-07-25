3a95672666e4090ade955760768fa2e9cf784a43
Southparkfan <southparkfan223@hotmail.com>
2015-07-25 14:44:30 +0200

    Fall back to DB for caching
    Redis is extremely slow.

--- RedisConfig.php
+++ RedisConfig.php
@@ -1,14 +1,14 @@
 <?php
-$wgObjectCaches['redis'] = array(
+/* $wgObjectCaches['redis'] = array(
     'class'             => 'RedisBagOStuff',
     'servers'           => array( '185.52.1.76:6379' ),
     'password'          => $wmgRedisPassword,
-);
+); */
 
-$wgMainCacheType = 'redis';
-$wgSessionCacheType = 'redis';
+$wgMainCacheType = CACHE_DB;
+// $wgSessionCacheType = 'redis';
 
 # Disabled because they make the wiki not faster, but slower --SPF
 // $wgMessageCacheType = 'redis';
-$wgParserCacheType = 'redis';
+// $wgParserCacheType = 'redis';
 // $wgLanguageConverterCacheType = 'redis';
