fbb8cfceeafe24e78fd73765d8061cc9f0a3978e
Reception123 <motoc.luca@gmail.com>
2015-07-28 12:50:46 +0200

    move to wfLoadExtensions

--- GlobalExtensions.php
+++ GlobalExtensions.php
@@ -2,8 +2,8 @@
 require_once( "$IP/extensions/AbuseFilter/AbuseFilter.php" );
 require_once( "$IP/extensions/CentralAuth/CentralAuth.php" );
 require_once( "$IP/extensions/CreateWiki/CreateWiki.php" );
-require_once( "$IP/extensions/Gadgets/Gadgets.php" );
 wfLoadExtension( 'CheckUser' );
+wfLoadExtension( 'Gadgets' );
 wfLoadExtension( 'MassMessage' );
 wfLoadExtension( 'Nuke' );
 wfLoadExtension( 'TitleBlacklist' );
