53bbb346f6152fedb3f7d1bc0de3bde053ff55f5
John F. Lewis <johnflewis93@gmail.com>
2015-07-28 17:30:04 +0100

    load url shortener extension

--- GlobalExtensions.php
+++ GlobalExtensions.php
@@ -4,6 +4,7 @@ require_once( "$IP/extensions/AntiSpoof/AntiSpoof.php" );
 require_once( "$IP/extensions/CentralAuth/CentralAuth.php" );
 require_once( "$IP/extensions/CreateWiki/CreateWiki.php" );
 require_once( "$IP/extensions/GlobalBlocking/GlobalBlocking.php" );
+require_once( "$IP/extensions/UrlShortener/UrlShortener.php" );
 wfLoadExtension( 'CheckUser' );
 wfLoadExtension( 'Cite' );
 wfLoadExtension( 'Gadgets' );
