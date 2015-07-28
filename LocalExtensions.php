2e79d63f34f615846a18457eed322bc9bbb1ef27
Reception123 <motoc.luca@gmail.com>
2015-07-28 12:07:42 +0200

    ()

--- LocalExtensions.php
+++ LocalExtensions.php
@@ -2,8 +2,8 @@
 // Switch case
 switch( $wgDBname ) {
 	case 'metawiki':
-                require_once "$IP/extensions/Echo/Echo.php";
-                require_once "$IP/extensions/Thanks/Thanks.php";
+                require_once( "$IP/extensions/Echo/Echo.php" );
+                require_once( "$IP/extensions/Thanks/Thanks.php" );
                 
 		break;
 }
