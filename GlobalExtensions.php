ede9e8a35f29224d8d5a1a4a23fe98bb1e1a79eb
Reception123 <motoc.luca@gmail.com>
2015-07-28 12:11:21 +0200

    add Nuke, Gadgets, MassMessage, GlobalBlocking
    add global extensions

--- GlobalExtensions.php
+++ GlobalExtensions.php
@@ -2,7 +2,11 @@
 require_once( "$IP/extensions/AbuseFilter/AbuseFilter.php" );
 require_once( "$IP/extensions/CentralAuth/CentralAuth.php" );
 require_once( "$IP/extensions/CreateWiki/CreateWiki.php" );
+require_once( "$IP/extensions/Gadgets/Gadgets.php" );
+require_once( "$IP/extensions/GlobalBlocking/GlobalBlocking.php" );
 wfLoadExtension( 'CheckUser' );
+wfLoadExtension( 'MassMessage' );
+wfLoadExtension( 'Nuke' );
 wfLoadExtension( 'TitleBlacklist' );
 wfLoadExtension( 'ParserFunctions' );
 wfLoadExtension( 'SpamBlacklist' );
