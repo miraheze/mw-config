09d3febc9fb42e42c5e0eaf8469f1c66327ceeaf
Reception123 <motoc.luca@gmail.com>
2015-07-28 12:45:56 +0200

    rem GlobalBlocking

--- GlobalExtensions.php
+++ GlobalExtensions.php
@@ -3,7 +3,6 @@ require_once( "$IP/extensions/AbuseFilter/AbuseFilter.php" );
 require_once( "$IP/extensions/CentralAuth/CentralAuth.php" );
 require_once( "$IP/extensions/CreateWiki/CreateWiki.php" );
 require_once( "$IP/extensions/Gadgets/Gadgets.php" );
-require_once( "$IP/extensions/GlobalBlocking/GlobalBlocking.php" );
 wfLoadExtension( 'CheckUser' );
 wfLoadExtension( 'MassMessage' );
 wfLoadExtension( 'Nuke' );
