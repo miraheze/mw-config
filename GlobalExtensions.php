217209b82f6a675b6a2abe10c511eb62af71f6eb
John F. Lewis <johnflewis93@gmail.com>
2015-08-16 16:21:14 +0100

    load Magic not Messages

--- GlobalExtensions.php
+++ GlobalExtensions.php
@@ -6,7 +6,7 @@ require_once( "$IP/extensions/ConfirmEdit/ConfirmEdit.php" );
 require_once( "$IP/extensions/ConfirmEdit/QuestyCaptcha.php" );
 require_once( "$IP/extensions/GlobalBlocking/GlobalBlocking.php" );
 require_once( "$IP/extensions/Math/Math.php" );
-require_once( "$IP/extensions/MirahezeMessages/MirahezeMessages.php" );
+require_once( "$IP/extensions/MirahezeMagic/MirahezeMagic.php" );
 require_once( "$IP/extensions/MobileFrontend/MobileFrontend.php" );
 require_once( "$IP/extensions/SiteMatrix/SiteMatrix.php" );
 require_once( "$IP/extensions/UrlShortener/UrlShortener.php" );
