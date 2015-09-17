83efd9e952774ff3c1debaa566626ecec8f34649
Reception123 <motoc.luca@gmail.com>
2015-09-17 06:24:07 +0200

    enable a few extensions for att per req
    The rest are not downloaded/configured.

--- LocalSettings.php
+++ LocalSettings.php
@@ -222,6 +222,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseDynamicPageList' => array(
 		'default' => false,
+		'allthetropeswiki' => true,
 		'camerainfowiki' => true,
 		'extloadwiki' => true,
 	),
@@ -244,6 +245,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseScribunto' => array(
 		'default' => false,
+		'allthetropeswiki' => true,
 		'cbmediawiki' => true,
 		'antiguabarbudacalypsowiki' => true,
 		'extloadwiki' => true,
@@ -278,6 +280,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseWikiEditor' => array(
 		'default' => true,
+		'allthetropeswiki' => true,
 	),
 
 	// Files
