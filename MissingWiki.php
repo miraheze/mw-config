f57d9fab0930aeb84d776997ca29da60989e3671
Yongmin Hong <revi@openmailbox.org>
2015-09-18 23:54:52 +0900

    Fix indent
    Duh

--- MissingWiki.php
+++ MissingWiki.php
@@ -4,7 +4,7 @@ if ( !in_array( $wgDBname, $wgLocalDatabases ) ) {
     header( "HTTP/1.0 404 Not Found" );
     echo <<<EOF
     <center><h1>404 Wiki Not Found</h1></center>
-<center>You can request your own wiki, follow the steps <a href="https://meta.miraheze.org/wiki/Special:RequestWiki">here</a>!</center>
+    <center>You can request your own wiki, follow the steps <a href="https://meta.miraheze.org/wiki/Special:RequestWiki">here</a>!</center>
     <hr>
     <center>nginx - MediaWiki</center>
 EOF;
