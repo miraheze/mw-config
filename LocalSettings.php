66b9b1efb14ee650402229f49ff021200b615b12
Arcane21 <Arcane21@users.noreply.github.com>
2015-09-25 16:16:17 -0500

    Request following exts be enabled on ATT

--- LocalSettings.php
+++ LocalSettings.php
@@ -228,6 +228,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseCollapsibleVector' => array(
 		'default' => false,
+		'allthetropeswiki' => true,
 		'anuwiki' => true,
 		'extloadwiki' => true,
 	),
@@ -255,6 +256,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseImageMap' => array(
 		'default' => false,
+		'allthetropeswiki' => true,
 		'extloadwiki' => true,
 		'creersonarbrewiki' => true,
 	),
@@ -265,6 +267,7 @@ $wgConf->settings = array(
 	'wmgUseMultiUpload' => array(
 		'default' => false,
 		'8stationwiki' => true,
+		'allthetropeswiki' => true,
 		'antiguabarbudacalypsowiki' => true,
 		'catboxwiki' => true,
 		'cinemawiki' => true,
@@ -319,6 +322,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseVisualEditor' => array(
 		'default' => false,
+		'allthetropeswiki' => true,
 		'cbmediawiki' => true,
 		'cinemawiki' => true,
 		'clicordiwiki' => true,
@@ -330,6 +334,7 @@ $wgConf->settings = array(
 		'spiralwiki' => true,
 		'torejorgwiki' => true,
 		'unikumwiki' => true,
+		
 	),
 	'wmgUseWikiEditor' => array(
 		'default' => true,
