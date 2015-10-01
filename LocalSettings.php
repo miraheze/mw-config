aebb0bf3b62dbebe1f6d02d5315c8b081a3655e5
John F. Lewis <johnflewis93@gmail.com>
2015-10-01 15:11:47 +0100

    re-enabled Flow occupy on permanentfuturelabwiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -407,10 +407,10 @@ $wgConf->settings = array(
 	// Flow
 	'wmgFlowOccupyNamespaces' => array(
 		'default' => array(),
-		//'permanentfuturelabwiki' => array(
-		//	NS_TALK, NS_USER_TALK, NS_PROJECT_TALK, NS_FILE_TALK, 
-		//	NS_MEDIAWIKI_TALK, NS_TEMPLATE_TALK, NS_HELP_TALK, NS_CATEGORY_TALK
-		//),
+		'permanentfuturelabwiki' => array(
+			NS_TALK, NS_USER_TALK, NS_PROJECT_TALK, NS_FILE_TALK, 
+			NS_MEDIAWIKI_TALK, NS_TEMPLATE_TALK, NS_HELP_TALK, NS_CATEGORY_TALK
+		),
 		'spiralwiki' => array(
 			NS_TALK, NS_USER_TALK, NS_PROJECT_TALK, NS_FILE_TALK,
 			NS_MEDIAWIKI_TALK, NS_TEMPLATE_TALK, NS_HELP_TALK, NS_CATEGORY_TALK
