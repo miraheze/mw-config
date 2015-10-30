c63cfc1c85617826f92cc7b93b0d6d2a880265a9
Martin Sidaway <msidaway@gmail.com>
2015-10-30 20:39:42 +0000

    Configure extensions for walthamstowlabourwiki.

--- LocalSettings.php
+++ LocalSettings.php
@@ -226,6 +226,7 @@ $wgConf->settings = array(
 		'poserdazfreebieswiki' => true,
 		'welcomewiki' => true,
 		'wikiacawiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 
 	// Extensions
@@ -238,6 +239,7 @@ $wgConf->settings = array(
 		'poserdazfreebieswiki' => true,
 		'szkwiki' => true,
 		'testwiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 	'wmgUseBabel' => array(
 		'default' => true,
@@ -246,6 +248,7 @@ $wgConf->settings = array(
 		'default' => false,
 		'allthetropeswiki' => true,
 		'extloadwiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 	'wmgUseCollapsibleVector' => array(
 		'default' => false,
@@ -265,6 +268,7 @@ $wgConf->settings = array(
 		'camerainfowiki' => true,
 		'extloadwiki' => true,
 		'heistwiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 	'wmgUseEchoThanks' => array(
 		'default' => true,
@@ -277,6 +281,7 @@ $wgConf->settings = array(
 		'permanentfuturelabwiki' => true,
 		'spiralwiki' => true,
 		'spiraltestwiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 	'wmgUseImageMap' => array(
 		'default' => false,
@@ -314,6 +319,7 @@ $wgConf->settings = array(
 		'default' => false,
 		'allthetropeswiki' => true,
 		'extloadwiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 	'wmgUseScribunto' => array(
 		'default' => false,
@@ -335,6 +341,7 @@ $wgConf->settings = array(
 		'spiraltestwiki' => true,
 		'szkwiki' => true,
 		'tanodswiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 	// Do not ever enable without contacting me! --Southparkfan
 	'wmgUseSocialProfile' => array(
@@ -397,12 +404,14 @@ $wgConf->settings = array(
 		'spiralwiki' => true,
 		'torejorgwiki' => true,
 		'unikumwiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 	'wmgUseVariables' => array(
 		'default' => false,
 		'allthetropeswiki' => true,
 		'extloadwiki' => true,
 		'szkwiki' => true,
+		'walthamstowlabourwiki' => true,
 	),
 	'wmgUseWikiEditor' => array(
 		'default' => true,
@@ -456,6 +465,10 @@ $wgConf->settings = array(
 			NS_MEDIAWIKI_TALK, NS_TEMPLATE_TALK, NS_HELP_TALK, NS_CATEGORY_TALK
 		),
 		'spiraltestwiki' => array(
+			NS_TALK, NS_USER_TALK, NS_PROJECT_TALK, NS_FILE_TALK,
+			NS_MEDIAWIKI_TALK, NS_TEMPLATE_TALK, NS_HELP_TALK, NS_CATEGORY_TALK
+		),
+		'walthamstowlabourwiki' => array(
 			NS_TALK, NS_USER_TALK, NS_PROJECT_TALK, NS_FILE_TALK,
 			NS_MEDIAWIKI_TALK, NS_TEMPLATE_TALK, NS_HELP_TALK, NS_CATEGORY_TALK
 		),
