d5c6b301613360eebd7a1b6faaa63d760e64153b
Owen Baines <OwenBaines@users.noreply.github.com>
2015-12-23 17:09:33 +0000

    developmentwiki, flow, visualeditor
    https://meta.miraheze.org/wiki/Request_features#Development_Wiki_.28Status:_Open.29
    
    pr 44 in puppet

--- LocalSettings.php
+++ LocalSettings.php
@@ -296,6 +296,7 @@ $wgConf->settings = array(
 	'wmgUseFlow' => array(
 		'default' => false,
 		'8stationwiki' => true,
+		'developmentwiki' => true,
 		'extloadwiki' => true,
 		'mecanonwiki' => true,
 		'permanentfuturelabwiki' => true,
@@ -421,6 +422,7 @@ $wgConf->settings = array(
 		'braindumpwiki' => true,
 		'cbmediawiki' => true,
 		'clicordiwiki' => true,
+		'developmentwiki' => true,
 		'extloadwiki' => true,
 		'esswaywiki' => true,
 		'etpowiki' => true,
@@ -1413,13 +1415,3 @@ if ( $wgDBname == 'extloadwiki' || $wgDBname == 'allthetropeswiki' ) {
 }
 
 $wgDefaultUserOptions['enotifwatchlistpages'] = 0;
-# TestWiki hook
-if ( $wgDBname === 'testwiki' ) {
-    $wgHooks['PrefsPasswordAudit'][] = 'test_onPrefsPasswordAudit';
-    function test_onPrefsPasswordAudit( $user, $newPass, $error ) {
-        if ( $user->getName() == 'Example' ) {
-            return "User not allowed to change password, Example account";
-        }
-        return true;
-    }
-}
