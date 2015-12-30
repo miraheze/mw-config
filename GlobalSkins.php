f0eee54c1a86f17866aef4c0964ab9a108a367a6
Reception123 <motoc.luca@gmail.com>
2015-12-30 15:08:08 +0100

    add foreground
    I am not sure if this is supposed to be a global skin. DO NOT MERGE WITHOUT THE CONSENT OF SOUTHPARKFAN!

--- GlobalSkins.php
+++ GlobalSkins.php
@@ -1,5 +1,6 @@
 <?php
 require_once( "$IP/skins/CologneBlue/CologneBlue.php" );
+require_once( "$IP/skins/Foreground/Foreground.php" );
 wfLoadSkin( 'Modern' );
 wfLoadSkin( 'MonoBook' );
 wfLoadSkin( 'Vector' );
