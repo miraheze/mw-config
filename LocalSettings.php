6fb251bf6a60e693568bdfa2f7e8b13000cc2408
Southparkfan <Southparkfan@users.noreply.github.com>
2016-01-02 18:14:13 +0100

    Add dormancy policy notice

--- LocalSettings.php
+++ LocalSettings.php
@@ -1594,3 +1594,15 @@ function onPrefsPasswordAuditTestWiki( $user, $newPass, $error ) {
 		return true;
 }
 
+// **** TEMPORARILY FOR DORMANCY POLICY ****
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+function onSiteNoticeAfter( &$siteNotice, $skin ) {
+	$siteNotice .= <<<EOF
+	<table class="wikitable" style="text-align:center;"><tbody><tr>
+	<td>On 5 January 2016, Miraheze will implement an official <a class="external free" href="https://meta.miraheze.org/wiki/Dormancy_Policy">Dormancy Policy.</a>. This policy contains important information about the activity levels a wiki should show to avoid closure or deletion of the wiki. Please do not hesitate to <a class="external free" href="https://meta.miraheze.org/wiki/Stewards'_Noticeboard">contact us</a> if you have any questions regarding this policy.</td>
+	</tr></tbody></table>
+EOF;
+
+	return true;
+}
+// **** TEMPORARILY FOR DORMANCY POLICY ****
