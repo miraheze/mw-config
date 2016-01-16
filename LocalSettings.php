febd76ed976f79ca82797e8abe09cd85142cc84b
Southparkfan <Southparkfan@users.noreply.github.com>
2016-01-17 00:19:21 +0100

    Increase wgResourceLoaderMaxage's unversioned cached times

--- LocalSettings.php
+++ LocalSettings.php
@@ -135,8 +135,8 @@ $wgConf->settings = array(
 				'client' => 1 * 24 * 60 * 60, // 1 day
 			),
 			'unversioned' => array(
-				'server' => 5 * 60, // 5 minutes
-				'client' => 15 * 60, // 15 minutes
+				'server' => 10 * 60, // 10 minutes
+				'client' => 60 * 60, // 1 hour
 			),
 		),
 	),
