8f61b46ee77d3abb0ead3ef2d28bdc8aaa138ea3
Reception123 <motoc.luca@gmail.com>
2016-01-23 11:40:37 +0100

    spacing

--- LocalExtensions.php
+++ LocalExtensions.php
@@ -80,9 +80,11 @@ if ( $wmgUseMultiUpload ) {
 if ( $wmgUseMobileFrontend ) {
 	require_once( "$IP/extensions/MobileFrontend/MobileFrontend.php" );
 }
+
 if { $wmgUseMonaco ) {
 	require_once( "$IP/extensions/Monaco/Monaco-port.php" );
 }
+
 if ( $wmgUsePopups ) {
         require_once( "$IP/extensions/PageImages/PageImages.php" );
         require_once( "$IP/extensions/Popups/Popups.php" );
