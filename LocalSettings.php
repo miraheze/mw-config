2303663b0f8da3aaa1abd30ec21d1b86e02f2e8c
Reception123 <motoc.luca@gmail.com>
2016-02-26 06:48:29 +0100

    add MSUpload per req

--- LocalSettings.php
+++ LocalSettings.php
@@ -437,6 +437,7 @@ $wgConf->settings = array(
 		'exitsincwiki' => true,
 		'izanagiwiki' => true,
 		'luckandlogicwiki' => true,
+		'universebuildwiki' => true,
 		'webflowwiki' => true,
 	),
 	'wmgUseMultimediaViewer' => array(
