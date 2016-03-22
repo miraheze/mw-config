69260709e63f90ff905d685e7c421d0952e72890
Southparkfan <southparkfan223@hotmail.com>
2016-03-22 15:52:09 +0100

    Revert "Convert dblist to JSON"
    This reverts commit 586e32ce60e3fab1a2a01726c6d496e9e6353a55.

--- LocalSettings.php
+++ LocalSettings.php
@@ -1942,23 +1942,26 @@ if ( defined( 'MW_DB' ) ) {
 
 # Initialize dblist
 $wgLocalDatabases = array();
-$wmgDatabaseList = file_get_contents( "/srv/mediawiki/dblist/all.dblist" );
+$wmgDatabaseList = file( "/srv/mediawiki/dblist/all.dblist" );
 
-foreach ( json_decode( $wmgDatabaseList, true ) as $entry ) {
-	$wikiDB = array_keys( $entry )[0];
-	
-	$wgLocalDatabases[] = $wikiDB;
-	
-	$wgConf->settings['wgSitename'][$wikiDB] = $entry[$wikiDB]['settings']['sitename'];
-	$wgConf->settings['wgLanguageCode'][$wikiDB] = $entry[$wikiDB]['settings']['language'];
+foreach ( $wmgDatabaseList as $wikiLine ) {
+	$wikiDB = explode( '|', $wikiLine, 4 );
+	list( $DBname, $siteName, $siteLang, $wikiTagList ) = array_pad( $wikiDB, 4, '' );
+	$wgLocalDatabases[] = $DBname;
+	$wgConf->settings['wgSitename'][$DBname] = $siteName;
+	$wgConf->settings['wgLanguageCode'][$DBname] = $siteLang;
+}
 
-	if ( $entry[$wikiDB]['private'] == true ) {
-		$wgConf->settings['wmgPrivateWiki'][$wikiDB] = $entry[$wikiDB]['private'];
-	}
-	
-	if ( $entry[$wikiDB]['closed'] == true ) {
-		$wgConf->settings['wmgClosedWiki'][$wikiDB] = $entry[$wikiDB]['closed'];
-	}
+$wmgPrivateDatabasesList = file( "/srv/mediawiki/dblist/private.dblist" );
+foreach ( $wmgPrivateDatabasesList as $database ) {
+	$database = trim( $database );
+	$wgConf->settings['wmgPrivateWiki'][$database] = true;
+}
+
+$wmgClosedDatabasesList = file( "/srv/mediawiki/dblist/closed.dblist" );
+foreach ( $wmgClosedDatabasesList as $database ) {
+	$database = trim( $database );
+	$wgConf->settings['wmgClosedWiki'][$database] = true;
 }
 
 require_once( "/srv/mediawiki/config/MissingWiki.php" );
