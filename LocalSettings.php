miraheze/mw-config

commit d76d61b566f4d6bc3d3a5b473acfb80413997426

Adding Visual Editor and Flow for ernaehrungsrathhwiki
* Adding Visual Editor and Flow ernaehrungsrathh

Requested at https://meta.miraheze.org/wiki/Request_features#ErnaehrungsratHH_.28Status:_Open.29

Closes #444

LocalSettings.php
@@ -397,6 +397,7 @@ $wgConf->settings = array(
 		'spiralwiki' => true,
 		'universebuildwiki' => true,
 		'walthamstowlabourwiki' => true,
+		'ernaehrungsrathhwiki' => true
 	),
 	'wmgUseForeground' => array(
 		'default' => false,
@@ -713,6 +714,7 @@ $wgConf->settings = array(
 		'vrgowiki' => true,
 		'walthamstowlabourwiki' => true,
 		'webflowwiki' => true,
+		'ernaehrungsrathhwiki' => true,
 	),
 	'wmgUseVariables' => array(
 		'default' => false,

commit 296a5ff82ddee7ec8c6976516d88a7f753eed3bf

Fix errors
Regression introduced in #444

LocalSettings.php
@@ -388,6 +388,7 @@ $wgConf->settings = array(
 		'christipediawiki' => true,
 		'developmentwiki' => true,
 		'drunkenpeasantswikiwiki' => true,
+		'ernaehrungsrathhwiki' => true,
 		'extloadwiki' => true,
 		'mecanonwiki' => true,
 		'ndtestwiki' => true,
@@ -397,7 +398,6 @@ $wgConf->settings = array(
 		'spiralwiki' => true,
 		'universebuildwiki' => true,
 		'walthamstowlabourwiki' => true,
-		'ernaehrungsrathhwiki' => true
 	),
 	'wmgUseForeground' => array(
 		'default' => false,
@@ -670,6 +670,7 @@ $wgConf->settings = array(
 		'drunkenpeasantswikiwiki' => true,
 		'extloadwiki' => true,
 		'elainarmuawiki' => true,
+		'ernaehrungsrathhwiki' => true,
 		'esswaywiki' => true,
 		'etpowiki' => true,
 		'gameswiki' => true,
@@ -714,7 +715,6 @@ $wgConf->settings = array(
 		'vrgowiki' => true,
 		'walthamstowlabourwiki' => true,
 		'webflowwiki' => true,
-		'ernaehrungsrathhwiki' => true,
 	),
 	'wmgUseVariables' => array(
 		'default' => false,