miraheze/mw-config

commit 62a66fccffa099fa375e225dd458259ff3550b57

TMEwiki aliases (#491)
* TMEwiki aliases

* Update LocalSettings.php

More TMEwiki aliases

LocalSettings.php
@@ -1277,6 +1277,20 @@ $wgConf->settings = array(
 		'+tmewiki' => array(
 			'The_Multilingual_Encyclopedia' => NS_PROJECT,
 			'The_Multilingual_Encyclopedia_talk' => NS_PROJECT_TALK,
+			'Bestand' => NS_FILE,
+			'Categorie' => NS_CATEGORY,
+			'Categoría' => NS_CATEGORY,
+			'Archivo' => NS_FILE,
+			'Módulo' => NS_MODULE,
+			'Especial' => NS_SPECIAL,
+			'Espesyal' => NS_SPECIAL,
+			'Specialaĵo' => NS_SPECIAL,
+			'Specialis' => NS_SPECIAL,
+			'Категория' => NS_CATEGORY,
+			'Портал' => NS_PORTAL,
+			'Служебная' => NS_SPECIAL,
+			'Kuva' => NS_FILE,
+			'Luokka' => NS_CATEGORY,
 		),
 	),
 	'+wgNamespacesWithSubpages' => array(

commit 424f9daa8a429047bee75b7d5df633c150a03dda

Fix farm-wide warning caused by 62a66fc (pull #491)

LocalSettings.php
@@ -71,6 +71,10 @@ define( 'NS_OFFICIAL_TALK', 1625 );
 define( 'NS_PORTAL', 1626 );
 define( 'NS_PORTAL_TALK', 1627 );
 
+// Refer to NS_MODULE before importing Scribunto (tmewiki)
+define( 'WMG_NS_MODULE', 828 );
+define( 'WMG_NS_MODULE_TALK', 829 );
+
 $wgConf->settings = array(
 	// AbuseFilter
 	'wgAbuseFilterCentralDB' => array(
@@ -1281,7 +1285,7 @@ $wgConf->settings = array(
 			'Categorie' => NS_CATEGORY,
 			'Categoría' => NS_CATEGORY,
 			'Archivo' => NS_FILE,
-			'Módulo' => NS_MODULE,
+			'Módulo' => WMG_NS_MODULE,
 			'Especial' => NS_SPECIAL,
 			'Espesyal' => NS_SPECIAL,
 			'Specialaĵo' => NS_SPECIAL,