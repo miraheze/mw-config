6f1fc786a2341f27c0853f99db4668b6a873778a
Southparkfan <southparkfan223@hotmail.com>
2016-07-27 21:50:20 +0200

    Back to misc2

--- RedisConfig.php
+++ RedisConfig.php
@@ -1,7 +1,7 @@
 <?php
 $wgObjectCaches['redis'] = array(
 	'class' => 'RedisBagOStuff',
-	'servers' => array( '185.52.1.76:6379' ),
+	'servers' => array( '81.4.127.174:6379' ),
 	'password' => $wmgRedisPassword,
 );
 
@@ -15,7 +15,7 @@ $wgLanguageConverterCacheType = CACHE_DB;
 
 $wgJobTypeConf['default'] = array(
 	'class' => 'JobQueueRedis',
-	'redisServer' => '185.52.1.76:6379',
+	'redisServer' => '81.4.127.174:6379',
 	'redisConfig' => array(
 		'connectTimeout' => 2,
 		'password' => $wmgRedisPassword,
@@ -27,7 +27,7 @@ $wgJobTypeConf['default'] = array(
 
 $wgJobQueueAggregator = array(
         'class' => 'JobQueueAggregatorRedis',
-        'redisServers' => array( '185.52.1.76:6379', '185.52.1.76:6379' ), // fake misc2 as fallback
+        'redisServers' => array( '81.4.127.174:6379', '81.4.127.174:6379' ), // fake misc2 as fallback
         'redisConfig' => array(
                 'connectTimeout' => 2,
                 'password' => $wmgRedisPassword,
