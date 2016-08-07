a79d14187b0cef750e6812411ca7ffc2cdc0a3bc
Reception123 <motoc.luca@gmail.com>
2016-08-07 09:42:05 +0200

    re-enable ReplaceText on extloadwiki
    Not a bug of the extension, it was stock mediawiki (@labster found it)

--- LocalSettings.php
+++ LocalSettings.php
@@ -958,7 +958,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseReplaceText' => array(
 		'default' => false,
-		'extloadwiki' => false, // Let me create Special:RecentChanges page, please don't enable until being sure what the bug is
+		'extloadwiki' => true,
 	),
 	'wmgUseSandboxLink' => array(
 		'default' => false,
