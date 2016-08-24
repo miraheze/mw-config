77f2314285e2f8eb60efbac7354c7650a6dbfea9
Reception123 <motoc.luca@gmail.com>
2016-08-24 16:03:53 +0200

    move RSS settings above to see if that is the issue per NDKilla

--- LocalSettings.php
+++ LocalSettings.php
@@ -1000,6 +1000,19 @@ $wgConf->settings = array(
 		'extloadwiki' => true,
 		'thelonsdalebattalionwiki' => true,
 	),
+	// RSS Settings
+	'wgRSSCacheAge' => array(
+		'default' => '3600'
+	),
+	'wgRSSUrlWhitelist' => array(
+		'default' => array( "*" )
+	),
+	'wgRSSProxy' => array(
+		'default' => false,
+	),
+	'wgRSSDateDefaultFormat' => array(
+		'default' => 'Y-m-d H:i:s'
+	),
 	'wmgUseRSS' => array(
 		'default' => false,
 		'extloadwiki' => true,
