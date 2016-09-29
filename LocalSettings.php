ce559d409d526e5a8d7554fd59e25647a5c84750
ImBoPhil <josh.rolandez@gmail.com>
2016-09-29 20:18:52 +0200

    enable VE + YouTube (1209wiki) T814

--- LocalSettings.php
+++ LocalSettings.php
@@ -1217,6 +1217,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseVisualEditor' => array(
 		'default' => false, // Please make sure parsoid is enabled on modules/parsoid/manifests/init.pp or modules/parsoid/templates/settings.js (custom domains only)
+		'1209wiki' => true,
 		'3dicxyzwiki' => true,
 		'8stationwiki' => true,
 		'aacenterpriselearningwiki' => true,
@@ -1453,6 +1454,7 @@ $wgConf->settings = array(
 	),
 	'wmgUseYouTube' => array(
 		'default' => false,
+		'1209wiki' => true,
 		'airwiki' => true,
 		'alanpediawiki' => true,
 		'allthetropeswiki' => true,
