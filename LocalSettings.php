38f12a1f54162680a3bd0a5490f9b1e57c835aaa
LegoFan4000 <paulfkeffer@gmail.com>
2016-10-24 16:57:51 -0400

    Enable SyntexHighlight on noalatalawiki for T935

--- LocalSettings.php
+++ LocalSettings.php
@@ -1236,6 +1236,7 @@ $wgConf->settings = array(
 		'trexwiki' => true,
 		'ndnwiki' => true,
 		'nextlevelwikiwiki' => true,
+		'noalatalawiki' => true,
 		'partupwiki' => true,
 		'pascalscada' => true,
 		'perpuswiki' => true,
