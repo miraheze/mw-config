473fc120cc24ea0aaf608590425478cade7ac09d
LegoFan4000 <paulfkeffer@gmail.com>
2016-11-04 13:52:43 -0400

    Update LocalExtensions.php

--- LocalExtensions.php
+++ LocalExtensions.php
@@ -330,6 +330,10 @@ if ( $wmgUseSectionHide ) {
 	wfLoadExtension( 'SectionHide' );
 }
 
+if ( $wmgUseShortURL ) {
+	require_once( "$IP/extensions/UrlShortener/UrlShortener.php" );
+}
+
 if ( $wmgUseSimpleTooltip ) {
 	require_once( "$IP/extensions/SimpleTooltip/SimpleTooltip.php" );
 }
