b74eca5f831a1399d5143d17beeb18024706af8d
ImBoPhil <josh.rolandez@gmail.com>
2016-11-08 20:30:27 +0100

    add TorBlock for T981

--- LocalSettings.php
+++ LocalSettings.php
@@ -1328,6 +1328,10 @@ $wgConf->settings = array(
 		'wisdomwikiwiki' => true,
 		'wisdomsandboxwiki' => true,
 	),
+	'wmgUseTorBlock' => array(
+		'default' => false,
+		'extloadwiki' => true,
+        ),
 	'wmgUseTranslate' => array(
 		'default' => false,
 		'3dicwiki' => true,
