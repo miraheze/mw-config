ee3342448ce071d5907425eb8bfb59cc74276bdf
John F. Lewis <johnflewis93@gmail.com>
2016-12-02 18:59:37 +0000

    site notice for upgrade

--- LocalSettings.php
+++ LocalSettings.php
@@ -4215,13 +4215,13 @@ if ( !file_exists( '/srv/mediawiki/w/cache/l10n/l10n_cache-en.cdb' ) ) {
 }
 
 // Global SiteNotice
-/* $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	$siteNotice .= <<<EOF
 	<table class="wikitable" style="text-align:center;"><tbody><tr>
-	<td>The search feature has been disabled on all Miraheze wikis, because it was responsible for the server crash yesterday. Miraheze staff is trying to find a fix for this bug, until that is done you won't be able to use the search feature.</td>
+	<td>MediaWiki 1.28 upgrade: Following the previous attempt, Miraheze has tested MediaWiki 1.28 and is satisfied a deployment is possible. Therefore, at around 19:30 UTC wikis will be put into read-only while the relevant changes are made to deploy 1.28. The window for this is expected to be short ranging from 30 minutes to 1 hour. If you have any issues, speak to #miraheze on freenode.net.</td>
 	</tr></tbody></table>
 EOF;
 
 	return true;
-} */
+}
