98bcb9da3d9501f8c133b6a3263828179b4dc1cc
DeltaQuad <amandaquad@yahoo.com>
2016-12-27 19:10:21 -0500

    Actually, it's better to limit all Flow admin actions to myself

--- LocalSettings.php
+++ LocalSettings.php
@@ -3280,6 +3280,11 @@ $wgConf->settings = array(
 				'editusercss' => true,
 				'edituserjs' => true,
 				'unblockself' => true,
+				'flow-hide' => true,
+				'flow-delete' => true,
+				'flow-lock' => true,
+				'flow-create-board' => true,
+				'flow-edit-post' => true,
 			),
 			'moderator' => array(
 				'deletelogentry' => true,
@@ -3315,13 +3320,6 @@ $wgConf->settings = array(
 			),
 			'bureaucrat' => array(
 				'tboverride' => true,
-				'flow-hide' => true,
-				'flow-delete' => true,
-				'flow-lock' => true,
-			),
-			'user' => array(
-				'flow-create-board' => true,
-				'flow-edit-post' => true,
 			),
 		
 		),
