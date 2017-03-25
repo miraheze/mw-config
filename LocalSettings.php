26d82f4dfba73b513077dc30debcb09b52c7aee1
Reception123 <motoc.luca@gmail.com>
2017-03-25 09:54:29 +0100

    enable AutoCreateCategoryPages on extloadwiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -862,6 +862,10 @@ $wgConf->settings = array(
 		'testauthorprotectwiki' => true,
 		'trexwiki' => true,
 	),
+	'wmgUseAutoCreateCategoryPages' => array
+		'default' => false, // DO NOT enable on wikis that have more than 500 categories
+		'extloadwiki' => true,
+	),
 	'wmgUseAutomaticBoardWelcome' => array(
 		'default' => false,
 		'allthetropeswiki' => true,
