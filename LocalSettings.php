9f8ac4f80624a1f789fbef84b3b3c37edebccb68
Amanda <Amanda1234567@users.noreply.github.com>
2017-04-19 06:49:16 -0400

    Revert removal of implicit checkuser

--- LocalSettings.php
+++ LocalSettings.php
@@ -4457,7 +4457,7 @@ $wgConf->settings = array(
 	'wgImplicitGroups' => array(
 		'default' => array( '*', 'user', 'autoconfirmed' ),
 		'bitcoindebateswiki' => array( '*', 'user', 'autoconfirmed', 'emailconfirmed' ),
-		'wikicanadawiki' => array( '*', 'user', 'autoconfirmed' ),
+		'wikicanadawiki' => array( '*', 'user', 'autoconfirmed', 'checkuser' ),
 	),
 	
 	// Piwik settings
