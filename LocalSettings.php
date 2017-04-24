a78f2204f895b6cf80f38604533801cc0ee9ff33
Amanda <Amanda1234567@users.noreply.github.com>
2017-04-24 19:06:06 -0400

    Enable Popups on nanatsunotaizaiwiki per T1714

--- LocalSettings.php
+++ LocalSettings.php
@@ -1604,6 +1604,7 @@ $wgConf->settings = array(
 		'jayuwikiwiki' => true,
 		'justinbieberwiki' => true,
 		'marcoschriekwiki' => true,
+		'nanatsunotaizaiwiki' => true,
 		'pgnwikiwiki' => true,
 		'takethatwikiwiki' => true,
 		'thefosterswiki' => true,
