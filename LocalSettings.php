fe66320465fb9f8cd8d0205fbc83b4d57c094b3b
Reception123 <utilizator.receptie123@gmail.com>
2017-05-01 07:38:20 +0200

    increment sitenotice to id 3

--- LocalSettings.php
+++ LocalSettings.php
@@ -5475,7 +5475,7 @@ putenv( "GDFONTPATH=/usr/share/fonts/truetype/freefont" );
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 2;
+$wgMajorSiteNoticeID = 3;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
