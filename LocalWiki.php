b519991cf3756c2678cc18bd4e7b936cf7b9baea
Amanda <Amanda1234567@users.noreply.github.com>
2017-05-06 12:34:34 -0400

    Drop restriction on changetags and massmessage

--- LocalWiki.php
+++ LocalWiki.php
@@ -158,8 +158,6 @@ if ( $wgDBname == 'wikicanadawiki' ) {
 	$wgGroupPermissions['*']['writeapi'] = false;
 	$wgGroupPermissions['*']['viewmyprivateinfo'] = false;
 	$wgGroupPermissions['*']['viewmywatchlist'] = false;
-	$wgGroupPermissions['sysop']['managechangetags'] = false;
-	$wgGroupPermissions['sysop']['deletechangetags'] = false;
 	$wgGroupPermissions['sysop']['importupload'] = false;
 	$wgGroupPermissions['sysop']['import'] = false;
 	$wgGroupPermissions['sysop']['markbotedits'] = false;
@@ -168,7 +166,6 @@ if ( $wgDBname == 'wikicanadawiki' ) {
 	$wgGroupPermissions['sysop']['abusefilter-revert'] = false;
 	$wgGroupPermissions['sysop']['unwatchedpages'] = false;
 	$wgGroupPermissions['sysop']['override-antispoof'] = false;
-	$wgGroupPermissions['sysop']['massmessage'] = false;
 	$wgGroupPermissions['sysop']['editusercss'] = false;
 	$wgGroupPermissions['sysop']['edituserjs'] = false;
 	$wgGroupPermissions['sysop']['editinterface'] = false;
