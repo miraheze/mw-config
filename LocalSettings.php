0c5d2315a2b624cb7d035403deccab5452cb2afb
ImBoPhil <josh.rolandez@gmail.com>
2017-06-08 18:25:03 +0200

    enable TocTree (tedcswiki) T1860

--- LocalSettings.php
+++ LocalSettings.php
@@ -1937,6 +1937,7 @@ $wgConf->settings = array(
 	'wmgUseTocTree' => array(
 		'default' => false,
 		'extloadwiki' => true,
+		'tedcswiki' => true,
 	),
 	'wmgUseTranslate' => array(
 		'default' => false,
