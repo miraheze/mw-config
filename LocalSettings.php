78c916a4a4f259b1365309c847c754b80608dec8
Brent Laabs <bslaabs@gmail.com>
2017-06-25 20:41:28 -0700

    Bump sitenotice number

--- LocalSettings.php
+++ LocalSettings.php
@@ -5720,7 +5720,7 @@ putenv( "GDFONTPATH=/usr/share/fonts/truetype/freefont" );
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 4;
+$wgMajorSiteNoticeID = 5;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
