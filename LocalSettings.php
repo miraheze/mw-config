3f130ae4c328613a73bf967fea687d61f82a77ce
Devin/Zppix <megadev44s.mail@gmail.com>
2017-07-20 13:54:58 -0500

    deleted since it was false

--- LocalSettings.php
+++ LocalSettings.php
@@ -1809,7 +1809,6 @@ $wgConf->settings = array(
 		'extloadwiki' => true,
 		'grandtheftautowiki' => true,
 		'inazumaelevenwiki' => true,
-		'thebbwiki' => false,
 	),
 	'wmgUseRelatedArticles' => array(
 		'default' => false,
