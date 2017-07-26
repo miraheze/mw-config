bc9dd49eee88307928ab8c214c7c545716ab5f9d
CnocBride <cnocbridebusiness@gmail.com>
2017-07-26 15:56:08 +0100

    Removing sysex from Variables.

--- LocalSettings.php
+++ LocalSettings.php
@@ -2416,7 +2416,6 @@ $wgConf->settings = array(
 		'extloadwiki' => true,
 		'eotewiki' => true,
 		'marcoschriekwiki' => true,
-		'sysexwiki' => true,
 		'szkwiki' => true,
 		'takethatwikiwiki' => true,
 		'wikikaisagawiki' => true,
