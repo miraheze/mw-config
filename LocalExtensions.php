b1961485c5757ca776e9ba6e6e0ab9b68816db7d
MacFan4000 <paulfkeffer@gmail.com>
2017-09-10 13:40:58 -0400

    Update LocalExtensions.php

--- LocalExtensions.php
+++ LocalExtensions.php
@@ -287,11 +287,14 @@ if ( $wmgUseLoginNotify ) {
 	wfLoadExtension( 'LoginNotify' );
 }
 
-if ( $wmgUseLoopsCombo ) {
-	require_once( "$IP/extensions/Variables/Variables.php" );
-	require_once( "$IP/extensions/Loops/Loops.php");
+if ( $wmgUseMagicNoCache ) {
+	require_once "$IP/extensions/MagicNoCache/MagicNoCache.php";
 }
 
+if ( $wmgUseMaps ) {
+	require_once( "$IP/extensions/Maps/Maps.php" );
+}
+	
 if ( $wmgUseMaps ) {
 	require_once( "$IP/extensions/Maps/Maps.php" );
 	$egMapsDefaultService = 'openlayers';
