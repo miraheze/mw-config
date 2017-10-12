5dab61190a5168c38695c1a2ef55a37cf64c238b
Reception123 <utilizator.receptie123@gmail.com>
2017-10-12 19:59:15 +0200

    allow mp3 uploads on themirrorwiki per req T2292

--- LocalSettings.php
+++ LocalSettings.php
@@ -2989,6 +2989,7 @@ $wgConf->settings = array(
 		'+scruffywiki' => array( 'pdf', 'mid', 'mp3', 'flac', 'svg', 'fpd', 'oga', 'ogv'),
 		'+serinfhospwiki' => array( 'pdf', 'zip' ),
 		'+techeducationwiki' => array( 'docx', 'doc', 'odt', 'ods', 'odp', 'ppt', 'xls', 'xlsx','xml'),
+		'+themirrorwiki' => array( 'mp3'),
 		'+valentinaprojectwiki' => array( 'val', 'vit', 'vst'),
 		'+wisdomwikiwiki' => array( 'docx', 'doc', 'odt', 'ods', 'odp', 'xls', 'xlsx', 'txt', 'rtf', 'zip'),
 		'+wisdomsandboxwiki' => array( 'docx', 'doc', 'odt', 'ods', 'odp', 'xls', 'xlsx', 'txt', 'rtf', 'zip'),
