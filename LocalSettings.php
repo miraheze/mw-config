e08b53153b0bfc340733436bcb12b5ef07f2697d
Rob Kam <robkam@ymail.com>
2017-11-25 10:07:34 +0000

    sdiywiki moderation user rights

--- LocalSettings.php
+++ LocalSettings.php
@@ -4452,6 +4452,13 @@ $wgConf->settings = array(
 				'rollbacker',
 			),
 		),
+		'+sdiywiki' => array(
+			'moderators' => array(
+				'moderation', // Allow moderators to use Special:Moderation
+				'skip-moderation', // Allow moderators to skip moderation
+				'automoderated', // Allow moderators to assign/remove "automoderated" flag
+			),
+		),
 		'+testwiki' => array(
 			'bureaucrat' => array(
 				'testgroup',
@@ -5038,8 +5045,10 @@ $wgConf->settings = array(
 			),
 		),
 		'+sdiywiki' => array(
-			'sysop' => array(
-				'moderation' => true,
+			'moderators' => array(
+				'moderation', // Allow moderators to use Special:Moderation
+				'skip-moderation', // Allow moderators to skip moderation
+				'automoderated', // Allow moderators to assign/remove "automoderated" flag
 			),
 		),		
 		'+serinfhospwiki' => array(
@@ -5400,6 +5409,13 @@ $wgConf->settings = array(
 				'confirmed',
 				'sysop',
 				'rollbacker',
+			),
+		),
+		'+sdiywiki' => array(
+			'moderators' => array(
+				'moderation', // Allow moderators to use Special:Moderation
+				'skip-moderation', // Allow moderators to skip moderation
+				'automoderated', // Allow moderators to assign/remove "automoderated" flag
 			),
 		),
 		'+serinfhospwiki' => array(
