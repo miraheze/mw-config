d88dd94d6c09e3c090f82b390deda101c19f28e8
Reception123 <utilizator.receptie123@gmail.com>
2017-12-09 12:04:49 +0100

    enable visualeditor on testarkclswiki per req T2509

--- LocalSettings.php
+++ LocalSettings.php
@@ -2766,6 +2766,7 @@ $wgConf->settings = array(
 		'tekkenwiki' => true,
 		'teleswikiwiki' => true,
 		'testwiki' => true,
+		'testarkcls' => true,
 		'thebbwiki' => true,
 		'thecscwiki' => true,
 		'thefosterswiki' => true,
