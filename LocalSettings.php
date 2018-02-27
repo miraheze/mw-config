d46d75a3db162db9a4683c720a99d8d3744f5b0c
JVLopes <jvlopestheidiot@gmail.com>
2018-02-27 15:05:39 -0300

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -6473,7 +6473,7 @@ $wgConf->settings = array(
 		'worlduniversityandschoolwiki' => "//$wmgUploadHostname/worlduniversityandschoolwiki/6/60/WorldUnivAndSchLogo2017-08-18Wave.png",
 		'zharkunuwiki' => "//$wmgUploadHostname/zharkunuwiki/4/41/Zharkunu_Logo_-_135.png",
 	),
-	'53' => array(
+	'wgLogo' => array(
 		'default' => "//$wmgUploadHostname/metawiki/3/35/Miraheze_Logo.svg",
 		'690squadronwiki' => "//$wmgUploadHostname/690squadronwiki/f/f4/Squadlogo.png",
 		'8stationwiki' => "//$wmgUploadHostname/8stationwiki/3/3b/Wiki_logo.png",
