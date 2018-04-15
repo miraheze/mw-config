73ced4be9b59d4cd4c726057a0e15dbdf07af1f8
paladox <paladox@users.noreply.github.com>
2018-04-15 16:45:43 +0100

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -7823,7 +7823,7 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	 if ( !$wmgSiteNoticeOptOut || $snImportant ) {
 		$siteNotice .= <<<EOF
 		<table class="wikitable" style="text-align:center;"><tbody><tr>
-		<td>We will be setting all wiki's into read only mode starting at 18:00 UTC time. We are doing some maintenance which requires read only mode. It should last about 1 hour and 30 mins. Please see our <a href="https://www.facebook.com/miraheze/">Facebook</a> or our <a href="https://twitter.com/miraheze">Twitter</a> for more updates.</p></td>
+		<td>We will be setting all wiki's into read only mode starting at 16:00 UTC time. We are doing some maintenance which requires read only mode. It should last about 1 hour and 30 mins. Please see our <a href="https://www.facebook.com/miraheze/">Facebook</a> or our <a href="https://twitter.com/miraheze">Twitter</a> for more updates.</p></td>
 		</tr></tbody></table>
 EOF;
 	 }
