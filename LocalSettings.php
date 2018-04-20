3e7e92bab57e8efd7314beafdae96a6d18c0b813
paladox <paladox@users.noreply.github.com>
2018-04-20 22:40:15 +0100

    Set read only message on poserdazfreebieswiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -7839,17 +7839,17 @@ EOF;
 */
 
 // leaving here as placeholder for future db work
-/*if ( $wgDBname == 'conworldwiki' ) {
+if ( $wgDBname == 'poserdazfreebieswiki' ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter2';
 	function onSiteNoticeAfter2( &$siteNotice, $skin ) {
 			$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze will perform database maintenance on this wiki, from 19:05 UTC until approx. 19:35 UTC. During this time the wiki will be in read-only mode. Please save your edits before 19:00 UTC!</td>
+			<td>Miraheze will perform database maintenance on this wiki, from 21:50 UTC until approx. 20:20 UTC. During this time the wiki will be in read-only mode. Please save your edits before 21:45 UTC!</td>
 			</tr></tbody></table>
 EOF;
 		return true;
 	}
-}*/
+}
 
 // Hook so that Terms of Service is included in footer
 $wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'lfTOSLink';
