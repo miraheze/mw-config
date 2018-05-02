f1ab002a538342d8b83c2cdabd5f94516b1e389d
paladox <paladox@users.noreply.github.com>
2018-05-02 21:38:11 +0100

    Setup restbase for visual editor take 2

--- LocalExtensions.php
+++ LocalExtensions.php
@@ -630,12 +630,21 @@ if ( $wmgUseVariables ) {
 
 if ( $wmgUseVisualEditor ) {
 	wfLoadExtension ( 'VisualEditor' );
-	
-	$wgVirtualRestConfig['modules']['parsoid'] = array(
-		'url' => 'https://parsoid-lb.miraheze.org:443',
-		'prefix' => $wgDBname,
+
+	/*$wgVirtualRestConfig['modules']['parsoid'] = array(
+  		'url' => 'https://parsoid-lb.miraheze.org:443',
+  		'prefix' => $wgDBname,
+  		'forwardCookies' => true,
+ 	);*/
+
+	$wgVirtualRestConfig['modules']['restbase'] = array(
+		'url' => 'https://restbase-lb.miraheze.org',
 		'forwardCookies' => true,
-	);
+		'parsoidCompat' => false
+  	);
+
+        $wgVisualEditorRestbaseURL = 'https://$wmgHostname/api/rest_v1/page/html/';
+	$wgVisualEditorFullRestbaseURL = "https://restbase-lb.miraheze.org/";
 
 
 	if ( $wmgVisualEditorEnableDefault ) {
