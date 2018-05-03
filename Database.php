d935c85f42b248d4f281c50a7ce74499dacf47dd
MacFan4000 <paulfkeffer@gmail.com>
2018-05-03 10:55:23 -0400

    rm another use of c2

--- Database.php
+++ Database.php
@@ -142,7 +142,6 @@ $wgLBFactoryConf = array(
 	'readOnlyBySection' => array(
 	//	'DEFAULT' => '',
 	//	'c1' => 'Maintenance ongoing on the database server.',
-	//	'c2' => 'To avoid data loss, this wiki is in read-only mode.',
 	//	'c3' => 'Maintenance ongoing on the database server.',
 	),
 );
