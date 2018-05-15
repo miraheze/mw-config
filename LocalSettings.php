3047fcde50c7b0c486dce22d1706bd7bbef1fe8a
paladox <paladox@users.noreply.github.com>
2018-05-15 14:52:11 +0100

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -6181,7 +6181,7 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	if ( $wgDBname !== 'rpgbrigadewiki' ) { // Wants to opt out of global sitenotices (T1187)
 		$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>There is currently an <a href="https://meta.miraheze.org/wiki/Requests_for_Stewardship#Void.27s_Request_for_Stewardship">open Request for Stewardship</a> and proposal for <a href="https://meta.miraheze.org/wiki/Requests_for_Comment/Changes_to_CVT_group">changes to the Counter Vandalism Team's global permissions</a>. All Miraheze users are welcome to comment on both of these.</td>
+			<td>There is currently an <a href="https://meta.miraheze.org/wiki/Requests_for_Stewardship#John.27s_Request_for_Stewardship">open Request for Stewardship</a>. All Miraheze users are welcome to comment on this.</td>
 			</tr></tbody></table>
 EOF;
 	}
