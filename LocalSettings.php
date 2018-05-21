8e6756c412cff0e4c0a58035adb70ce8005d5686
paladox <paladox@users.noreply.github.com>
2018-05-21 21:37:37 +0100

    Set a site notice regarding CookieWarnning being enabled globally

--- LocalSettings.php
+++ LocalSettings.php
@@ -5687,22 +5687,22 @@ putenv( "GDFONTPATH=/usr/share/fonts/truetype/freefont" );
 // Increment this version number whenever you change the site notice
 // and don't comment it out
 $wgMajorSiteNoticeID = 15;
-$snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
+$snImportant = true; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 	 if ( !$wmgSiteNoticeOptOut || $snImportant ) {
 		$siteNotice .= <<<EOF
 		<table class="wikitable" style="text-align:center;"><tbody><tr>
-		<td>Congrats to <a href="https://meta.miraheze.org/User:John">@John</a> for becoming a steward</td>
+		<td>Due to the GDPR, we will have to enable CookieWarnning on all sites on the 25 may 2018.</td>
 		</tr></tbody></table>
 EOF;
 	 }
 	return true;
 }
-*/
+
 // Hook so that Terms of Service is included in footer
 $wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'lfTOSLink';
 function lfTOSLink( $sk, &$tpl ) {
