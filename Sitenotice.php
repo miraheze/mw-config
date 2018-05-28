8f4fcb84202c21b33af42df32de2fc0599cf6211
John Lewis <johnflewis93@gmail.com>
2018-05-28 19:33:36 +0100

    change SN for db4 service upgrade

--- Sitenotice.php
+++ Sitenotice.php
@@ -2,7 +2,7 @@
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 21;
+$wgMajorSiteNoticeID = 22;
 $snImportant = true; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
@@ -12,7 +12,7 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>If you are having issues logging into your wiki, and it has a custom domain, then please try clearing your cookies/cache. If the issue persists, then please contact us on <a href="https://phabricator.miraheze.org">phabricator</a>.</td>
+                <td>In order to apply security patches to our database server, wikis will be read-only after 20:00 UTC and wikis will be unavailable for a few minutes after this time. Apologies in advance for the inconvenience.</td>
                 </tr></tbody></table>
 EOF;
          }
