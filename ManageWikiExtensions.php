6dd9b15e0282a615ccca6b10064862ec65f70930
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2018-05-29 19:34:52 -0400

    fix htmlmetaandtitle

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -10,7 +10,7 @@ $wgManageWikiExtensions = array(
 		),
  		'htmlmetaadntitle' => array(
 			'name' => 'HTML Meta and Title',
-			'var' => 'wmgUseHTMLMetaAndTitle',
+			'var' => 'wmgUseAddHTMLMetaAndTitle',
 			'conflicts' => false,
 			'requires' => false,
 			'restricted' => false,
