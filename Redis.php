1d206d293d5b15040f290d203c15f0623cafa22a
paladox <paladox@users.noreply.github.com>
2018-05-31 16:39:18 +0100

    Set wgMainCacheType to CACHE_ACCEL

--- Redis.php
+++ Redis.php
@@ -5,7 +5,7 @@ $wgObjectCaches['redis'] = array(
 	'password' => $wmgRedisPassword,
 );
 
-$wgMainCacheType = 'redis';
+$wgMainCacheType = CACHE_ACCEL;
 $wgSessionCacheType = 'redis';
 $wgSessionsInObjectCache = true;
 
