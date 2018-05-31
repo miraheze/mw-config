7bc595776740ddc459f40aef19b2dc1fa66f706e
Yongmin Hong <revi@pobox.com>
2018-06-01 02:55:45 +0900

    Revert "Revert "Revert "Unset readonly"""
    This reverts commit 5b981870f0c891a43c6eb7e8c0e68cb0224fb0c6.

--- LocalSettings.php
+++ LocalSettings.php
@@ -566,6 +566,7 @@ $wgConf->settings = array(
 
 	'wgReadOnly' => array(
 		'default' => false,
+		'nonsensopediawiki' => 'We\'re currently importing data from Wikia. Please wait until import is done.',
 	),
 	'wgSharedDB' => array(
 		'default' => 'metawiki',
