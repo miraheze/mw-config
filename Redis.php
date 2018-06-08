5c7f9950d039512198f962e5791b81f5df9f8fc8
Southparkfan <southparkfan223@hotmail.com>
2018-06-08 23:29:05 +0200

    Use memcached as default cache backend

--- Redis.php
+++ Redis.php
@@ -5,7 +5,11 @@ $wgObjectCaches['redis'] = array(
 	'password' => $wmgRedisPassword,
 );
 
-$wgMainCacheType = 'redis';
+$wgMemCachedServers = array(
+	'127.0.0.1:11211' // 192M local memcached instance
+);
+
+$wgMainCacheType = CACHE_MEMCACHED;
 $wgSessionCacheType = 'redis';
 $wgSessionsInObjectCache = true;
 
