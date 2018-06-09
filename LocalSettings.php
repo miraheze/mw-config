79fd32e0bb49f82499c3b0e9ea4e98ddf0f96c00
kakletron <bdr1998@live.co.uk>
2018-06-09 13:38:20 +0100

    Change tapestrywiki logo

--- LocalSettings.php
+++ LocalSettings.php
@@ -5044,6 +5044,7 @@ $wgConf->settings = array(
 	),
 	'wgLogo' => array(
 		'default' => "//$wmgUploadHostname/metawiki/3/35/Miraheze_Logo.svg",
+		'tapestrywiki' => "//$wmgUploadHostname/tapestrywiki/d/dd/Tapestrylogologo.png",
 	),
 
 	// TemplateSandbox
