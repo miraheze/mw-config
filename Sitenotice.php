95b42fffd035ca9f290cf4370599219e26005572
paladox <paladox@users.noreply.github.com>
2018-06-17 19:20:33 +0100

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -12,7 +12,7 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>At 22:00 UTC Miraheze will upgrade its wikis to MediaWiki 1.31. We will set all wikis will be put into read-only mode by 21:30 UTC, so be sure to save your edits before then.</td>
+                <td>At <a href="https://www.timeanddate.com/worldclock/fixedtime.html?msg=Miraheze+upgrades+to+MW+1.31%21&iso=20180617T22&p1=1440">22:00 UTC</a> Miraheze will upgrade its wikis to MediaWiki 1.31. We will set all wikis into read-only mode by 21:30 UTC, so be sure to save your edits before then.</td>
                 </tr></tbody></table>
 EOF;
          }
