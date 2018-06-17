4c9e0f779235b5ca9b7a829cdfbf0223455ac0c5
paladox <paladox@users.noreply.github.com>
2018-06-17 18:56:26 +0100

    set read only site notice (#2260)
    * set read only site notice
    
    * Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -6,15 +6,15 @@ $wgMajorSiteNoticeID = 22;
 $snImportant = true; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>In order to apply security patches to our database server, wikis will be read-only after 20:00 UTC and wikis will be unavailable for a few minutes after this time. Apologies in advance for the inconvenience.</td>
+                <td>At 22:00 UTC Miraheze will upgrade its wikis to MediaWiki 1.31. During this time all wikis will be put into read-only mode, so be sure to save your edits before 20:30 UTC when the read-only will be set.</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
-}*/
+}
