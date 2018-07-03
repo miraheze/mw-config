3198d42589450bd186a05da223174385c100d621
Reception123 <utilizator.receptie123@gmail.com>
2018-07-03 17:56:08 +0200

    add astrapedia.ru to whitelist

--- LocalSettings.php
+++ LocalSettings.php
@@ -5514,6 +5514,7 @@ $wgConf->settings = array(
 			'allthetropes\.org',
 			'aman\.info\.tm',
 			'antiguabarbudacalypso\.com',
+			'astrapedia\.ru'
 			'athenapedia\.org',
 			'b1tes\.org',
 			'bconnected\.aidanmarkham\.com',
