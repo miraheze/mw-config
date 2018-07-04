8db21289500287714495b79fc69fdc88acaaa0de
Paladox <thomasmulhall410@yahoo.com>
2018-07-04 02:51:53 +0100

    Update site notice

--- Sitenotice.php
+++ Sitenotice.php
@@ -12,7 +12,7 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>We have now switched to swift. If you notice your images are not working please report it to us either on phabricator or irc. We appologize for any inconvenience.</td>
+                <td>We have switched back to nfs due to complications with swift, which we are fixing. We are running low on disk space which is why we are moving to swift. We appologize for any inconvenience.</td>
                 </tr></tbody></table>
 EOF;
          }
