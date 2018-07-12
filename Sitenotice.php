aa8b67c47718a960133dcf9e062f32f8ac1105de
paladox <paladox@users.noreply.github.com>
2018-07-12 23:41:33 +0100

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -6,16 +6,15 @@ $wgMajorSiteNoticeID = 25;
 $snImportant = true; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>We apologize for any inconvenience caused with images not showing in the last few days. We have added our second node and images should be working now.</td>
+                <td>We are aware that images are itermittently showing. We are looking into this. We apologize for any inconvenience caused by this..</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
 }
-*/
