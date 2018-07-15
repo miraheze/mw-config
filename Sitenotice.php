d77b2a43d5f6dba9e40efdb3f9c9b3531942944a
Paladox <thomasmulhall410@yahoo.com>
2018-07-16 00:39:20 +0100

    Revert "Set site notice for db maintenance"
    This reverts commit d428cd9cc5ffe6f51a803cc58038e6d49cd0e531.

--- Sitenotice.php
+++ Sitenotice.php
@@ -6,15 +6,15 @@ $wgMajorSiteNoticeID = 25;
 $snImportant = true; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>We will be doing some db server maintenance at 23:00 UTC to 00:00 UTC. We will set wiki's into read only mode at 22:55 UTC. We apologize for any inconvenience caused by this.</td>
+                <td>We are aware that images are itermittently showing. We are looking into this. We apologize for any inconvenience caused by this..</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
-}
+}*/
