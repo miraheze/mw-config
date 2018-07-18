f4c1976da8dde7702255b59df0abef7e569c1add
paladox <paladox@users.noreply.github.com>
2018-07-18 23:35:10 +0100

    Image maint

--- Sitenotice.php
+++ Sitenotice.php
@@ -6,15 +6,15 @@ $wgMajorSiteNoticeID = 25;
 $snImportant = true; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>We are aware that images are itermittently showing. We are looking into this. We apologize for any inconvenience caused by this..</td>
+                <td>We will be doing image maintenance at 23:00 UTC. You may notice images not apearing. We will be disabling uploads to allow us to move to different software for our file store. We apologize for any inconvenience caused by this.</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
-}*/
+}
