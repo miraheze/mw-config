09f5977463be4d5a4b283bd056d622554cd1f0a1
paladox <paladox@users.noreply.github.com>
2018-07-24 15:27:46 +0100

    Add wgServer to ManageWiki

--- ManageWiki.php
+++ ManageWiki.php
@@ -1059,6 +1059,13 @@ $wgManageWikiSettings = array(
 		'overridedefault' => null,
                 'help' => 'The logo should be smaller than 135x135 in order for it to display properly. Format example: //static.miraheze.org/metawiki//3/35/Miraheze_Logo.svg',
 	),
+	'wgServer' => array(
+		'name' => 'Custom Domain',
+		'requires' => 'sysadmin assistance',
+		'type' => 'text',
+		'overridedefault' => null,
+                'help' => 'This sets your custom domain. Otherwise it defaults to submdomain.miraheze.org.',
+	),
 	'wmgWikiLicense' => array(
 		'name' => 'Content License',
 		'requires' => false,
