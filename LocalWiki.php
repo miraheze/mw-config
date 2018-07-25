659a5bbc0f8b244825335b7496245a1f4cc57a36
paladox <paladox@users.noreply.github.com>
2018-07-25 16:08:46 +0100

    Update LocalSettings.php (#2296)
    * Update LocalSettings.php
    
    * Update LocalWiki.php
    
    * Update LocalWiki.php

--- LocalWiki.php
+++ LocalWiki.php
@@ -343,7 +343,12 @@ if ( $wgDBname === 'weatherwiki' ) {
  	$wgAddGroups['bureaucrat'] = array();
  	$wgRemoveGroups['bureaucrat'] = array();
  	unset( $wgGroupPermissions['oversight'] ); // grandfathered into the local "steward" group
-	unset( $wgGroupPermissions['checkuser'] ); // grandfathered into the local "steward" group
+
+        $wgExtensionFunctions[] = function() {
+                if ( array_key_exists( 'checkuser', $GLOBALS['wgGroupPermissions'] ) ) {
+                        unset( $GLOBALS['wgGroupPermissions']['checkuser'] );
+                }
+        };
 }
 
 if ( $wgDBname === 'zhdelwiki' ) {
