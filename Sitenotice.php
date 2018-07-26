13a9a751adefb7b2e12d0016e3e7e4146792de5b
Reception123 <utilizator.receptie123@gmail.com>
2018-07-26 09:47:20 +0200

    sitenotice for RfC and CVT reqs

--- Sitenotice.php
+++ Sitenotice.php
@@ -2,19 +2,19 @@
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 25;
-$snImportant = true; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
+$wgMajorSiteNoticeID = 26;
+$snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>We have re enabled uploads after migrating to a more secure and more stable file storage. We apologize for any inconvenience caused by this maintenance.</td>
+                <td><a href="https://meta.miraheze.org/wiki/Requests_for_Comment/Interwiki_links_editing">a Request for Comment</a> regarding interwiki editing, as well as <a href="https://meta.miraheze.org/wiki/Requests for global rights">two requests for CVT</a> All users are invited to comment.</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
-}*/
+}
