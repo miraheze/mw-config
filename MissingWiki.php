3c0178a95987822c2a85a296a47e3d505da51390
MacFan4000 <paulfkeffer@gmail.com>
2018-08-14 16:04:24 -0400

    redesign (#2375)
    * redesign
    
    * Convert to tabs from spaces
    
    * Update MissingWiki.php
    
    * Create font-awesome.min.css
    
    * Create font-awesome.min.css
    
    * Remove css folder
    
    * Update MissingWiki.php
    
    * Update MissingWiki.php
    
    * Minor tweaks/fixes
    
    * tweak
    
    * Update MissingWiki.php
    
    * Update MissingWiki.php

--- MissingWiki.php
+++ MissingWiki.php
@@ -1,78 +1,89 @@
 <?php
 
 if ( !in_array( $wgDBname, $wgLocalDatabases ) && !$wgCommandLineMode ) {
-    header( "HTTP/1.1 404 Not Found" );
-    $requestURL = htmlspecialchars( $_SERVER['REQUEST_URI'] );
-    date_default_timezone_set( 'UTC' ); // Set to UTC +0
-    $fullTimestamp = date( 'Y-m-d H:i:s' );
-    echo <<<EOF
-    <html>
-        <head>
-            <style type="text/css">
-                html, body {
-                    height: 100%;
-                    margin: 0;
-                    padding: 0;
-                    font-family: sans-serif;
-                }
-                html {
-                    font-size: 100%;
-                }
-                body {
-                    background-color: hsl(0, 0%, 96%);
-                }
-                h1, h2 {
-                    margin-bottom: .6em !important;
-                }
-                h1 {
-                    font-size: 188%;
-                }
-                h1, h2, h3, h4, h5, h6 {
-                    color: hsl(0, 0%, 0%);
-                    background: none;
-                    font-weight: normal;
-                    margin: 0;
-                    overflow: hidden;
-                    padding-top: .5em;
-                    padding-bottom: .17em;
-                    border-bottom: 1px solid hsl(0, 0%, 67%);
-                }
-                p {
-                    margin: .4em 0 .5em 0;
-                }
-            </style>
-        </head>
-        <body>
-            <div style="text-align: center;">
-                <h1>404 Wiki Not Found</h1>
-                <p>The wiki you wanted to visit does not exist. Please be sure you typed the URL correctly.</p>
-                <p>You can find all of our existing wikis <a href="https://meta.miraheze.org/wiki/Special:WikiDiscover">here</a>.</p>
-                <p>If you think there's a technical problem, please <a href="https://meta.miraheze.org/wiki/Help_center">contact the system administrators</a> and provide the following details:</p>
-                <p style="align:center;">
-                    <table style="font-style: italic; font-size: 14px;" align="center">
-                        <tbody>
-                            <tr>
-                                <td>Host: {$_SERVER['HTTP_HOST']}</td>
-                                <td>Visitor IP: {$_SERVER['REMOTE_ADDR']}</td>
-                            </tr>
-                            <tr>
-                                <td>Request URL: {$requestURL}</td>
-                                <td>Timestamp: {$fullTimestamp}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </p> 
-            </div>
-            <div style="float: right; padding-right: 1em;">
-                <a href="https://meta.miraheze.org/wiki/Miraheze">
-                    <img src="https://static.miraheze.org/metawiki/7/7e/Powered_by_Miraheze.png" alt="Powered by Miraheze" />
-                </a>
-            </div>
-        </body>
-    </html>
+	header( "HTTP/1.1 404 Not Found" );
+	$requestURL = htmlspecialchars( $_SERVER['REQUEST_URI'] );
+	date_default_timezone_set( 'UTC' ); // Set to UTC +0
+	$fullTimestamp = date( 'Y-m-d H:i:s' );
+	echo <<<EOF
+	<!DOCTYPE html>
+	<html lang="en">
+		<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Wiki not Found">
+		<title>Wiki not Found</title>
+		<!-- Bootstrap core CSS -->
+		<link rel="stylesheet" href="https://static.miraheze.org/static/css/bootstrap.min.css">
+		<style>
+			/* Error Page Inline Styles */
+			body {
+			  padding-top: 20px;
+			}
+			/* Layout */
+			.jumbotron {
+			  font-size: 21px;
+			  font-weight: 200;
+			  line-height: 2.1428571435;
+			  color: inherit;
+			  padding: 10px 0px;
+			}
+			/* Everything but the jumbotron gets side spacing for mobile-first views */
+			.masthead, .body-content {
+			  padding-left: 15px;
+			  padding-right: 15px;
+			}
+			/* Main marketing message and sign up button */
+			.jumbotron {
+			  text-align: center;
+			  background-color: transparent;
+			}
+			.jumbotron .btn {
+			  font-size: 21px;
+			  padding: 14px 24px;
+			}
+			/* Colors */
+			.green {color:#5cb85c;}
+			.orange {color:#f0ad4e;}
+			.red {color:#d9534f;}
+		</style>
+		<script>
+		  function loadDomain() {
+			var display = document.getElementById("display-domain");
+			display.innerHTML = document.domain;
+		  }
+		</script>
+		</head>
+		<div class="container">
+		  <!-- Jumbotron -->
+		  <div class="jumbotron">
+			<h1><img src="https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg" alt="Miraheze Logo"> 404 Wiki not Found</h1>
+			<p class="lead">We couldn't find the wiki you were looking for on our servers.</p>
+			<p><a onclick=javascript:checkSite(); class="btn btn-default btn-lg"><span class="green">Take Me To The Homepage</span></a>
+				<script>
+					function checkSite(){
+						var currentSite = window.location.hostname;
+						window.location = "https://meta.miraheze.org";
+					}
+				</script>
+			</p>
+		  </div>
+		</div>
+		<div class="container">
+		  <div class="body-content">
+			<div style="text-align:center;">
+				<h2>What can I do?</h2>
+				<p class="lead">If you're a wiki visitor:</p>
+				<p>This wiki does not exist on our servers. You can browse wikis on our network <a href="//meta.miraheze.org/wiki/Special:WikiDiscover">here</a></p>
+				<p class="lead">If you're the wiki bureaucrat:</p>
+				<p>If this is a problem, please <a href="https://meta.miraheze.org/wiki/Help_center">contact us</a></p>
+			</div>
+		  </div>
+		</div>
+	</html>
 EOF;
-    die( 1 );
+	die( 1 );
 } elseif ( !in_array( $wgDBname, $wgLocalDatabases ) && $wgCommandLineMode ) {
-    // $wgDBname will always be set to a string, even if the --wiki parameter was not passed to a script.
-    echo "The wiki database '$wgDBname' was not found." . PHP_EOL;
+	// $wgDBname will always be set to a string, even if the --wiki parameter was not passed to a script.
+	echo "The wiki database '$wgDBname' was not found." . PHP_EOL;
 }
