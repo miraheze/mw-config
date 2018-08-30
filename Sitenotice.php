addf09fd6195b5b3b21553e56eda50e10292c0dd
Reception123 <utilizator.receptie123@gmail.com>
2018-08-30 21:49:09 +0200

    rfc sitenotice

--- Sitenotice.php
+++ Sitenotice.php
@@ -6,16 +6,16 @@ $wgMajorSiteNoticeID = 26;
 $snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>Please note that there is a <a href="https://meta.miraheze.org/wiki/Requests_for_Comment/Interwiki_links_editing">a Request for Comment</a> regarding interwiki editing, as well as <a href="https://meta.miraheze.org/wiki/Requests for global rights">two requests for CVT</a>. All users are invited to comment.</td>
+                <td>Please note that there is currently a <a href="https://meta.miraheze.org/wiki/Requests_for_Comment/Future_of_Wikicreators">a Request for Comment</a> regarding the future of wiki creators. All users are invited to comment.</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
 }
-*/
+
