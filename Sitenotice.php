89b1684d0162eabe59ab3ae0bc0fda100a258d9c
MacFan4000 <paulfkeffer@gmail.com>
2018-09-12 08:00:52 -0400

    Add rfs notice

--- Sitenotice.php
+++ Sitenotice.php
@@ -6,16 +6,15 @@ $wgMajorSiteNoticeID = 26;
 $snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>Please note that there is currently <a href="https://meta.miraheze.org/wiki/Requests_for_Comment/Future_of_Wikicreators">a Request for Comment</a> regarding the future of wiki creators. All users are invited to comment.</td>
+                <td>Please note that there is currently <a href="https://meta.miraheze.org/wiki/Requests_for_Stewardship#MacFan4000.27s_Request_for_Stewardship">a Request for Stewardship</a>.</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
 }
-*/
