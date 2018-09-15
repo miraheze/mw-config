c4df1447cafd59eaa28d11940d5d1c615adb022b
paladox <paladox@users.noreply.github.com>
2018-09-16 00:29:12 +0100

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -6,16 +6,15 @@ $wgMajorSiteNoticeID = 26;
 $snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>Please note that there is currently <a href="https://meta.miraheze.org/wiki/Requests_for_Stewardship#MacFan4000.27s_Request_for_Stewardship">a Request for Stewardship</a>.</td>
+                <td>Please note we will be doing database maintenance at 23:35 UTC. Please save your edits 5 minutes before! We anticipate the maintenance to last 1 hour.</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
 }
-*/
