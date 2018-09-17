5ffe42bbec35bf394ba32011e79405345033ab5a
bonnedav <theD_2011@hotmail.com>
2018-09-17 09:58:49 -0600

    re-add autocreateaccount per @Amanda-Catherine

--- LocalSettings.php
+++ LocalSettings.php
@@ -2098,6 +2098,7 @@ $wgConf->settings = array(
 			'abusefilter-private',
 			'abusefilter-private-log',
 			'aft-oversighter',
+			'autocreateaccount',
 			'centralauth-lock',
 			'centralauth-oversight',
 			'centralauth-rename',
