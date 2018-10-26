e896eb1aeeb8c8ec0f637ea215bcbbfc6a41e163
MacFan4000 <paulfkeffer@gmail.com>
2018-10-25 22:37:05 -0400

    Add notice

--- Sitenotice.php
+++ Sitenotice.php
@@ -2,19 +2,19 @@
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 26;
-$snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
+$wgMajorSiteNoticeID = 27;
+$snImportant = true; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>Please note we will be doing database maintenance at 23:35 UTC. Please save your edits 5 minutes before! We anticipate the maintenance to last 1 hour.</td>
+                <td>We are aware of the current permissions issues and are working to fix them as soon as possible.</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
-}*/
+}
