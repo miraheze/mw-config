128d2eb70048588d63cb964c57f6d030e4b60b0c
paladox <paladox@users.noreply.github.com>
2018-11-12 22:00:00 +0000

    Revert "Revert "Set all wikis to write to actor (no longer read and write to both) (#2553)" (#2554)" (#2555)
    This reverts commit 21c0561f7d68897db57444280c9f4208641cd519.

--- LocalSettings.php
+++ LocalSettings.php
@@ -708,7 +708,7 @@ $wgConf->settings = array(
 	// TODO use 'SCHEMA_COMPAT_WRITE_NEW | SCHEMA_COMPAT_READ_NEW'
 	// when upgrading to mw 1.32
 	'wgActorTableSchemaMigrationStage' => array(
-		'default' => MIGRATION_WRITE_BOTH,
+		'default' => MIGRATION_NEW,
 		'metawiki' => MIGRATION_NEW,
 		'test1wiki' => SCHEMA_COMPAT_WRITE_NEW | SCHEMA_COMPAT_READ_NEW,
 	),
