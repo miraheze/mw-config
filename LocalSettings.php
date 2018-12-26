5635d93b02efeb66ffc57e447cbf5ca5eac6b556
Paladox <thomasmulhall410@yahoo.com>
2018-12-26 23:59:32 +0000

    Revert "Revert "set ktswiki to inkscape in wgSVGConverter""
    This reverts commit 2f360e0af89337aafcc7ae46ffe60ddffe583e66.

--- LocalSettings.php
+++ LocalSettings.php
@@ -1597,6 +1597,7 @@ $wgConf->settings = array(
 	'wgSVGConverter' => array(
 		'default' => 'ImageMagick',
 		'arphilosophywiki' => 'inkscape',
+		'ktswiki' => 'inkscape',
 	),
 
 	// Flow [MWCandidate] (MWNamespaces?)
