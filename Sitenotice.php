7f8ee98cf628fe9f3fd2a6fead859f4436ab16ac
paladox <paladox@users.noreply.github.com>
2019-01-10 22:44:36 +0000

    Update Sitenotice.php (#2596)

--- Sitenotice.php
+++ Sitenotice.php
@@ -12,15 +12,15 @@ $wgMajorSiteNoticeID = 28;
 $snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>Please note we will be doing database maintenance at 23:35 UTC. Please save your edits 5 minutes before! We anticipate the maintenance to last 1 hour.</td>
+                <td>Miraheze will be setting all wiki's to read only at 00:00 UTC. This is so we can do a MediaWiki upgrade to 1.32!</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
-}*/
+}
