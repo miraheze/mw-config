9927aa30d97dacad9a9d871dd9bbc7383631e601
paladox <paladox@users.noreply.github.com>
2019-01-12 15:22:08 +0000

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,19 +8,19 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 29;
+$wgMajorSiteNoticeID = 30;
 $snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
         global $wmgSiteNoticeOptOut, $snImportant;
          if ( !$wmgSiteNoticeOptOut || $snImportant ) {
                 $siteNotice .= <<<EOF
                 <table class="wikitable" style="text-align:center;"><tbody><tr>
-                <td>Miraheze will be setting all wiki's to read only at 00:00 UTC. This is so we can do a MediaWiki upgrade to 1.32!</td>
+                <td>At 15:36 UTC Miraheze will disable Uploads on its wikis to perform some maintenance on it’s fileservers. During this time you will not be able to upload files. This will take about 1-3 hours.</td>
                 </tr></tbody></table>
 EOF;
          }
         return true;
-}*/
+}
