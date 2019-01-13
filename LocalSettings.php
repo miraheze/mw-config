2f1ed9ed1f40d72d662c9b15d1e80cab6bb25e84
Amanda <amandaquad@yahoo.com>
2019-01-13 11:36:08 -0500

    Enable compressed file uploads on guiasdobrasilwiki T3995 (#2602)

--- LocalSettings.php
+++ LocalSettings.php
@@ -1575,6 +1575,7 @@ $wgConf->settings = array(
 		'+exercicesdefrancaisprodfrwiki' => array('html', 'htm' ),
 		'+exitsincwiki' => array('txt' ),
 		'+fawiki' => array('ttf', 'eot', 'woff', 'apk'),
+		'+guiasdobrasilwiki' => array('zip', 'tar', 'tar.gz', 'rar', ),
 		'+hypernostalgiawiki' => array('mp4', 'webm', 'ogv', 'mp3'),
 		'+indrikwiki' => array('mp3', 'mus', 'mid' ),
 		'+jadtechwiki' => array('png', 'bmp', 'gif', 'ico', 'ogg', 'mp3', 'svg', 'pdf', 'flac', 'mp4', 'exe', 'zip', 'jpeg', 'jpg'),
