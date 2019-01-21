f3f7912547d073118542439595877abaff431683
John Lewis <johnflewis93@gmail.com>
2019-01-21 15:17:59 +0000

    SN

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,21 +8,17 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 29;
-$snImportant = false; // Set to true if the sitenotice should be show regardless of if wikis want it to be shown
+$wgMajorSiteNoticeID = 30;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
-	
-	if ( !$wmgSiteNoticeOptOut || $snImportant ) {
-		$siteNotice .= <<<EOF
-				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze will be setting all wiki's to read only at 00:00 UTC. This is so we can do a MediaWiki upgrade to 1.32!</td>
-				</tr></tbody></table>
+
+	$siteNotice .= <<<EOF
+			<table class="wikitable" style="text-align:center;"><tbody><tr>
+			<td>Miraheze is aware of an on-going outage. We are trying our best to mitigate and investigate.</td>
+			</tr></tbody></table>
 EOF;
-	 }
-	
 	return true;
-}*/
+}
