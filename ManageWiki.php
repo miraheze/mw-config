0d831a097bb55e924bf8177e1ec95faca115746b
paladox <paladox@users.noreply.github.com>
2019-02-21 02:23:46 +0000

    Update ManageWiki.php

--- ManageWiki.php
+++ ManageWiki.php
@@ -2081,7 +2081,7 @@ $wgManageWikiSettings = [
 		'type' => 'check',
 		'overridedefault' => true,
 		'section' => 'media',
-		'help' => 'Whether should old thumbnails be displayed on the image\'s detail page.',
+		'help' => 'Whether to show old thumbnails be displayed on the image\'s description page.',
 	],
 	'wgMediaViewerIsInBeta' => [
 		'name' => 'Enable Media Viewer Beta Mode',
