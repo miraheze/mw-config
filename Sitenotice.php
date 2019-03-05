cce9310b5c8aabbc43b43ef4f067100beae508e0
paladox <paladox@users.noreply.github.com>
2019-03-05 23:34:09 +0000

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -11,14 +11,14 @@ if ( $wmgSiteNoticeOptOut ) {
 $wgMajorSiteNoticeID = 30;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze is aware of an on-going outage. We are trying our best to mitigate and investigate.</td>
+			<td>If you use a *.miraheze.org domain you can ignore this. If you use a custom domain then please read on. Due to a fault on our side a private key associated with your custom domain may have been compromised. This private key allows users to intercept any traffic related to your wiki which could result in compromised user accounts.</td>
 			</tr></tbody></table>
 EOF;
 	return true;
-}*/
+}
