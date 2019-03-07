f6e03d216b3bde9f62d77dbcb1d5ce57b8d7def9
paladox <paladox@users.noreply.github.com>
2019-03-07 17:21:34 +0000

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -11,14 +11,14 @@ if ( $wmgSiteNoticeOptOut ) {
 $wgMajorSiteNoticeID = 31;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>If you use a *.miraheze.org domain or a domain that dosen't use letsencrypt you can ignore this. If you use a custom domain then please read on. Due to a fault on our side a private key associated with your custom domain may have been compromised. This private key allows users to intercept any traffic related to your wiki which could result in compromised user accounts. We are currently generating new certificates for all custom domains.</td>
+			<td></td>
 			</tr></tbody></table>
 EOF;
 	return true;
-}
+}*/
