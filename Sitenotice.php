c46a717aaff861f3458d6f1ac8a62115ba017804
Southparkfan <southparkfan223@hotmail.com>
2019-03-13 00:16:01 +0100

    Prepare Meta for CirrusSearch migration (1/2)

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,7 +8,7 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 31;
+$wgMajorSiteNoticeID = 32;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 /*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
@@ -22,3 +22,16 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
 EOF;
 	return true;
 }*/
+
+if ( $wgDBname === 'metawiki' ) {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+
+		$siteNotice .= <<<EOF
+			<table class="wikitable" style="text-align:center;"><tbody><tr>
+			<td>Miraheze is currently performing maintenance that might affect the search functionality on Meta. We are sorry for the inconvenience.</td>
+			</tr></tbody></table>
+EOF;
+		return true;
+	}
+}
