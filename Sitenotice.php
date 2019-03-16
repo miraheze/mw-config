b45a0180e979bd8b55603365da8dd329ae771c5f
paladox <paladox@users.noreply.github.com>
2019-03-16 01:50:37 +0000

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -13,18 +13,11 @@ $wgMajorSiteNoticeID = 34;
 // Write your SiteNotice below.  Comment out this section to disable.
 $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
-	global $wmgSiteNoticeOptOut, $snImportant, $wgDBname;
+	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
 			<td>Miraheze is currently low on storage for uploads. Because of this most upload attempts will fail. We apologize for this.</td>
-			</tr></tbody></table>
-EOF;
-
-	if ( $wgDBname === 'allthetropeswiki' ) {
-		$siteNotice .= <<<EOF
-			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze is currently performing maintenance that might affect the search functionality on this wiki. We are sorry for the inconvenience.</td>
 			</tr></tbody></table>
 EOF;
 	}
