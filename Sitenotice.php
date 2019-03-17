e5f33a4bc4b875385d99c495dd643563f5bfb77d
Paladox <thomasmulhall410@yahoo.com>
2019-03-17 19:58:21 +0000

    Remove site notice
    Change-Id: I45027ca7808d504715d71e26ec1ae8d8502e115b

--- Sitenotice.php
+++ Sitenotice.php
@@ -11,15 +11,15 @@ if ( $wmgSiteNoticeOptOut ) {
 $wgMajorSiteNoticeID = 34;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze is currently low on storage for uploads. Because of this most upload attempts will fail. We apologize for this.</td>
+			<td></td>
 			</tr></tbody></table>
 EOF;
 
 	return true;
-}
+}*/
