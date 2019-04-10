f1838474d47a27086faaf3696bd7a273310ab165
paladox <paladox@users.noreply.github.com>
2019-04-10 16:50:39 +0100

    Move wgAccountCreationThrottle to ManageWiki (#2661)
    * Move wgAccountCreationThrottle to ManageWiki
    
    * Update ManageWiki.php
    
    * Update ManageWiki.php

--- ManageWiki.php
+++ ManageWiki.php
@@ -2124,6 +2124,17 @@ $wgManageWikiSettings = [
 		'section' => 'anti-spam',
 		'help' => 'Number of seconds before autoblock entries expire. Minmum value allowed is 0 where as maxmium is 10 years (315360000).',
 	],
+	'wgAccountCreationThrottle' => [
+		'name' => 'Account Creation Throttle',
+		'from' => 'mediawiki',
+		'restricted' => false,
+		'type' => 'integer',
+		'minint' => 0,
+		'maxint' => 900000000,
+		'overridedefault' => 5,
+		'section' => 'anti-spam',
+		'help' => 'Number of accounts each IP address may create, 0 to disable.',
+	],
 
 	// Media/File
 	'wgEnableUploads' => [
