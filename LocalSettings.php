a4ce8c2bd6f8d92320a8aeb9a6e52690185fdcd6
thomasmulhall410 <thomasmulhall410@yahoo.com>
2019-05-04 17:15:20 +0000

    Split up ManageWiki.php
    This moves extensions/skins config to ManageWikiExtensionsAndSkins.php,
    it also moves settings to ManageWikiSettings.php.
    
    The reason why i'm splitting this is so that it's easier to edit the file,
    the file is becoming too large.
    
    Change-Id: Ia0f90a66d94dcb17ec363b1255eaf0b93553178e

--- LocalSettings.php
+++ LocalSettings.php
@@ -3746,7 +3746,8 @@ $wgLocalDatabases = [];
 $wmgDatabaseList = file( "/srv/mediawiki/dblist/all.dblist" );
 
 // ManageWiki settings
-require_once "/srv/mediawiki/config/ManageWiki.php";
+require_once __DIR__ . "/ManageWikiExtensions.php";
+require_once __DIR__ . "/ManageWikiSettings.php";
 
 foreach ( $wmgDatabaseList as $wikiLine ) {
 	$wikiDB = explode( '|', $wikiLine, 6 );
