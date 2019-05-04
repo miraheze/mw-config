2c06a26ca76e1d777502d4674d0809fe79e62f4c
Southparkfan <southparkfan223@hotmail.com>
2019-05-04 23:40:57 +0200

    Sitenotice for ES server maintenance

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,18 +8,18 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 34;
+$wgMajorSiteNoticeID = 35;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td></td>
+			<td>Miraheze will perform maintenance related to the search functionality of this wiki at 21:50 UTC. The maintenance will take about 5 minutes. During this maintenance window you may not be able to use the search box.</td>
 			</tr></tbody></table>
 EOF;
 
 	return true;
-}*/
+}
