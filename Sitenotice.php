1efb25148f25c8d6f805951a1cf9c7cbe4436cb5
Southparkfan <southparkfan223@hotmail.com>
2019-05-05 00:21:56 +0200

    Maintenance window for Elasticsearch

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,20 +8,20 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 35;
+$wgMajorSiteNoticeID = 36;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*
-$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-function onSiteNoticeAfter( &$siteNotice, $skin ) {
-	global $wmgSiteNoticeOptOut, $snImportant;
+if ( $wmgSearchType ) {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+		global $wmgSiteNoticeOptOut, $snImportant;
 
-	$siteNotice .= <<<EOF
-			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze will perform maintenance related to the search functionality of this wiki at 21:50 UTC. The maintenance will take about 5 minutes. During this maintenance window you may not be able to use the search box.</td>
-			</tr></tbody></table>
+		$siteNotice .= <<<EOF
+				<table class="wikitable" style="text-align:center;"><tbody><tr>
+				<td>Miraheze will perform maintenance related to the search functionality of this wiki at 22:40 UTC. The maintenance will take about 30 minutes. During this maintenance window you may not be able to use the search box.</td>
+				</tr></tbody></table>
 EOF;
 
-	return true;
+		return true;
+	}
 }
-*/
