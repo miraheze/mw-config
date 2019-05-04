1f1f8310da93606471c053a3054f4498daaf3bd4
Southparkfan <southparkfan223@hotmail.com>
2019-05-04 23:57:14 +0200

    Remove sitenotice

--- Sitenotice.php
+++ Sitenotice.php
@@ -11,17 +11,17 @@ if ( $wmgSiteNoticeOptOut ) {
 $wgMajorSiteNoticeID = 35;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-if ( $wmgSearchType ) {
-	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-	function onSiteNoticeAfter( &$siteNotice, $skin ) {
-		global $wmgSiteNoticeOptOut, $snImportant;
+/*
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+function onSiteNoticeAfter( &$siteNotice, $skin ) {
+	global $wmgSiteNoticeOptOut, $snImportant;
 
-		$siteNotice .= <<<EOF
-				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze will perform maintenance related to the search functionality of this wiki at 21:50 UTC. The maintenance will take about 5 minutes. During this maintenance window you may not be able to use the search box.</td>
-				</tr></tbody></table>
+	$siteNotice .= <<<EOF
+			<table class="wikitable" style="text-align:center;"><tbody><tr>
+			<td>Miraheze will perform maintenance related to the search functionality of this wiki at 21:50 UTC. The maintenance will take about 5 minutes. During this maintenance window you may not be able to use the search box.</td>
+			</tr></tbody></table>
 EOF;
 
-		return true;
-	}
+	return true;
 }
+*/
