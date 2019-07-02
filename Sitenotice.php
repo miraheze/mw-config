6f3270cbd62ffd29f9c5738e043b9c3ef9aede45
paladox <thomasmulhall410@yahoo.com>
2019-07-02 21:40:18 +0000

    Announce that Miraheze is upgrading to MediaWiki 1.33 at 12am utc
    Change-Id: If6b182d8a7ee72f795f917cd856dd0f41db42eec

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,20 +8,18 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 36;
+$wgMajorSiteNoticeID = 37;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*
 $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze will perform maintenance related to the search functionality of this wiki at 22:40 UTC. The maintenance will take about 30 minutes. During this maintenance window you may not be able to use the search box.</td>
+			<td>Miraheze is proud to announce that we will be upgrading to MediaWiki 1.33. We will begin the upgrade on Wednesday, July 3rd at 12am UTC. During this time you may still edit.</td>
 			</tr></tbody></table>
 EOF;
 
 	return true;
 }
-*/
