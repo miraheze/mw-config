129f2dae8f3f4890586a15f351d9c6bf6b8d699f
paladox <thomasmulhall410@yahoo.com>
2019-07-08 23:03:47 +0000

    Fix capitals in Sitenotice
    Change-Id: Ic54a29cee5ca29002d91ea30836acad740af22a6

--- Sitenotice.php
+++ Sitenotice.php
@@ -11,14 +11,14 @@ if ( $wmgSiteNoticeOptOut ) {
 $wgMajorSiteNoticeID = 37;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'buswiki' || $wgDBname === 'isvwiki' || $wgDBname === 'metawiki' || $wgDBname === 'nonsensopediawiki' || $wgDBname === 'pointmanwiki' || $wgDBname === 'test1wiki') {
+if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'buswiki' || $wgDBname === 'isvwiki' || $wgDBname === 'metawiki' || $wgDBname === 'nonsensopediawiki' || $wgDBname === 'pointmanwiki' || $wgDBname === 'test1wiki' ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze will be updating ElasticSearch (the search index used by your wiki) at 1am UTC time tuesday.</td>
+				<td>Miraheze will be updating ElasticSearch (the search index used by this wiki) on Tuesday, 9th July at 1am UTC time.</td>
 				</tr></tbody></table>
 EOF;
 
