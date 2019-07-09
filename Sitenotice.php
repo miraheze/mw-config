3cedfc8d7417fae08ccfae0b9db6bf7314824a01
Paladox <thomasmulhall410@yahoo.com>
2019-07-09 16:02:51 +0100

    Revert "Add sitenotice for ElasticSearch update"
    This reverts commit 45b354630ff56ada0e950405278518a7b95ffd91.

--- Sitenotice.php
+++ Sitenotice.php
@@ -11,17 +11,15 @@ if ( $wmgSiteNoticeOptOut ) {
 $wgMajorSiteNoticeID = 37;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'buswiki' || $wgDBname === 'isvwiki' || $wgDBname === 'metawiki' || $wgDBname === 'nonsensopediawiki' || $wgDBname === 'pointmanwiki' || $wgDBname === 'test1wiki') {
-	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-	function onSiteNoticeAfter( &$siteNotice, $skin ) {
-		global $wmgSiteNoticeOptOut, $snImportant;
+/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+function onSiteNoticeAfter( &$siteNotice, $skin ) {
+	global $wmgSiteNoticeOptOut, $snImportant;
 
-		$siteNotice .= <<<EOF
-				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze will be updating ElasticSearch (the search index used by your wiki) at 1am UTC time tuesday.</td>
-				</tr></tbody></table>
+	$siteNotice .= <<<EOF
+			<table class="wikitable" style="text-align:center;"><tbody><tr>
+			<td>Miraheze is proud to announce that we will be upgrading to MediaWiki 1.33. We will begin the upgrade on Wednesday, July 3rd at 12am UTC. During this time you may still edit.</td>
+			</tr></tbody></table>
 EOF;
 
-		return true;
-	}
-}
+	return true;
+}*/
