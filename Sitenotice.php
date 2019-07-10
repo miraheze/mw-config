97678dc924ee4579a356eb5a412177eec99d9c60
paladox <thomasmulhall410@yahoo.com>
2019-07-10 22:32:13 +0000

    Sitenotice for the enabling of ElasticSearch on all wikis
    Change-Id: Ib76b008226d7cd6c6d4bcb9ce2215f79cc306fa3

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,18 +8,30 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 37;
+$wgMajorSiteNoticeID = 38;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-function onSiteNoticeAfter( &$siteNotice, $skin ) {
-	global $wmgSiteNoticeOptOut, $snImportant;
+$val = [
+	'allthetropeswiki',
+	'buswiki',
+	'isvwiki',
+	'metawiki',
+	'nonsensopediawiki',
+	'pointmanwiki',
+	'test1wiki',
+];
 
-	$siteNotice .= <<<EOF
-			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze is proud to announce that we will be upgrading to MediaWiki 1.33. We will begin the upgrade on Wednesday, July 3rd at 12am UTC. During this time you may still edit.</td>
-			</tr></tbody></table>
+if ( !in_array( $wgDBname, $val ) ) {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+		global $wmgSiteNoticeOptOut, $snImportant;
+
+		$siteNotice .= <<<EOF
+				<table class="wikitable" style="text-align:center;"><tbody><tr>
+				<td>Miraheze will be migrating this wiki to use ElasticSearch. We plan to start the migration on Thursday, 11th July at 23:00 UTC.</td>
+				</tr></tbody></table>
 EOF;
 
-	return true;
-}*/
+		return true;
+	}
+}
