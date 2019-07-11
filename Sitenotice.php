8ace7e1ff8625c7d4a3cb9a02c155201a8bed222
paladox <thomasmulhall410@yahoo.com>
2019-07-11 21:21:44 +0000

    extend maintenance window
    Change-Id: Ib2ac6a9626f62cf6b1fd58a295f7aaa2eabb12d5

--- Sitenotice.php
+++ Sitenotice.php
@@ -28,7 +28,7 @@ if ( in_array( $wgDBname, $val ) ) {
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze will be doing ElasticSearch maintenance at 21:30 UTC to 22:30 UTC. During this period, searching may not work.</td>
+				<td>Miraheze will be doing ElasticSearch maintenance at 21:30 UTC to 23:30 UTC. During this period, searching may not work.</td>
 				</tr></tbody></table>
 EOF;
 
