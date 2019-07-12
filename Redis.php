d9a1de88a694c36812d5946248d754cfa6ea15bc
paladox <paladox@users.noreply.github.com>
2019-07-12 18:55:22 +0100

    Do not merge

--- Redis.php
+++ Redis.php
@@ -1,17 +1,20 @@
 <?php
-$wgObjectCaches['redis'] = [
-	'class' => 'RedisBagOStuff',
-	'servers' => [ '81.4.127.174:6379' ],
-	'password' => $wmgRedisPassword,
-	'persistent' => true,
-];
 
-/*$wgMemCachedServers = [
-	'127.0.0.1:11211'
-];*/
+if ( php_sapi_name() === 'cli' ) {
+	$wgObjectCaches['redis'] = [
+		'class' => 'RedisBagOStuff',
+		'servers' => [ '81.4.127.174:6379' ],
+		'password' => $wmgRedisPassword,
+		'persistent' => true,
+	];
 
-$wgMainCacheType = 'redis';
-$wgSessionCacheType = 'redis';
+	/*$wgMemCachedServers = [
+		'127.0.0.1:11211'
+	];*/
+
+	$wgMainCacheType = 'redis';
+	$wgSessionCacheType = 'redis';
+}
 $wgSessionsInObjectCache = true;
 
 $wgMessageCacheType = CACHE_NONE;
@@ -19,24 +22,26 @@ $wgUseLocalMessageCache = true; // may be required for $wgMessageCacheType = fal
 $wgParserCacheType = CACHE_DB;
 $wgLanguageConverterCacheType = CACHE_DB;
 
-$wgJobTypeConf['default'] = [
-	'class' => 'JobQueueRedis',
-	'redisServer' => '81.4.127.174:6379',
-	'redisConfig' => [
-		'connectTimeout' => 2,
-		'password' => $wmgRedisPassword,
-		'compression' => 'gzip',
-	],
-	'claimTTL' => 3600,
-	'daemonized' => true,
-];
+if ( php_sapi_name() === 'cli' ) {
+	$wgJobTypeConf['default'] = [
+		'class' => 'JobQueueRedis',
+		'redisServer' => '81.4.127.174:6379',
+		'redisConfig' => [
+			'connectTimeout' => 2,
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		],
+		'claimTTL' => 3600,
+		'daemonized' => true,
+	];
 
-$wgJobQueueAggregator = [
-	'class' => 'JobQueueAggregatorRedis',
-	'redisServers' => [ '81.4.127.174:6379', '81.4.127.174:6379' ], // fake misc2 as fallback
-	'redisConfig' => [
-		'connectTimeout' => 2,
-		'password' => $wmgRedisPassword,
-		'compression' => 'gzip',
-	]
-];
+	$wgJobQueueAggregator = [
+		'class' => 'JobQueueAggregatorRedis',
+		'redisServers' => [ '81.4.127.174:6379', '81.4.127.174:6379' ], // fake misc2 as fallback
+		'redisConfig' => [
+			'connectTimeout' => 2,
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		]
+	];
+}
