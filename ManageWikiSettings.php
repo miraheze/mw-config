483025d7699c75658f189274477c191b97f5062f
Amanda <amandaquad@yahoo.com>
2019-07-16 10:31:54 -0400

    Fix syntax (maybe?)

--- ManageWikiSettings.php
+++ ManageWikiSettings.php
@@ -690,14 +690,15 @@ $wgManageWikiSettings = [
 		'help' => 'Allow Anonymous (users who are not logged in) to use WikiForum',
 	],
 	'wgRCWatchCategoryMembership' => [
-		'name' => 'Treat Category Membership as a RecentChange'
+		'name' => 'Treat Category Membership as a RecentChange',
 		'from' => 'mediawiki',
 		'type' => 'check',
 		'restricted' => false,
 		'overridedefault' => false,
 		'section' => 'edit',
-		'help' => 'Shows changes to category membership as an action in Special:RecentChanges'
+		'help' => 'Shows changes to category membership as an action in Special:RecentChanges',
 	],
+	
 	// Links
 	'wgExternalLinkTarget' => [
 		'name' => 'External Link Target',
