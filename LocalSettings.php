48ebf1bfa5fcaca6aadd3408bbe66af81444303c
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-08-09 23:22:35 +0100

    Update LocalSettings.php
    fix

--- LocalSettings.php
+++ LocalSettings.php
@@ -701,15 +701,15 @@ $wgConf->settings = [
 	'wmgUseHideSection' => [
 		'default' => false,
 	],
-	$wgHideSectionImages = [
+	'wgHideSectionImages' = [
 		'default' => [
-			"show" => "https://upload.wikimedia.org/wikipedia/commons/b/bc/Vector_right_arrow_link.png ",
-			"hide" => "https://upload.wikimedia.org/wikipedia/commons/2/27/Vector_down_arrow_link.png ",
+			"show" => "https://upload.wikimedia.org/wikipedia/commons/b/bc/Vector_right_arrow_link.png",
+			"hide" => "https://upload.wikimedia.org/wikipedia/commons/2/27/Vector_down_arrow_link.png",
 			"location" => "begin" # or "end"
 		],
 		'cikansaiwiki' => [
-			"show" => "https://static.miraheze.org/cikansaiwiki/4/43/HideSectionDOWN.png ",
-			"hide" => "https://static.miraheze.org/cikansaiwiki/b/bd/HideSectionUP.png ",
+			"show" => "https://static.miraheze.org/cikansaiwiki/4/43/HideSectionDOWN.png",
+			"hide" => "https://static.miraheze.org/cikansaiwiki/b/bd/HideSectionUP.png",
 			"location" => "end"
 		],
 	],
