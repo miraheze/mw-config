49c2548c611f9d7ee0326a7b7e4f2405954d58c9
paladox <paladox@users.noreply.github.com>
2019-08-25 21:08:21 +0100

    Update ManageWikiSettings.php

--- ManageWikiSettings.php
+++ ManageWikiSettings.php
@@ -1,14 +1,5 @@
 <?php
 
-// Used by wgDefaultSkin and wgTheme
-$skinOptions = [
-	'CologneBlue' => 'cologneblue',
-	'Modern' => 'modern',
-	'MonoBook' => 'monobook',
-	'Timeless' => 'timeless',
-	'Vector' => 'vector',
-];
-
 /**
  * ManageWiki settings are added using the variable below.
  *
@@ -1233,7 +1224,13 @@ $wgManageWikiSettings = [
 		'from' => 'mediawiki',
 		'restricted' => false,
 		'type' => 'list',
-		'options' => $skinOptions,
+		'options' => [
+			'CologneBlue' => 'cologneblue',
+			'Modern' => 'modern',
+			'MonoBook' => 'monobook',
+			'Timeless' => 'timeless',
+			'Vector' => 'vector',
+		],
 		'overridedefault' => 'vector',
 		'section' => 'styling',
 		'help' => 'This change the visual interface to the selected skin for all users, however it can be changed through user\'s preferences.',
