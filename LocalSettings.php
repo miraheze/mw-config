d8a8a95e8e1570a007a0b786c8311b771b73cd31
paladox <paladox@users.noreply.github.com>
2019-08-25 14:55:42 +0100

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -2126,6 +2126,11 @@ $wgConf->settings = [
 		'default' => null,
 	],
 
+	// Used for the DisqusTag extension
+	'wgDisqusShortname' => [
+		'default' => null,
+	],
+
 	// Page Size
 	'wgMaxArticleSize' => [
 		'default' => 2048,
