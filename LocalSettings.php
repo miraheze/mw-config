a1fc1e202da7e2d69d15e8a7c1a43907732e67b8
Reception123 <utilizator.receptie123@gmail.com>
2019-09-09 19:04:54 +0200

    rm double setting

--- LocalSettings.php
+++ LocalSettings.php
@@ -2489,17 +2489,6 @@ $wgConf->settings = [
 		'default' => true,
 		'allthetropeswiki' => false,
 	],
-	
-	'wgUserProfileDisplay' => [
-		'default' => [
-			'friends' => true,
-			'foes' => true,
-		],
-		'allthetropeswiki' => [
-			'friends' => false,
-			'foes' => false,
-		],
-	],
 
 	// Statistics
 	'wgArticleCountMethod' => [
