f29568d1665fa52b1e836c2e1057a8eddac6ad6c
Paladox <thomasmulhall410@yahoo.com>
2019-09-19 18:04:58 +0100

    Revert "Revert "Move allthetropeswiki and metawiki to use the new mount point (#2762)""
    This reverts commit a51b544aecd3de0a39b18cbf8476c2b77bf22866.

--- LocalSettings.php
+++ LocalSettings.php
@@ -2946,7 +2946,12 @@ foreach ( $wmgInactiveDatabasesList as $database ) {
 }
 
 // Needs to be set AFTER $wgDBname is set to a correct value
-$wgUploadDirectory = "/mnt/mediawiki-static/$wgDBname";
+if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'metawiki') {
+	$wmgUploadHostname = 'static-temp.miraheze.org';
+	$wgUploadDirectory = "/mnt/mediawiki-static-new/$wgDBname";
+} else {
+	$wgUploadDirectory = "/mnt/mediawiki-static/$wgDBname";
+}
 $wgUploadPath = "https://static.miraheze.org/$wgDBname";
 
 $wgConf->wikis = $wgLocalDatabases;
