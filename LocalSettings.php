1e4e4dca46316e839e6ef3c8cf3e0c4f2fef1067
Southparkfan <southparkfan223@hotmail.com>
2019-09-21 11:23:23 +0200

    Disable read only for baobabarchiveswiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -3007,7 +3007,7 @@ putenv( "GDFONTPATH=/usr/share/fonts/truetype/freefont" );
 
 // Placeholder for DB migrations
 
-if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'nonciclopediawiki' || $wgDBname === 'nonsensopediawiki' || $wgDBname ==='baobabarchiveswiki') {
+if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'nonciclopediawiki' || $wgDBname === 'nonsensopediawiki' ) {
 	$wgReadOnly = 'Miraheze is conducting emergency maintenance.';
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter2';
 	function onSiteNoticeAfter2( &$siteNotice, $skin ) {
