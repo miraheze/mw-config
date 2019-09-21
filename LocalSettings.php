1e38463387f772297a3ea0d469e39e43d6bb3cec
Southparkfan <southparkfan223@hotmail.com>
2019-09-21 11:40:20 +0200

    Disable read only for nonsensopediawiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -3007,7 +3007,7 @@ putenv( "GDFONTPATH=/usr/share/fonts/truetype/freefont" );
 
 // Placeholder for DB migrations
 
-if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'nonciclopediawiki' || $wgDBname === 'nonsensopediawiki' ) {
+if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'nonciclopediawiki' ) {
 	$wgReadOnly = 'Miraheze is conducting emergency maintenance.';
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter2';
 	function onSiteNoticeAfter2( &$siteNotice, $skin ) {
