e2987b793ff3f2d2406254cc25084bdbb3f25aee
paladox <paladox@users.noreply.github.com>
2019-10-14 00:17:42 +0100

    Update LocalWiki.php

--- LocalWiki.php
+++ LocalWiki.php
@@ -283,17 +283,20 @@ if ( $wgDBname === 'gyaanipediawiki' ||
 }
 
 // Experimenting caching commons for these 10 wikis
-if ( $wgDBname === 'allthetropeswiki' ||
-	$wgDBname === 'animeshoes' ||
-	$wgDBname === 'anotheredenwiki' ||
-	$wgDBname === 'crappygameswiki' ||
-	$wgDBname === 'cwarswiki' ||
-	$wgDBname === 'fapceowiki' ||
-	$wgDBname === 'giantesswiki' ||
-	$wgDBname === 'nonciclopediawiki' ||
-	$wgDBname === 'unmusicfestwiki' ||
-	$wgDBname === 'wikiaiwiki' ||
-) {
+if ( in_array( [
+		'allthetropeswiki',
+		'animeshoes',
+		'anotheredenwiki',
+		'crappygameswiki',
+		'cwarswiki',
+		'fapceowiki',
+		'giantesswiki',
+		'nonciclopediawiki',
+		'unmusicfestwiki',
+		'wikiaiwiki'
+	],
+	$wgDBname
+) ) {
 	// Disable non cached version
 	$wgUseInstantCommons = false;
 
