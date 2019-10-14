e9ca11e2c70fcf71b0e288e1cc844c21b6c64e26
RhinosF1 <46229976+RhinosF1@users.noreply.github.com>
2019-10-14 22:02:55 +0100

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -526,7 +526,7 @@ $wgConf->settings = [
 	],
 	'wmgUseAutoCreatePage' => [
 		'default' => false,
-		'test1wiki' => false,
+		'test1wiki' => true,
 	],
 	'wmgUseBlogPage' => [
 		'default' => false,
