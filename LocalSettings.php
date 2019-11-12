ef87ebc899f8b1dec45a306d05b7a6e1bd8b24ca
paladox <paladox@users.noreply.github.com>
2019-11-12 23:22:30 +0000

    move testwiki over to new mount

--- LocalSettings.php
+++ LocalSettings.php
@@ -3014,7 +3014,7 @@ $wgUploadPath = "https://static.miraheze.org/$wgDBname";
 $wgUploadDirectory = "/mnt/mediawiki-static/$wgDBname";
 
 // Temporarily testing a new file storage system
-if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'metawiki' ) {
+if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'metawiki' || $wgDBname === 'testwiki' ) {
 	$wgUploadPath = "https://static-temp.miraheze.org/$wgDBname";
 	$wgUploadDirectory = "/mnt/mediawiki-static-new/$wgDBname";
 }
