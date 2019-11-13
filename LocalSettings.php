2b962705005b00c94fc668cce3f2450d33844c94
paladox <paladox@users.noreply.github.com>
2019-11-13 15:03:17 +0000

    Move browndustwiki to new mount

--- LocalSettings.php
+++ LocalSettings.php
@@ -3014,7 +3014,7 @@ $wgUploadPath = "https://static.miraheze.org/$wgDBname";
 $wgUploadDirectory = "/mnt/mediawiki-static/$wgDBname";
 
 // Temporarily testing a new file storage system
-if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'metawiki' || $wgDBname === 'testwiki' || $wgDBname === 'unmusicfestwiki' || $wgDBname === 'cwarswiki' || $wgDBname === 'fapceowiki' || $wgDBname === 'nonciclopediawiki' || $wgDBname === 'sidemwiki' ) {
+if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'metawiki' || $wgDBname === 'testwiki' || $wgDBname === 'unmusicfestwiki' || $wgDBname === 'cwarswiki' || $wgDBname === 'fapceowiki' || $wgDBname === 'nonciclopediawiki' || $wgDBname === 'sidemwiki' || $wgDBname === 'browndustwiki' ) {
 	$wgUploadPath = "https://static-temp.miraheze.org/$wgDBname";
 	$wgUploadDirectory = "/mnt/mediawiki-static-new/$wgDBname";
 }
