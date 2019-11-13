e7c75e9129a2c85fac2aa9fc035a4ed60eb2f2a1
paladox <paladox@users.noreply.github.com>
2019-11-13 02:06:03 +0000

    Move fapceowiki to the new mount

--- LocalSettings.php
+++ LocalSettings.php
@@ -3014,7 +3014,7 @@ $wgUploadPath = "https://static.miraheze.org/$wgDBname";
 $wgUploadDirectory = "/mnt/mediawiki-static/$wgDBname";
 
 // Temporarily testing a new file storage system
-if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'metawiki' || $wgDBname === 'testwiki' || $wgDBname === 'unmusicfestwiki' || $wgDBname === 'cwarswiki' ) {
+if ( $wgDBname === 'allthetropeswiki' || $wgDBname === 'metawiki' || $wgDBname === 'testwiki' || $wgDBname === 'unmusicfestwiki' || $wgDBname === 'cwarswiki' || $wgDBname === 'fapceowiki' ) {
 	$wgUploadPath = "https://static-temp.miraheze.org/$wgDBname";
 	$wgUploadDirectory = "/mnt/mediawiki-static-new/$wgDBname";
 }
