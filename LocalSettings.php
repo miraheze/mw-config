05e9e7936f99612432e1c71abb04f643ac782d57
Paladox <thomasmulhall410@yahoo.com>
2019-11-15 15:23:44 +0000

    Revert "Move all wikis that start with the letter j to the new mount"
    This reverts commit ea503b68442a30d26a608e0a7bf8f5f36f22cff6.

--- LocalSettings.php
+++ LocalSettings.php
@@ -3023,7 +3023,6 @@ if ( substr( $wgDBname, 0, 1 ) === 'a' ||
     substr( $wgDBname, 0, 1 ) === 'g' ||
     substr( $wgDBname, 0, 1 ) === 'h' ||
     substr( $wgDBname, 0, 1 ) === 'i' ||
-    substr( $wgDBname, 0, 1 ) === 'j' ||
     $wgDBname === 'metawiki' ||
     $wgDBname === 'testwiki' ||
     $wgDBname === 'unmusicfestwiki' ||
