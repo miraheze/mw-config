fc53881c04657689b6de6cac18bd1180011174f1
paladox <paladox@users.noreply.github.com>
2019-12-22 20:38:34 +0000

    MediaWiki 1.34 site notice

--- Sitenotice.php
+++ Sitenotice.php
@@ -12,15 +12,15 @@ $wgMajorSiteNoticeID = 40;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 
-/* $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze will be doing Server maintenance at 20:15 UTC. Please save your edit's at least 5 minutes before. we apologize for the inconvenience and short notice.</td>
+			<td>Miraheze will upgrading to MediaWiki 1.34 at 21:38 UTC! Please save your edits 5 minutes before.</td>
 			</tr></tbody></table>
 EOF;
 
 	return true;
-}*/
+}
