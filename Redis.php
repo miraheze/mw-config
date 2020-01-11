69c19bae98e911d4728a82b8a16fd02666a930e8
paladox <paladox@users.noreply.github.com>
2020-01-11 23:36:36 +0000

    Redis: Increase timeout to 3

--- Redis.php
+++ Redis.php
@@ -8,6 +8,7 @@ $wgObjectCaches['redis-local'] = [
 	'persistent' => true,
 	'loggroup' => 'redis',
 	'reportDupes' => false,
+	'connectTimeout' => 2,
 ];
 
 // misc2 (used for sessions)
@@ -18,6 +19,7 @@ $wgObjectCaches['redis-central'] = [
 	'persistent' => true,
 	'loggroup' => 'redis',
 	'reportDupes' => false,
+	'connectTimeout' => 3,
 ];
 
 /*$wgMemCachedServers = [
