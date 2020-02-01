ae45852bcb4ed8a3f321a442290d3c2589ea8ab3
paladox <paladox@users.noreply.github.com>
2020-02-01 22:13:50 +0000

    Compress altversewiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -383,6 +383,7 @@ $wgConf->settings = [
 		'default' => false,
 		'absurdopediawiki' => true,
 		'allthetropeswiki' => true,
+		'altversewiki' => true,
 		'americangirldollswiki' => true,
 		'animebathswiki' => true,
 		'baobabarchiveswiki' => true,
