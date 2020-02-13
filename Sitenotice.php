3f94c4f56595f383981009dcb62efa00547756f2
paladox <paladox@users.noreply.github.com>
2020-02-13 19:21:19 +0000

    New server migration announcement

--- Sitenotice.php
+++ Sitenotice.php
@@ -12,15 +12,15 @@ $wgMajorSiteNoticeID = 40;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze will be upgrading to MediaWiki 1.34 at 22:00 UTC! The upgrade will last 30 minutes. Please save your edits 5 minutes before.</td>
+			<td>Miraheze will be doing server maintenance from 19:00 UTC to 02:00 UTC on Friday 14th 2020.</td>
 			</tr></tbody></table>
 EOF;
 
 	return true;
-}*/
+}
