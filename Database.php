5c19f72097138f5bf7a1068bf19c344874033c00
paladox <paladox@users.noreply.github.com>
2020-02-15 04:51:44 +0000

    Unset readonly from all wikis apart from a few

--- Database.php
+++ Database.php
@@ -29,7 +29,7 @@ $wgLBFactoryConf = [
 		],
 	],
 	'readOnlyBySection' => [
-		'DEFAULT' => 'Maintenance ongoing on the database server.',
+		// 'DEFAULT' => 'Maintenance ongoing on the database server.',
 		// 'c1' => 'Maintenance ongoing on the database server.',
 	],
 ];
