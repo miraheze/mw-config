6a0c777bad10ccaeb09f2d668165a3bbc52513d4
paladox <paladox@users.noreply.github.com>
2020-02-15 19:38:30 +0000

    Update site notice

--- Sitenotice.php
+++ Sitenotice.php
@@ -18,7 +18,10 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze will be doing server maintenance from 19:00 UTC (Friday 14th 2020) to 02:00 UTC (Saturday 15th 2020). During this maintenance window some services may not work properly or operate in read-only mode. Please save your edits locally before submitting them!</td>
+			<td>This is very important! If you have performed an action (edited/created a page, uploaded content, created an account, etc. basically everything that's not reading) between 14:45 UTC and 16:30 UTC at 2020-02-15, you can't see those anymore.</td>
+			<td>For (most) public wikis, we are able to find edits, but for private wikis we cannot do that. If you have edited/uploaded during this time period something, regardless of whether the wiki is public or private, please contact us as soon as possible using our procedure. It can be found at https://phabricator.miraheze.org/maniphest/task/edit/form/15/.</td>
+			<td>Regarding the migration issues, system administrators are working on fixing remnants of the rollback. All wikis can be read and edited without issues now.
+			<td>Miraheze would like to apologise again, due to the huge complexity of this migration, things went wrong (not only technical wise but also communication wise). We are focussing on restoring full functionality, that is our highest priority now. A post-mortem is in the works and will be provided when ready</td>
 			</tr></tbody></table>
 EOF;
 
