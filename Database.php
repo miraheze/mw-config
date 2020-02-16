bad82fa174af977513d77e161ad0cec04c9d6b3e
paladox <paladox@users.noreply.github.com>
2020-02-16 15:08:30 +0000

    Database: Set DBO_DEFAULT flag

--- Database.php
+++ Database.php
@@ -29,7 +29,7 @@ $wgLBFactoryConf = [
 		'user' => $wgDBuser,
 		'password' => $wgDBpassword,
 		'type' => 'mysql',
-		'flags' => DBO_SSL | DBO_COMPRESS,
+		'flags' => DBO_DEFAULT | DBO_SSL | DBO_COMPRESS,
 		'sslCertPath' => '/etc/ssl/certs/wildcard.miraheze.org.crt',
 		'sslKeyPath' => '/etc/ssl/private/wildcard.miraheze.org.key',
 	],
