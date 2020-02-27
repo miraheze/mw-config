ffc6fe8a13c910c96942337dedc32d76769e8f7d
Reception123 <utilizator.receptie123@gmail.com>
2020-02-27 12:25:33 +0100

    simswiki readonly for db migration

--- LocalSettings.php
+++ LocalSettings.php
@@ -3160,21 +3160,21 @@ $wgExtensionEntryPointListFiles[] = "/srv/mediawiki/config/extension-list";
 putenv( "GDFONTPATH=/usr/share/fonts/truetype/freefont" );
 
 // Placeholder for DB migrations
-/*
 
-if ( $wgDBname === 'browndustwiki' ) {
+
+if ( $wgDBname === 'simswiki' ) {
 	$wgReadOnly = 'Miraheze is conducting a database migration.';
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter2';
 	function onSiteNoticeAfter2( &$siteNotice, $skin ) {
 			$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze will be performing database maintenance on this wiki from 10:00 UTC until approximately 10:10 UTC today. During this maintenance time this wiki will be put in read-only mode. Please save your edits before 10:00 UTC!</td>
+			<td>Miraheze will be performing database maintenance on this wiki from 11:30 UTC until approximately 11:40 UTC today. During this maintenance time this wiki will be put in read-only mode. Please save your edits before 11:30 UTC!</td>
 			</tr></tbody></table>
 EOF;
 		return true;
 	}
 }
-*/
+
 
 // Hook so that Terms of Service is included in footer
 $wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'lfTOSLink';
