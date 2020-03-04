a118ed9ca1b48d81c82648627102ae935f13e92e
paladox <paladox@users.noreply.github.com>
2020-03-04 22:45:20 +0000

    New infrastructure migration site notice (#2926)

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,22 +8,91 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 40;
+$wgMajorSiteNoticeID = 41;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-function onSiteNoticeAfter( &$siteNotice, $skin ) {
-	global $wmgSiteNoticeOptOut, $snImportant;
+$list = [
+	'buswiki',
+	'pathofexilewiki',
+	'tmewiki',
+	'vsrecommendedgameswiki',
+	'animatedfeetwiki',
+	'crappygameswiki',
+	'anglishwiki',
+	'trollpastawiki',
+	'poserdazfreebieswiki',
+	'nltramswiki',
+	'beidipediawiki',
+	'nilamwikiubzx217c40wiki',
+	'bluepageswiki',
+	'awfulmovieswiki',
+	'uncyclopediawiki',
+	'tolololpediawiki',
+	'platprojectwiki',
+	'trollpastawikiwiki',
+	'ansaikuropediawiki',
+	'pluspiwiki',
+	'csydeswiki',
+	'atrociousyoutuberswiki',
+	'anterrawiki',
+	'jayuvandalwiki',
+	'ciptamediawiki',
+	'bpwiki',
+	'terribletvshowswiki',
+	'osaindexwiki',
+	'newusopediawiki',
+	'mc2wiki',
+	'jawp2chwiki',
+	'sumroletaericwiki',
+	'sidemwiki',
+	'ranchstorywiki',
+	'maiasongcontestwiki',
+	'awesomegameswiki',
+	'animebathswiki',
+	'americangirldollswiki',
+	'schattenvonskeloswiki',
+	's23wiki',
+	'libertygamewiki',
+	'healthyfandomsandandhatedomwiki',
+	'gyaanipediawiki',
+	'bigforestwiki',
+	'2b2twiki',
+	'simswiki',
+	'frikipediawiki',
+	'uncyclomirrorwiki',
+	'baobabarchiveswiki',
+	'zhdelwiki',
+	'allthetropeswiki',
+	'nonciclopediawiki',
+	'toxicfandomsandhatedomswiki',
+	'nonsensopediawiki',
+];
 
-	$siteNotice .= <<<EOF
-			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>This is very important! If you have performed an action (edited/created a page, uploaded content, created an account, etc. basically everything that's not reading) between 14:45 UTC and 16:30 UTC at 2020-02-15, you can't see those anymore.
-			For (most) public wikis, we are able to find edits, but for private wikis we cannot do that. If you have edited/uploaded during this time period something, regardless of whether the wiki is public or private, please contact us as soon as possible using our procedure. It can be found at https://phabricator.miraheze.org/maniphest/task/edit/form/15/.
-			Regarding the migration issues, system administrators are working on fixing remnants of the rollback. All wikis can be read and edited without issues now.
-			Miraheze would like to apologise again, due to the huge complexity of this migration, things went wrong (not only technical wise but also communication wise). We are focussing on restoring full functionality, that is our highest priority now. A post-mortem is in the works and will be provided when ready</td>
-			</tr></tbody></table>
+if ( in_array( $wgDBname, $list ) ) {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+		global $wmgSiteNoticeOptOut, $snImportant;
+
+		$siteNotice .= <<<EOF
+				<table class="wikitable" style="text-align:center;"><tbody><tr>
+				<td>Miraheze is intending to move this wiki to new infrastructure on Friday, March 6th 2020. You will still be able to read but won’t be able to edit between the times of 19:00 UTC and 23:00 UTC.</td>
+				</tr></tbody></table>
 EOF;
 
-	return true;
-}*/
+		return true;
+	}
+} else {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+		global $wmgSiteNoticeOptOut, $snImportant;
+
+		$siteNotice .= <<<EOF
+				<table class="wikitable" style="text-align:center;"><tbody><tr>
+				<td>Miraheze is intending to move this wiki to new infrastructure on Sunday, March 8th 2020. You will still be able to read but won’t be able to edit between the times of 13:00 UTC and 23:00 UTC.</td>
+				</tr></tbody></table>
+EOF;
+
+		return true;
+	}
+}
