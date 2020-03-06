541398174f0b64a26ed1f7fd97b68ef471661a13
paladox <paladox@users.noreply.github.com>
2020-03-06 22:44:51 +0000

    Remove site notice from large wikis

--- Sitenotice.php
+++ Sitenotice.php
@@ -69,20 +69,7 @@ $list = [
 	'nonsensopediawiki',
 ];
 
-if ( in_array( $wgDBname, $list ) ) {
-	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-	function onSiteNoticeAfter( &$siteNotice, $skin ) {
-		global $wmgSiteNoticeOptOut, $snImportant;
-
-		$siteNotice .= <<<EOF
-				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze is intending to move this wiki to new infrastructure on Friday, March 6th 2020. You will still be able to read but won’t be able to edit between the times of 19:00 UTC and 23:00 UTC.</td>
-				</tr></tbody></table>
-EOF;
-
-		return true;
-	}
-} else {
+if ( !in_array( $wgDBname, $list ) ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
