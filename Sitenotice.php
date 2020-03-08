17fa72e09f9ace0527eb971b94ef6094a2ee6380
Southparkfan <southparkfan223@hotmail.com>
2020-03-09 00:34:25 +0100

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -13,70 +13,32 @@ $wgMajorSiteNoticeID = 42;
 // Write your SiteNotice below.  Comment out this section to disable.
 
 $list = [
-	'buswiki',
-	'pathofexilewiki',
-	'tmewiki',
-	'vsrecommendedgameswiki',
-	'animatedfeetwiki',
-	'crappygameswiki',
-	'anglishwiki',
-	'trollpastawiki',
-	'poserdazfreebieswiki',
-	'nltramswiki',
-	'beidipediawiki',
-	'nilamwikiubzx217c40wiki',
-	'bluepageswiki',
-	'awfulmovieswiki',
-	'uncyclopediawiki',
-	'tolololpediawiki',
-	'platprojectwiki',
-	'trollpastawikiwiki',
-	'ansaikuropediawiki',
-	'pluspiwiki',
-	'csydeswiki',
-	'atrociousyoutuberswiki',
-	'anterrawiki',
-	'jayuvandalwiki',
-	'ciptamediawiki',
-	'bpwiki',
-	'terribletvshowswiki',
-	'osaindexwiki',
-	'newusopediawiki',
-	'mc2wiki',
-	'jawp2chwiki',
-	'sumroletaericwiki',
-	'sidemwiki',
-	'ranchstorywiki',
-	'maiasongcontestwiki',
-	'awesomegameswiki',
-	'animebathswiki',
-	'americangirldollswiki',
-	'schattenvonskeloswiki',
-	's23wiki',
-	'libertygamewiki',
-	'healthyfandomsandandhatedomwiki',
-	'gyaanipediawiki',
-	'bigforestwiki',
-	'2b2twiki',
-	'simswiki',
-	'frikipediawiki',
-	'uncyclomirrorwiki',
-	'baobabarchiveswiki',
-	'zhdelwiki',
-	'allthetropeswiki',
 	'nonciclopediawiki',
 	'toxicfandomsandhatedomswiki',
 	'nonsensopediawiki',
 ];
 
-if ( !in_array( $wgDBname, $list ) ) {
+if ( in_array( $wgDBname, $list ) ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze is intending to move this wiki to new infrastructure on Sunday, March 8th 2020. You will still be able to read but won’t be able to edit between the times of 13:00 UTC and 23:00 UTC.</td>
+				<td>Miraheze is performing emergency server maintenance. This wiki will be read-only until approximately 00:30 UTC.</td>
+				</tr></tbody></table>
+EOF;
+
+		return true;
+	}
+} else {
+		$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+		global $wmgSiteNoticeOptOut, $snImportant;
+
+		$siteNotice .= <<<EOF
+				<table class="wikitable" style="text-align:center;"><tbody><tr>
+				<td>The maintenance window for the migration is over. Miraheze is sorry for the issues that popped up after the migration.</td>
 				</tr></tbody></table>
 EOF;
 
