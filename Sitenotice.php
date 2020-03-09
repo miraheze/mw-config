22600486fedc62bb232dc8ddf332530ef3c20ec9
Southparkfan <southparkfan223@hotmail.com>
2020-03-09 01:08:31 +0100

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,36 +8,18 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 42;
+$wgMajorSiteNoticeID = 43;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 
-$list = [
-	'nonsensopediawiki',
-];
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+function onSiteNoticeAfter( &$siteNotice, $skin ) {
+	global $wmgSiteNoticeOptOut, $snImportant;
 
-if ( in_array( $wgDBname, $list ) ) {
-	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-	function onSiteNoticeAfter( &$siteNotice, $skin ) {
-		global $wmgSiteNoticeOptOut, $snImportant;
-
-		$siteNotice .= <<<EOF
-				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze is performing emergency server maintenance. This wiki will be read-only until approximately 00:30 UTC.</td>
-				</tr></tbody></table>
-EOF;
-
-		return true;
-	}
-} else {
-		$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-	function onSiteNoticeAfter( &$siteNotice, $skin ) {
-		global $wmgSiteNoticeOptOut, $snImportant;
-
-		$siteNotice .= <<<EOF
-				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>The maintenance window for the migration is over. Miraheze is sorry for the issues that popped up after the migration.</td>
-				</tr></tbody></table>
+	$siteNotice .= <<<EOF
+			<table class="wikitable" style="text-align:center;"><tbody><tr>
+			<td>The maintenance window for the migration is over. Miraheze is sorry for the issues that popped up after the migration.</td>
+			</tr></tbody></table>
 EOF;
 
 		return true;
