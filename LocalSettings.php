5829bdd8a0592c1e95474b6000544add7c3682d0
paladox <paladox@users.noreply.github.com>
2020-03-25 23:24:52 +0000

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -3282,52 +3282,6 @@ if ( !defined( 'MW_NO_EXTENSION_MESSAGES' ) ) {
 	require_once "/srv/mediawiki/config/ExtensionMessageFiles.php";
 }
 
-function wmfSetTimeLimit() {
-	if ( PHP_SAPI === 'cli' ) {
-		// It should already be zero, and Maintenance.php should set it to zero
-		return;
-	}
-	$host = isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : '';
-	switch ( $host ) {
-		case 'videoscaler.svc.eqiad.wmnet':
-		case 'videoscaler.svc.codfw.wmnet':
-		case 'videoscaler.discovery.wmnet':
-			$limit = 86400;
-			break;
-
-		case 'jobrunner.svc.eqiad.wmnet':
-		case 'jobrunner.svc.codfw.wmnet':
-		case 'jobrunner.discovery.wmnet':
-			$limit = 1200;
-			break;
-
-		default:
-			if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
-				$limit = 200;
-			} else {
-				$limit = 60;
-			}
-	}
-	if ( extension_loaded( 'excimer' ) ) {
-		static $timer;
-		$timer = new ExcimerTimer;
-		$timer->setInterval( $limit );
-		$timer->setCallback( function () use ( $limit ) {
-			throw new WMFTimeoutException(
-				"the execution time limit of $limit seconds was exceeded"
-			);
-		} );
-		$timer->start();
-	} else {
-		set_time_limit( $limit );
-	}
-}
-
-class WMFTimeoutException extends RuntimeException {
-}
-
-wmfSetTimeLimit();
-
 if ( PHP_SAPI !== 'cli' ) {
 	$host = isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : '';
 	switch ( $host ) {
