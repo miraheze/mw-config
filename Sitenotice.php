9acd96556c1e47b55dfe83e86f80de02871deee1
paladox <paladox@users.noreply.github.com>
2020-03-28 17:00:26 +0000

    Set site notice for all wikis on db4 (#2953)
    * Set site notice for all wikis on db4
    
    * Update Sitenotice.php
    
    * Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -12,15 +12,17 @@ $wgMajorSiteNoticeID = 43;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+if ( !isset( $wgLBFactoryConf['sectionsByDB'][$wgDBname] ) ) {
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze is doing maintenance on the file backend. During this time you will not be able to upload files.</td>
+			<td>Miraheze is doing maintenance on DB at 20:00 UTC. This will last until approximately 02:00 UTC. During this time your wiki may be in read only mode. Please save edits 5 minutes before. We apologize that we are doing another migration, but it is necessary in order to reduce the recent slow loading times.</td>
 			</tr></tbody></table>
 EOF;
 
 		return true;
-}*/
+}
+}
