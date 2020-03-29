d57268da7f7ecabd66ab4fd0022439c3fb884a97
paladox <paladox@users.noreply.github.com>
2020-03-29 00:31:02 +0000

    Take dbt1 wikis out of read only (#2958)

--- Database.php
+++ Database.php
@@ -110,8 +110,8 @@ $wgLBFactoryConf = [
 		],
 	],
 	'readOnlyBySection' => [
-		'DEFAULT' => 'Maintenance ongoing on the database server.',
-		'c1' => 'Maintenance ongoing on the database server.',
+		// 'DEFAULT' => 'Maintenance ongoing on the database server.',
+		// 'c1' => 'Maintenance ongoing on the database server.',
 		// 'c4' => 'Maintenance ongoing on the database server.',
 	],
 ];
