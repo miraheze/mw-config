46a28368cc7ab6f4d95454094d5c5e7b3f4388b0
paladox <paladox@users.noreply.github.com>
2020-04-03 01:03:17 +0100

    Remove lizardfs6 reference

--- LocalSettings.php
+++ LocalSettings.php
@@ -3232,8 +3232,7 @@ if ( $wgDBname !== 'commonswiki' && $wgMirahezeCommons ) {
 }
 
 // Servers accessible by non cache proxies should not have squid/cdn config enabled
-if ( !preg_match( "/^mw[0-9]*/", wfHostname() ) ||
-     !preg_match( "/^lizardfs6*/", wfHostname() ) ) {
+if ( !preg_match( "/^mw[0-9]*/", wfHostname() ) ) {
 	$wgUseSquid = false;
 	$wgUseCdn = false;
 }
