3a0d2d82c7480e8df0c35505201d96adcfb1e316
paladox <paladox@users.noreply.github.com>
2020-04-09 15:28:12 +0100

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -2654,7 +2654,7 @@ $wgConf->settings = [
 	
 	// WikiSEO configs
 	'wgTwitterCardType' => [
-		'default' => null,
+		'default' => 'summery_large_image',
 	],
 	'wgGoogleSiteVerificationKey' => [
 		'default' => null,
