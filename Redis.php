752b00df2435c3eb735686b5f6c6836283d0da6b
paladox <paladox@users.noreply.github.com>
2020-04-13 01:48:27 +0100

    Update Redis.php

--- Redis.php
+++ Redis.php
@@ -3,8 +3,8 @@
 // Locally hosted and used for object caching
 $wgObjectCaches['redis-central'] = [
 	'class' => 'RedisBagOStuff',
-	'servers' => [ '/run/nutcracker/nutcracker.sock' ],
-	'password' => $wmgRedisPassword,
+	'servers' => [ $wmgRedisSettings['cache']['server'] ],
+	'password' => $wmgRedisSettings['cache']['password'],
 	'persistent' => true,
 	'loggroup' => 'redis',
 	'reportDupes' => false,
@@ -20,15 +20,12 @@ $wgUseLocalMessageCache = true;
 $wgParserCacheType = CACHE_DB;
 $wgLanguageConverterCacheType = CACHE_DB;
 
-$wmgRedisJobrunnerIp = '51.89.160.135';
-$wmgRedisJobrunnerPort = '6379';
-
 $wgJobTypeConf['default'] = [
 	'class' => 'JobQueueRedis',
-	'redisServer' => "{$wmgRedisJobrunnerIp}:{$wmgRedisJobrunnerPort}",
+	'redisServer' => $wmgRedisSettings['jobrunner']['server'],
 	'redisConfig' => [
 		'connectTimeout' => 2,
-		'password' => $wmgRedisPassword,
+		'password' => $wmgRedisSettings['jobrunner']['password'],
 		'compression' => 'gzip',
 	],
 	'claimTTL' => 3600,
@@ -38,12 +35,12 @@ $wgJobTypeConf['default'] = [
 $wgJobQueueAggregator = [
 	'class' => 'JobQueueAggregatorRedis',
 	'redisServers' => [
-		"{$wmgRedisJobrunnerIp}:{$wmgRedisJobrunnerPort}",
-		"{$wmgRedisJobrunnerIp}:{$wmgRedisJobrunnerPort}"
+		$wmgRedisSettings['jobrunner']['server'],
+		$wmgRedisSettings['jobrunner']['server']
 	], // fake redis fallback
 	'redisConfig' => [
 		'connectTimeout' => 2,
-		'password' => $wmgRedisPassword,
+		'password' => $wmgRedisSettings['jobrunner']['password'],
 		'compression' => 'gzip',
 	]
 ];
