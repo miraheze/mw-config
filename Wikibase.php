b6a58b30479beae6d23bd84b38222644ee2a5704
John Lewis <john@miraheze.org>
2020-04-27 23:28:32 +0100

    add additional setting

--- Wikibase.php
+++ Wikibase.php
@@ -12,8 +12,16 @@ $wgWBRepoSettings['specialSiteLinkGroups'] = [];
 $wgWBClientSettings['repoUrl'] = 'https://' . $wi->hostname;
 $wgWBClientSettings['repoDatabase'] = $wi->dbname;
 $wgWBClientSettings['changesDatabase'] = $wi->dbname;
+$wgWBClientSettings['siteGlobalID'] = $wi->dbname;
+$wgWBClientSettings['repoScriptPath'] = '/w';
+$wgWBClientSettings['repoArticlePath'] = '/wiki/$1';
 $wgWBClientSettings['siteGroup'] = 'miraheze';
 $wgWBClientSettings['repoNamespaces'] = [
 	'wikibase-item' => 'Item',
 	'wikibase-property' => 'Property'
 ];
+$wgWBClientSettings['siteLinksGroups'] = [
+	'miraheze'
+];
+$wgWBClientSettings['purgeCacheBatchSize'] = 100;
+$wgWBClientSettings['recentChangesBatchSize'] = 100;
