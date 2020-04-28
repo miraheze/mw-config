72e1bc32835ab05b67ce2fe8cd74af57a5a2eda3
examknow <ek311@outlook.com>
2020-04-28 13:14:26 -0500

    remove GlobalExtensions
    dependent on $wgMirahezeCommons

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -752,18 +752,6 @@ $wgManageWikiExtensions = [
 			'conflicts' => false,
 			'requires' => [],
 		],
-		'globalusage' => [
-			'name' => 'GlobalUsage',
-			'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:GlobalUsage',
-			'var' => 'wmgUseGlobalUsage',
-			'conflicts' => false,
-			// Sysadmins feel free to remove restriction after testing
-			'requires' => [
-				'permissions' => [
-					'managewiki-restricted',
-				],
-			],
-		],
 		'globaluserpage' => [
 			'name' => 'GlobalUserPage',
 			'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:GlobalUserPage',
