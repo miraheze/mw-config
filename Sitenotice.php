771465bf0f73cf0fdf8febfe9513febd1aeb0195
Reception123 <utilizator.receptie123@gmail.com>
2020-04-30 09:05:25 +0200

    sitenotice for socialprofilewikis

--- Sitenotice.php
+++ Sitenotice.php
@@ -25,3 +25,18 @@ EOF;
 		return true;
 }
 */
+
+// Specific wiki sitenotices
+if ( $wmgUseSocialProfile ) {
+$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+function onSiteNoticeAfter( &$siteNotice, $skin ) {
+	global $wmgSiteNoticeOptOut, $snImportant;
+
+	$siteNotice .= <<<EOF
+			<table class="wikitable" style="text-align:center;"><tbody><tr>
+			<td>Due to a recent update of SocialProfile, some wikis may experience issues with comments and user profiles. We are aware of these issues and are looking into them. Thank you for your patience and we apologize for the incovenience. </td>
+			</tr></tbody></table>
+EOF;
+	}
+}
+		return true;
