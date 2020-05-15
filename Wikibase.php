04ee8944cf361518d1488f573702966bac4d6e8d
paladox <paladox@users.noreply.github.com>
2020-05-15 20:20:27 +0100

    Update Wikibase.php

--- Wikibase.php
+++ Wikibase.php
@@ -26,7 +26,7 @@ $wgWBClientSettings['siteLinksGroups'] = [
 $wgWBClientSettings['purgeCacheBatchSize'] = 100;
 $wgWBClientSettings['recentChangesBatchSize'] = 100;
 
-if ( in_array( $wi->dbname, [ 'hispanowiki', 'privadowiki', 'hispano76' ] ) ) {
+if ( in_array( $wi->dbname, [ 'hispanowiki', 'privadowiki', 'hispano76', 'ucroniaswiki' ] ) ) {
 	$wgWBRepoSettings['sharedCacheKeyPrefix'] = 'hispanowiki' . ':WBL/' . rawurlencode( WBL_VERSION );
 	$wgWBClientSettings['repoUrl'] = 'https://hispano.miraheze.org';
 	$wgWBClientSettings['repoDatabase'] = 'hispanowiki';
