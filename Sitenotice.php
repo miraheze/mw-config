28eb9f5975c23e3a35daa6b884217c47257168a4
paladox <paladox@users.noreply.github.com>
2020-06-03 17:14:54 +0100

    Site notice for db maintenance (#3097)

--- Sitenotice.php
+++ Sitenotice.php
@@ -12,33 +12,14 @@ $wgMajorSiteNoticeID = 44;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 
-/* $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-function onSiteNoticeAfter( &$siteNotice, $skin ) {
-	global $wmgSiteNoticeOptOut, $snImportant;
-
-	$siteNotice .= <<<EOF
-			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Due to a recent change, some users may experience login issues, to correct them please follow the steps outlined at <a href="https://meta.miraheze.org/m/4FU">https://meta.miraheze.org/m/4FU</a>.</td>
-			</tr></tbody></table>
-EOF;
-
-		return true;
-}
-*/
-
-// Specific wiki sitenotices
-/*
-if ( $wmgUseComments ) {
 $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Due to a recent update of Comments, some wikis may experience issues with comments. We are aware of these issues and are looking into them. Thank you for your patience and we apologize for the incovenience. </td>
+			<td>Miraheze plans to do database maintenance at 19:00 UTC time. This maintenance will take between 30 and 60 minutes. During this time you won't be able to view your wiki. We apologise for any inconvenience.</td>
 			</tr></tbody></table>
 EOF;
 		return true;
-	}
 }
-*/	
