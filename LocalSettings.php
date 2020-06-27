1c5d8ba7fb98273bd836d72ca583fa7633ead4e2
Paladox <thomasmulhall410@yahoo.com>
2020-06-28 00:53:58 +0100

    Revert "Set wgWikiDiscoverSubdomain to .miraheze.org (#3127)"
    This reverts commit 6ff9bdb18b68d4394412055825f55b13548d847a.

--- LocalSettings.php
+++ LocalSettings.php
@@ -3335,9 +3335,6 @@ $wi->config->settings = [
 	'wgWikiDiscoverPrivateList' => [
 		'default' => '/srv/mediawiki/dblist/private.dblist',
 	],
-	'wgWikiDiscoverSubdomain' => [
-		'default' => '.miraheze.org',
-	],
 
 	// CreateWiki Defined Special Variables
 	'cwClosed' => [
