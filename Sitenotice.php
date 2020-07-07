9528ab6bf3b22cca632f5598a35eedf09f82423e
paladox <paladox@users.noreply.github.com>
2020-07-07 23:12:41 +0100

    Remove sitenotice

--- Sitenotice.php
+++ Sitenotice.php
@@ -147,18 +147,3 @@ EOF;
 	}
 }
 */
-
-$db = $wi->config->settings['wgLBFactoryConf']['default']['sectionsByDB'][$wgDBname];
-if ( isset( $db ) && $db == 'c4' ) {
-	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter3';
-	function onSiteNoticeAfter3( &$siteNotice, $skin ) {
-		global $wmgSiteNoticeOptOut, $snImportant;
-
-		$siteNotice .= <<<EOF
-				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td>Miraheze plans to do database maintenance at 10pm UTC time. Please save your edits at least 5 minutes before. The maintenance will last 5 minutes.</td>
-				</tr></tbody></table>
-EOF;
-			return true;
-	}
-}
