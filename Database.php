2d599fbc00a405b6ff80cd6e835fa34e7ebd50d1
paladox <paladox@users.noreply.github.com>
2020-07-07 22:29:01 +0100

    Set c4 (db7) to read only

--- Database.php
+++ Database.php
@@ -116,6 +116,6 @@ $wi->config->settings['wgLBFactoryConf']['default'] = [
 	'readOnlyBySection' => [
 		// 'DEFAULT' => 'Maintenance ongoing on the database server.',
 		// 'c1' => 'Maintenance ongoing on the database server.',
-		// 'c4' => 'Maintenance ongoing on the database server.',
+		'c4' => 'Maintenance ongoing on the database server.',
 	],
 ];
