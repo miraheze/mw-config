cbc025b074ec9dbe24123c672be5ea381bcbe3fe
Hispano76 <43275442+Hispano76@users.noreply.github.com>
2020-07-09 15:51:55 -0500

    add wgNamespaceRobotPolicies on privadowiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -2892,6 +2892,12 @@ $wi->config->settings = [
 			'NS_MEDIAWIKI' => 'noindex,nofollow',
 			'NS_USER' => 'noindex,nofollow',
 		],
+		'privadowiki' => [
+			'NS_TEMPLATE' => 'noindex,nofollow',
+			'NS_MODULE' => 'noindex,nofollow',
+			'NS_MEDIAWIKI' => 'noindex,nofollow',
+			'NS_USER' => 'noindex,nofollow',
+		],
 		'ucroniaswiki' => [
 			'NS_TEMPLATE' => 'noindex,nofollow',
 			'NS_MODULE' => 'noindex,nofollow',
