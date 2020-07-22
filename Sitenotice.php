65945200175fed7cfa9faad27aa93d93acdc579d
Ferran Tufan <southparkfan223@hotmail.com>
2020-07-22 04:04:37 +0200

    Add global 5y anniversary notice, with opt out

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,19 +8,21 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 47;
+$wgMajorSiteNoticeID = 48;
 
 // Write your SiteNotice below.  Comment out this section to disable.
 
-/*$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-function onSiteNoticeAfter( &$siteNotice, $skin ) {
-	global $wmgSiteNoticeOptOut, $snImportant;
+if ( !$wmgSiteNoticeOptOut ) {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+		global $wmgSiteNoticeOptOut, $snImportant;
 
-	$siteNotice .= <<<EOF
-			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Miraheze needs to perform emergency maintenance on one of its database clusters. To do that, one of the servers must be restarted. Maintenance is scheduled between July 8, 20:45 UTC and 21:15 UTC. Please save your edits before 20:45 UTC!</td>
-			</tr></tbody></table>
-EOF;
+		$siteNotice .= <<<EOF
+				<table class="wikitable" style="text-align:center;"><tbody><tr>
+				<td>Miraheze celebrates its fifth birthday today. <a href="https://meta.miraheze.org/wiki/Miraheze-5-year">Come celebrate with us! 🎈</a></a></td>
+				</tr></tbody></table>
+	EOF;
 
-	return true;
-}*/
+		return true;
+	}
+}
