66ab1289bab130e7dddf5d828437438c218ca476
RhinosF1 <rhinosf1@miraheze.org>
2020-09-02 21:04:24 +0100

    missing-wiki: add archive.org link (#3200)
    per discord suggestion

--- MissingWiki.php
+++ MissingWiki.php
@@ -63,7 +63,7 @@ if ( !$wgCommandLineMode ) {
 				<div style="text-align:center;">
 					<h2>What can I do?</h2>
 					<p class="lead">If you're a wiki visitor:</p>
-					<p>This wiki does not exist on our servers. You can browse wikis on our network <a href="//meta.miraheze.org/wiki/Special:WikiDiscover">here</a></p>
+					<p>This wiki does not exist on our servers. You can browse wikis on our network <a href="//meta.miraheze.org/wiki/Special:WikiDiscover">here</a> or <a href="https://archive.org/search.php?query=miraheze">view archive.org archives</a>.</p>
 					<p class="lead">If you're the wiki bureaucrat:</p>
 					<p>If this is a problem, please <a href="https://meta.miraheze.org/wiki/Help_center">contact us</a></p>
 				</div>
