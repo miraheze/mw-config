21cba63a90eddbe31b94db45b00bdfb41e33b5d3
Reception123 <utilizator.receptie123@gmail.com>
2020-10-27 16:35:44 +0100

    1.35 sitenotice (#3321)

--- Sitenotice.php
+++ Sitenotice.php
@@ -11,17 +11,17 @@ if ( $wmgSiteNoticeOptOut ) {
 $wgMajorSiteNoticeID = 48;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td style="font-size:125%">Miraheze plans to do database maintenance on October 13th at 20:00 UTC time to 22:00 UTC. Please save edits 5 minutes before hand.</td>
+				<td style="font-size:125%">Miraheze plans to upgrade to the latest version of MediaWiki (1.35) today from 20:00 UTC time to approximately 20:30 UTC. Please save your edits 5 minutes before hand.</td>
 				</tr></tbody></table>
 EOF;
 
 		return true;
 	}
-}*/
+}
