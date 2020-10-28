5b20d3131ee23b10a710a5a617dc501b38e959e8
Devin/Zppix <zppix@miraheze.org>
2020-10-27 20:57:48 -0500

    Add a notice due to increased reports of issues. (#3328)
    * Add a notice due to increased reports of issues.
    
    * rm dupe <a>

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,20 +8,20 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 49;
+$wgMajorSiteNoticeID = 50;
 
 // Write your SiteNotice below.  Comment out this section to disable.
-/*if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td style="font-size:125%">Miraheze plans to upgrade to the latest version of MediaWiki (1.35) today from 20:00 UTC time to approximately 20:30 UTC. Please save your edits 5 minutes before hand.</td>
+				<td style="font-size:125%">Miraheze has upgraded to the latest version of MediaWiki (1.35)! However, if you experience any issues, please do let us know on <a href="https://phabricator.miraheze.org/">Phabricator</a>.</td>
 				</tr></tbody></table>
 EOF;
 
 		return true;
 	}
-}*/
+}
