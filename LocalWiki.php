1fc017b13cbc235cf802d622edfe314d495f84c0
paladox <thomasmulhall410@yahoo.com>
2020-11-06 22:22:17 +0000

    Fix wgUrlShortenerAllowedDomains

--- LocalWiki.php
+++ LocalWiki.php
@@ -100,7 +100,7 @@ if ( (bool)$wmgWikiapiaryFooterPageName ) {
 
 // $wgUrlShortenerAllowedDomains
 if ( !preg_match( '/^(.*).miraheze.org$/', $wi->hostname ) ) {
-	$wi->config->settings['wgUrlShortenerAllowedDomains']['+default'] = array_merge( $wgUrlShortenerAllowedDomains, [ preg_quote( $wi->hostname ) ] );
+	$wi->config->settings['wgUrlShortenerAllowedDomains']['default'] = array_merge( $wgUrlShortenerAllowedDomains, [ preg_quote( $wi->hostname ) ] );
 }
 
 // Per-wiki overrides
