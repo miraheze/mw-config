30c2c4053270d44a12b28a7d9a37da8ca8cc8caf
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-11-14 19:10:14 -0700

    fix

--- LocalSettings.php
+++ LocalSettings.php
@@ -210,20 +210,22 @@ $wi->config->settings = [
 	// ContactPage
 	'wgContactConfig' => [
 		'default' => [
-			'RecipientUser' => User::newFromId(2)->getName(), // Username of wiki requester
-			'SenderEmail' => $wgPasswordSender,
-			'SenderName' => 'Miraheze No Reply',
-			'RequireDetails' => true,
-			'IncludeIP' => false, // Should never be set to true
-			'MustBeLoggedIn' => false,
-			'AdditionalFields' => [],
-			'DisplayFormat' => 'table',
-			'RLModules' => [],
-			'RLStyleModules' => [],
+			'default' => [
+				'RecipientUser' => User::newFromId(2)->getName(), // Username of wiki requester
+				'SenderEmail' => $wgPasswordSender,
+				'SenderName' => 'Miraheze No Reply',
+				'RequireDetails' => true,
+				'IncludeIP' => false, // Should never be set to true
+				'MustBeLoggedIn' => false,
+				'AdditionalFields' => [],
+				'DisplayFormat' => 'table',
+				'RLModules' => [],
+				'RLStyleModules' => [],
+			],
 		],
 	],
 	'wmgContactPageRecipientUser' => [
-		'default' => User::newFromId(2)->getName(), // Username of wiki requester,
+		'default' => User::newFromId(2)->getName(), // Username of wiki requester
 	],
 
 	// Cosmos settings
