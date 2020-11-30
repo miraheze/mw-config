ee9d87ddee367a8d8a96bd21c81d238d7c46b5dd
paladox <paladox@users.noreply.github.com>
2020-11-30 23:22:40 +0000

    Revert "Update logging config for greylog (#3501)" (#3504)
    This reverts commit be7007372ea1f9ff358cd770932ad48d2197926f.

--- GlobalLogging.php
+++ GlobalLogging.php
@@ -1,170 +1,62 @@
 <?php
 
-use MediaWiki\Logger\LoggerFactory;
-use Monolog\Handler\WhatFailureGroupHandler;
+$hosts = [
+ 	'jobrunner1',
+	'jobrunner2',
+ 	'test2'
+ ];
 
-$wmgUseGraylogHost = '/^(jobrunner[0-9]|test[0-9])/';
-if ( preg_match( $wmgUseGraylogHost, wfHostname(), $matches ) ) {
-	// Monolog logging configuration
-
-	$wmgMonologProcessors = [
-		'psr' => [
-			'class' => \Monolog\Processor\PsrLogMessageProcessor::class,
-		],
-		'web' => [
-			'class' => \Monolog\Processor\WebProcessor::class,
-		],
-		'wiki' => [
-			'class' => \MediaWiki\Logger\Monolog\WikiProcessor::class,
-		],
-	];
-
-	$wmgMonologHandlers = [
-		'blackhole' => [
-			'class' => \Monolog\Handler\NullHandler::class,
-		],
-	];
-
-	foreach ( [ 'debug', 'info', 'warning', 'error' ] as $logLevel ) {
-		$wmgMonologHandlers[ "graylog-$logLevel" ] = [
-			'class'     => \MediaWiki\Logger\Monolog\SyslogHandler::class,
-			'formatter' => 'logstash',
-			'args'      => [
-				'mediawiki', // tag
-				'127.0.0.1', // host
-				10514,       // port
-				LOG_USER,    // facility
-				$logLevel,   // log level threshold
-			],
-		];
-	}
-
-	$wmgMonologHandlers['what-debug'] = [
-		'class'     => WhatFailureGroupHandler::class,
-		'formatter' => 'logstash',
-		'args' => [
-			function () {
-				$provider = LoggerFactory::getProvider();
-				return array_map( [ $provider, 'getHandler' ], [ 'graylog-debug' ] );
-			}
-		],
-	];
-
-	// Post construction calls to make for new Logger instances
-	$wmgMonologLoggerCalls = [
-		'setTimezone' => [ new DateTimeZone( 'UTC' ) ],
-		// https://phabricator.wikimedia.org/T116550 - Requires Monolog > 1.17.2
-		'useMicrosecondTimestamps' => [ false ],
-	];
-
-	$wmgMonologConfig = [
-		'loggers' => [
-			// Template for all undefined log channels
-			'@default' => [
-				'handlers' => [ 'what-debug' ],
-				'processors' => array_keys( $wmgMonologProcessors ),
-				'calls' => $wmgMonologLoggerCalls,
-			],
-		],
-		'processors' => $wmgMonologProcessors,
-		'handlers' => $wmgMonologHandlers,
-		'formatters' => [
-			'logstash' => [
-				'class' => \Monolog\Formatter\LogstashFormatter::class,
-				'args' => [ 'mediawiki', php_uname( 'n' ), null, '', 1 ],
-			],
-		],
-	];
-
-	// Add logging channels defined in $wmgMonologChannels
-	foreach ( $wmgMonologChannels as $channel => $opts ) {
-		if ( $opts === false ) {
-			// Log channel disabled on this wiki
-			$wmgMonologConfig['loggers'][$channel] = [
-				'handlers' => [ 'blackhole' ],
-				'calls' => $wmgMonologLoggerCalls,
-			];
-			continue;
-		}
-
-		$opts = is_array( $opts ) ? $opts : [ 'graylog' => $opts ];
-		$opts = array_merge(
-			[
-				'graylog' => 'debug',
-				'buffer' => false,
-			],
-			$opts
-		);
-
-		$handlers = [];
-
-		// Configure Logstash handler
-		if ( $opts['graylog'] ) {
-			$level = $opts['graylog'];
-			$graylogHandler = "graylog-{$level}";
-			if ( isset( $wmgMonologHandlers[ $graylogHandler ] ) ) {
-				$handlers[] = $graylogHandler;
-			}
-		}
-
-		if ( $opts['buffer'] ) {
-			foreach ( $handlers as $idx => $handlerName ) {
-				$bufferedHandler = "{$handlerName}-buffered";
-				if ( !isset( $wmgMonologConfig['handlers'][$bufferedHandler] ) ) {
-					// Register a handler that will buffer the event stream and
-					// pass events to the nested handler after closing the request
-					$wmgMonologConfig['handlers'][$bufferedHandler] = [
-						'class' => \MediaWiki\Logger\Monolog\BufferHandler::class,
-						'args' => [
-							function () use ( $handlerName ) {
-								return LoggerFactory::getProvider()->getHandler(
-									$handlerName
-								);
-							},
-						],
-					];
-				}
-				$handlers[$idx] = $bufferedHandler;
-			}
-		}
-
-		if ( $handlers ) {
-			// wrap the collection of handlers in a WhatFailureGroupHandler
-			// to swallow any exceptions that might leak out otherwise
-			$failureGroupHandler = 'failuregroup|' . implode( '|', $handlers );
-			if ( !isset( $wmgMonologConfig['handlers'][$failureGroupHandler] ) ) {
-				$wmgMonologConfig['handlers'][$failureGroupHandler] = [
-					'class' => WhatFailureGroupHandler::class,
-					'args' => [
-						function () use ( $handlers ) {
-							$provider = LoggerFactory::getProvider();
-							return array_map(
-								[ $provider, 'getHandler' ],
-								$handlers
-							);
-						}
-					],
-				];
-			}
-
-			$wmgMonologConfig['loggers'][$channel] = [
-				'handlers' => [ $failureGroupHandler ],
-				'processors' => array_keys( $wmgMonologProcessors ),
-				'calls' => $wmgMonologLoggerCalls,
-			];
-
-		} else {
-			// No handlers configured, so use the blackhole route
-			$wmgMonologConfig['loggers'][$channel] = [
-				'handlers' => [ 'blackhole' ],
-				'calls' => $wmgMonologLoggerCalls,
-			];
-		}
-	}
-
+// Only enable if syslog-ng is deployed on the server
+if ( in_array( wfHostname(), $hosts ) ) {
 	$wi->config->settings['wgMWLoggerDefaultSpi']['default'] = [
 		'class' => \MediaWiki\Logger\MonologSpi::class,
-		'args' => [ $wmgMonologConfig ],
+		'args' => [
+			[
+				'loggers' => [
+					'@default' => [
+						'processors' => [ 'wiki', 'psr', 'web' ],
+						'handlers' => [ 'what' ],
+					],
+				],
+				'processors' => [
+					'psr' => [
+						'class' => \Monolog\Processor\PsrLogMessageProcessor::class,
+					],
+					'web' => [
+						'class' => \Monolog\Processor\WebProcessor::class,
+					],
+					'wiki' => [
+						'class' => \MediaWiki\Logger\Monolog\WikiProcessor::class,
+					],
+				],
+				'handlers' => [
+					'syslog' => [
+						'class' => \MediaWiki\Logger\Monolog\SyslogHandler::class,
+						'formatter' => 'logstash',
+						'args' => [
+							'mediawiki', // tag
+							'127.0.0.1', // local syslog-ng daemon
+							10514, // local port,
+						],
+					],
+					'what' => [
+						'class' => \Monolog\Handler\WhatFailureGroupHandler::class,
+						'args' => [
+							function () {
+								$provider = \MediaWiki\Logger\LoggerFactory::getProvider();
+								return array_map( [ $provider, 'getHandler' ], [ 'syslog' ] );
+							}
+						],
+					],
+				],
+				'formatters' => [
+					'logstash' => [
+						'class' => \Monolog\Formatter\LogstashFormatter::class,
+						'args' => [ 'mediawiki', php_uname( 'n' ), null, '', 1 ],
+					],
+				],
+			],
+		],
 	];
 } else {
 	$wmgLogDir = "/var/log/mediawiki";
