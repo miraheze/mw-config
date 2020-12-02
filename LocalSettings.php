fc14655cbce989429aa1c43f6bd42a6f97c9ad91
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-01 22:26:57 -0700

    Add 2 DynamicPageList functions to MWS (#3512)
    * Add 2 DynamicPageList functions to MWS
    
    * add
    
    * re-order

--- LocalSettings.php
+++ LocalSettings.php
@@ -658,6 +658,14 @@ $wi->config->settings = [
 	'wgDjvuTxt' => [
 		'default' => '/usr/bin/djvutxt',
 	],
+	
+	// DynamicPageList
+	'wgDLPAllowUnlimitedCategories' => [
+		'default' => false,
+	],
+	'wgDLPAllowUnlimitedResults' => [
+		'default' => false,
+	],
 
 	// TimedMediaHandler config
 	'wgFFmpegLocation' => [
