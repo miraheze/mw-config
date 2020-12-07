05ed819df26d5624512aa159649459a3bec74468
paladox <paladox@users.noreply.github.com>
2020-12-07 17:49:28 +0000

    Revert "LE: Remove Parsoid/JS config (T6528) (#3529)" (#3532)
    This reverts commit a23487e946488f9b7d5730b657906b5d537d0dc0.

--- LocalExtensions.php
+++ LocalExtensions.php
@@ -295,6 +295,14 @@ if ( $wmgUseFlaggedRevs ) {
 if ( $wmgUseFlow ) {
 	wfLoadExtension( 'Flow' );
 
+	$wgVirtualRestConfig['modules']['parsoid'] = [
+		'url' => 'https://parsoid-lb.miraheze.org:443',
+		'domain' => $wgServer,
+		'prefix' => $wgDBname,
+		'forwardCookies' => true,
+		'restbaseCompat' => false,
+	];
+
 	$wi->config->settings['wgManageWikiPermissionsAdditionalRights']['default']['oversight']['flow-suppress'] = true;
 	$wi->config->settings['wgManageWikiNamespacesExtraContentModels']['default']['Flow'] = 'flow-board';
 }
@@ -1017,6 +1025,14 @@ if ( $wmgUseVideo ) {
 
 if ( $wmgUseVisualEditor ) {
 	wfLoadExtension( 'VisualEditor' );
+
+	$wgVirtualRestConfig['modules']['parsoid'] = [
+		'url' => 'https://parsoid-lb.miraheze.org:443',
+		'domain' => $wgServer,
+		'prefix' => $wgDBname,
+		'forwardCookies' => true,
+		'restbaseCompat' => false,
+	];
 
 	if ( $wmgVisualEditorEnableDefault ) {
 		$wi->config->settings['+wgDefaultUserOptions']['default']['visualeditor-enable'] = 1;
