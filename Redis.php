ee30d9ffc7dccd7922b26a736e879b5b58687e14
paladox <thomasmulhall410@yahoo.com>
2020-12-13 18:32:14 +0000

    Set wgLanguageConverterCacheType to use redis

--- Redis.php
+++ Redis.php
@@ -18,7 +18,7 @@ $wgSessionsInObjectCache = true;
 $wgMessageCacheType = CACHE_DB;
 $wgUseLocalMessageCache = true;
 $wgParserCacheType = CACHE_DB;
-$wgLanguageConverterCacheType = CACHE_DB;
+$wgLanguageConverterCacheType = 'redis-central';
 
 $wgJobTypeConf['default'] = [
 	'class' => 'JobQueueRedis',
