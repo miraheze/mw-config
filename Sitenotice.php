6a79484c384fe540622121c712130c0b7f857946
paladox <paladox@users.noreply.github.com>
2020-12-14 20:10:26 +0000

    Add emergency site notice for c4 cluster

--- Sitenotice.php
+++ Sitenotice.php
@@ -9,20 +9,21 @@ if ( $wmgSiteNoticeOptOut ) {
 // Increment this version number whenever you change the site notice
 // and don't comment it out
 $wgMajorSiteNoticeID = 50;
-/*
+
+
 // Write your SiteNotice below.  Comment out this section to disable.
-if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut && $wi->wikiDBClusters[$wgDBname] === 'c4' ?? null ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td style="font-size:125%">Miraheze has upgraded to the latest version of MediaWiki (1.35)! However, if you experience any issues, please do let us know on <a href="https://phabricator.miraheze.org/">Phabricator</a>.</td>
+				<td style="font-size:125%">Miraheze is planning to do emergency database maintenance at 20:20 UTC. The maintenance should only last 20 minutes.</td>
 				</tr></tbody></table>
 EOF;
 
 		return true;
 	}
 }
-*/
+
