31d70960437fa25e0325fe31be3dc5df02e83367
paladox <thomasmulhall410@yahoo.com>
2020-12-18 18:17:34 +0000

    Revert "Fix $wgTmpDirectory"
    This reverts commit 05fe17ff3d83e873fb02656985879eb22dcfb214.

--- LocalSettings.php
+++ LocalSettings.php
@@ -3852,7 +3852,7 @@ $wgDefaultUserOptions = array_merge( $wgDefaultUserOptions, $wmgDefaultUserOptio
 
 $wgUploadPath = "https://static.miraheze.org/$wgDBname";
 $wgUploadDirectory = "/mnt/mediawiki-static/$wgDBname";
-$wgTmpDirectory = "/mnt/mediawiki-static/private/temp/$wgDBname";
+$wgTmpDirectory = '/mnt/mediawiki-static/private/temp';
 
 // Fonts
 putenv( "GDFONTPATH=/usr/share/fonts/truetype/freefont" );
