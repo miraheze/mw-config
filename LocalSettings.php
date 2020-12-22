43c982868d66fc1ee4d0e9de2ce344474feb0a1a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-22 10:37:45 -0700

    test

--- LocalSettings.php
+++ LocalSettings.php
@@ -34,9 +34,9 @@ $wi->setVariables(
 
 $wi->config->settings += [
 	// invalidates user sessions - do not change unless it is an emergency.
-        'wgAuthenticationTokenVersion' => [
+	'wgAuthenticationTokenVersion' => [
 		'default' => '4',
-	],
+	]
 
 	// AbuseFilter
 	'wgAbuseFilterActions' => [
