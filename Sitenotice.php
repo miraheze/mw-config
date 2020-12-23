713792eafb936391706bc20c0df72d8355d9651b
RhinosF1 <rhinosf1@miraheze.org>
2020-12-23 22:18:07 +0000

    Update Sitenotice.php (#3589)

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,19 +8,19 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 51;
+$wgMajorSiteNoticeID = 52;
 
-/* if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td style="font-size:125%">Miraheze is planning to do emergency maintenance starting 22:55 UTC. Service may be degraded. This may last up to 4 hours. Apologies for any inconvenience.</td>
+				<td style="font-size:125%">Following last night's emergency maintenance and the removal of [[:mediawikiwiki:Extension:Widgets|Widgets]]. Miraheze has released a [[:m:23-12-2020 Security Disclosure|Security Disclosure]]. Apologies for any inconvenience.</td>
 				</tr></tbody></table>
 EOF;
 
 		return true;
 	}
-} */
+}
