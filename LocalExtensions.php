86cd9bf3fba1ef24155bc76133d8fa62afb7d7b0
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-24 00:37:38 -0700

    add Validator extension

--- LocalExtensions.php
+++ LocalExtensions.php
@@ -1000,6 +1000,10 @@ if ( $wmgUseUserWelcome ) {
 	wfLoadExtension( 'SocialProfile/UserWelcome' );
 }
 
+if ( $wmgUseValidator ) {
+	require_once( "$IP/extensions/Validator/Validator.php" );
+}
+
 if ( $wmgUseVariables ) {
 	wfLoadExtension( 'Variables' );
 }
