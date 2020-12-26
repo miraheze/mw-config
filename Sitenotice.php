2c403c345ef457c60f881229cf5c8b2f4ff9c13d
paladox <paladox@users.noreply.github.com>
2020-12-26 20:32:48 +0000

    fix time

--- Sitenotice.php
+++ Sitenotice.php
@@ -17,7 +17,7 @@ if ( !$wmgSiteNoticeOptOut ) {
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td style="font-size:125%">Miraheze will be performing maintenance between 20:45 and 20:55 UTC (this was 21:35 in the previous notice, has been corrected now). You may encouter some errors during this maintenance window. Please save your edits before 21:35 UTC!</td>
+				<td style="font-size:125%">Miraheze will be performing maintenance between 20:45 and 20:55 UTC (this was 21:35 in the previous notice, has been corrected now). You may encouter some errors during this maintenance window. Please save your edits before 20:35 UTC!</td>
 				</tr></tbody></table>
 		EOF;
 	}
