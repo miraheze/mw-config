cb547e42df1ab8d1fe607245b839f8696c56860e
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-26 16:24:19 -0700

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -2074,7 +2074,7 @@ $wi->config->settings += [
 		'default' => false,
 	],
 	'wgKartographerMapServer' => [
-		'default' => '/',
+		'default' => '//',
 	],
 	'wgKartographerDfltStyle' => [
 		'default' => '/tile.openstreetmap.org',
