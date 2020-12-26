45bd3783fc3e9913b9687f0e152e52205147f3ff
Ferran Tufan <southparkfan223@hotmail.com>
2020-12-26 21:26:44 +0100

    Add notice

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,17 +8,17 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 52;
+$wgMajorSiteNoticeID = 53;
 
-/* if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
 
 		$siteNotice .= <<<EOF
 				<table class="wikitable" style="text-align:center;"><tbody><tr>
-				<td style="font-size:125%">Following the emergency maintenance on 22nd December and the removal of <a href="https://mediawiki.org/wiki/Extension:Widgets">Widgets</a>. Miraheze has released a <a href="https://meta.miraheze.org/wiki/23-12-2020_Security_Disclosure">Security Disclosure</a>. Apologies for any inconvenience.</td>
+				<td style="font-size:125%">Miraheze will be performing maintenance between 21:35 and 21:45 UTC. You may encouter some errors during this maintenance window. Please save your edits before 21:35 UTC!</td>
 				</tr></tbody></table>
 		EOF;
 	}
-} */
+}
