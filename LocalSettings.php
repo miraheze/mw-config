18e542bed33c13756e64df482e4306c2e2c5d6e8
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-26 13:35:51 -0700

    set wgKartographerDfltStyle to null

--- LocalSettings.php
+++ LocalSettings.php
@@ -2076,6 +2076,9 @@ $wi->config->settings += [
 	'wgKartographerMapServer' => [
 		'default' => 'https://tile.openstreetmap.org',
 	],
+	'wgKartographerDfltStyle' => [
+		'default' => null,
+	],
 	'wgKartographerStyles' => [
 		'default' => [],
 	],
