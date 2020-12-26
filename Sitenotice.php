0db77a9f9561ece93b1a9cb6bfa053b4e925f1f9
RhinosF1 <rhinosf1@miraheze.org>
2020-12-26 22:18:07 +0000

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -16,7 +16,7 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td style="font-size:125%">Miraheze has updated the recent <a href="https://meta.miraheze.org/wiki/23-12-2020_Security_Disclosure">Security Disclosure</a> to advise ALL users immediately reset their passwords and 2FA tokens for Miraheze Wikis.</td>
+			<td style="font-size:125%">Miraheze has updated the recent <a href="https://meta.miraheze.org/wiki/23-12-2020_Security_Disclosure">Security Disclosure</a> to strongly advise ALL users immediately reset their passwords and 2FA tokens for Miraheze Wikis.</td>
 			</tr></tbody></table>
 EOF;
 // }
