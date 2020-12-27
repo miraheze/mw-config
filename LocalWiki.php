8446a3e6d93e6dcad2d601c17998f25dfebefb0f
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-26 18:14:12 -0700

    remove editwidgets from metawiki (#3607)

--- LocalWiki.php
+++ LocalWiki.php
@@ -331,10 +331,6 @@ if ( $wgDBname === 'r4356thwiki' ) {
 	$wgWhitelistRead[] = 'R4356th Wiki:Copyrights';
 }
 
-if ( $wgDBname === 'metawiki' ) {
-	$wgAvailableRights[] = 'editwidgets';
-}
-
 // Discord
 $wi->config->settings['wgDiscordFromName']['default'] = $wgSitename;
 $wi->config->settings['wgDiscordNotificationWikiUrl']['default'] = $wgServer . '/w/';
