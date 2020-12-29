25733c3b101d4b2b3dabcbde4b45c26d840e64fd
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-29 10:55:17 -0700

    remove $wgCookieWarningMoreUrl

--- ManageWikiSettings.php
+++ ManageWikiSettings.php
@@ -1376,15 +1376,6 @@ $wgManageWikiSettings = [
 		'section' => 'restricted',
 		'help' => 'The database name for the wiki to use as the file repository. DO NOT ever set this to a private wiki. Does nothing if the value is not a valid Miraheze-hosted wiki database.',
 	],
-	'wgCookieWarningMoreUrl' => [
-		'name' => 'CookieWarning More Url',
-		'from' => 'mediawiki',
-		'restricted' => true,
-		'type' => 'text',
-		'overridedefault' => 'https://meta.miraheze.org/wiki/Privacy_Policy#4._Cookies',
-		'section' => 'restricted',
-		'help' => 'Set the URL to your \'More Information\' page.',
-	],
 	// Default list must be kept insync with wgFileExtensions in LocalSettings.php
 	'wgFileExtensions' => [
 		'name' => 'File Extensions',
