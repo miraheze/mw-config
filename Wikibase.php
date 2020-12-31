6bb23f0ff5c553a81bd876117afe54563d955bf9
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2020-12-30 17:51:16 -0700

    test

--- Wikibase.php
+++ Wikibase.php
@@ -4,48 +4,4 @@
 // You should only need to set $wgWBClientSettings['repoUrl'], $wgWBClientSettings['repositories']['repoDatabase'] and $wgWBClientSettings['changesDatabase']
 // on the wiki.
 
-$wi->config->settings['+wgWBRepoSettings']['default'] = [
-	'entityNamespaces' => [
-		'item' => 860,
-		'property' => 862,
-	],
-	'sharedCacheKeyPrefix' => $wi->dbname . ':WBL',
-	'allowEntityImport' => $wmgAllowEntityImport,
-	'enableEntitySearchUI' => $wmgEnableEntitySearchUI,
-	'federatedPropertiesEnabled' => $wmgFederatedPropertiesEnabled,
-	'siteLinkGroups' => [	
-		'miraheze',	
-	],	
-	'specialSiteLinkGroups' => [],
-];
 
-$wi->config->settings['+wgWBClientSettings']['default'] = [
-	'repoUrl' => $wmgWikibaseRepoUrl,
-	'changesDatabase' => $wmgWikibaseRepoDatabase,
-	'repositories' => [
-		'' => [
-			'repoDatabase' => $wmgWikibaseRepoDatabase,
-			'baseUri' => $wmgWikibaseRepoUrl . '/entity/',
-			'entityNamespaces' => [
-				'item' => '',
-				'property' => 'Property',
-			],
-			'prefixMapping' => [
-				'' => '',
-			],
-		],
-	],
-	'siteGlobalID' => $wi->dbname,
-	'repoScriptPath' => '/w',
-	'repoArticlePath' => '/wiki/$1',
-	'siteGroup' => 'miraheze',
-	'repoNamespaces' => [
-		'item' => '',
-		'property' => 'Property',
-	],
-	'siteLinksGroups' => [
-		'miraheze',
-	],
-	'purgeCacheBatchSize' => 100,
-	'recentChangesBatchSize' => 100,
-];
