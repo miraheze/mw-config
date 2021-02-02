343db2f201cee902febdfb89152929d9e89c9f63
paladox <paladox@users.noreply.github.com>
2021-02-02 10:58:33 +0000

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -10,15 +10,15 @@ if ( $wmgSiteNoticeOptOut ) {
 // and don't comment it out
 $wgMajorSiteNoticeID = 56;
 
-// if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter'; // show to all users
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		global $wmgSiteNoticeOptOut, $snImportant;
 
 		$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td style="font-size:125%">Miraheze is in the process of migrating its' technical infrastructure to <a href="https://www.ovhcloud.com/en-gb/bare-metal/advance/adv-2/" target="_blank">new, upgraded, and improved servers</a>, as a result file uploads have been disabled on all wikis while this migration takes place. We understand the disruption this causes, and anticipate being able to re-enable file uploads as soon as possible. Thank you for your patience.</td>
+			<td style="font-size:125%">Uploads have been re-enabled.</td>
 			</tr></tbody></table>
 		EOF;
 	}
-// }
+}
