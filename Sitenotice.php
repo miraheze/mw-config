cd2281022e76584dc4ab20828f3aa56c4442c284
Doug Mehus <39928941+dmehus@users.noreply.github.com>
2021-02-01 17:02:41 -0800

    Trying to fix this

--- Sitenotice.php
+++ Sitenotice.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( $wmgSiteNoticeOptOut ) {
+/* if ( $wmgSiteNoticeOptOut ) { */
 	// only show important notices when optout
 	$wi->config->settings['wgNoticeProject']['default'] = 'optout';
 }
@@ -16,7 +16,7 @@ function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td style="font-size:125%">Miraheze is in the process of migrating its technical infrastructure, so it file uploads will be temporarily disabled on all Miraheze customer wikis until further notice (not to exceed 48 hours). We apologize for any inconvenience. Thank you</td>
+			<td style="font-size:125%">Miraheze is in the process of migrating its technical infrastructure, so it file uploads will be temporarily disabled on all Miraheze customer wikis until further notice. We apologize for any inconvenience. Thank you</td>
 			</tr></tbody></table>
 EOF;
 }
