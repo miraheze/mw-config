0cb0e3b455187a39b0f847bdac65909849186140
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-02-01 17:25:09 -0700

    Update ManageWikiSettings.php

--- ManageWikiSettings.php
+++ ManageWikiSettings.php
@@ -827,10 +827,14 @@ $wgManageWikiSettings = [
 		'name' => 'Enable File Uploads',
 		'from' => 'mediawiki',
 		'type' => 'check',
-		'overridedefault' => true,
+		'overridedefault' => false,
 		'section' => 'media',
 		'help' => 'Check or uncheck this option if you want to enable or disable the upload of files on your wiki.',
-		'requires' => [],
+		'requires' => [
+			'permissions' => [
+				'managewiki-restricted',
+			],
+		],
 	],
 	'wgAllowCopyUploads' => [
 		'name' => 'Enable File Uploads Through URL',
