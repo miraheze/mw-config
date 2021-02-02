9b6e97cddd02c144b482f859dd6f3f9dd09d7e58
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-02-01 18:40:38 -0700

    Minor updates to sitenotice

--- Sitenotice.php
+++ Sitenotice.php
@@ -1,6 +1,6 @@
 <?php
 
- if ( $wmgSiteNoticeOptOut ) {
+if ( $wmgSiteNoticeOptOut ) {
 	// only show important notices when optout
 	$wi->config->settings['wgNoticeProject']['default'] = 'optout';
 }
@@ -10,13 +10,15 @@
 // and don't comment it out
 $wgMajorSiteNoticeID = 56;
 
-//if ( !$wmgSiteNoticeOptOut ) {
-$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter'; // show to all users
-function onSiteNoticeAfter( &$siteNotice, $skin ) {
-	global $wmgSiteNoticeOptOut, $snImportant;
-	$siteNotice .= <<<EOF
+// if ( !$wmgSiteNoticeOptOut ) {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter'; // show to all users
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+		global $wmgSiteNoticeOptOut, $snImportant;
+
+		$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td style="font-size:125%">Miraheze is in the process of migrating its technical infrastructure, so it file uploads will be temporarily disabled on all Miraheze customer wikis until further notice. We apologize for any inconvenience. Thank you</td>
+			<td style="font-size:125%">Miraheze is in the process of migrating its technical infrastructure, so file uploads have been disabled on all Miraheze wikis until further notice. We apologize for any inconvenience. Thank you for your patience and we hope to get uploads re-enabled as soon as possible.</td>
 			</tr></tbody></table>
-EOF;
-}
+		EOF;
+	}
+// }
