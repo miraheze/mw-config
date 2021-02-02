c98da215abe47a6cca241809a4b301cbcea665f1
Doug Mehus <39928941+dmehus@users.noreply.github.com>
2021-02-01 17:09:46 -0800

    Add sitenotice regarding gluster migration (#3698)
    * Add sitenotice regarding gluster migration
    
    * Trying to fix this
    
    * Adding sitenotice about gluster migration
    
    Credit @Universal_Omega
    
    * Adding sitenotice re: gluster migration
    
    Credit to @Universal_Omega

--- Sitenotice.php
+++ Sitenotice.php
@@ -1,6 +1,6 @@
 <?php
 
-if ( $wmgSiteNoticeOptOut ) {
+ if ( $wmgSiteNoticeOptOut ) {
 	// only show important notices when optout
 	$wi->config->settings['wgNoticeProject']['default'] = 'optout';
 }
@@ -8,15 +8,15 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 55;
+$wgMajorSiteNoticeID = 56;
 
-/* if ( !$wmgSiteNoticeOptOut ) {
+//if ( !$wmgSiteNoticeOptOut ) {
 $wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter'; // show to all users
 function onSiteNoticeAfter( &$siteNotice, $skin ) {
 	global $wmgSiteNoticeOptOut, $snImportant;
 	$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td style="font-size:125%">Miraheze has updated the recent <a href="https://meta.miraheze.org/wiki/23-12-2020_Security_Disclosure">Security Disclosure</a> to strongly advise ALL users immediately reset their passwords and 2FA tokens for Miraheze Wikis.</td>
+			<td style="font-size:125%">Miraheze is in the process of migrating its technical infrastructure, so it file uploads will be temporarily disabled on all Miraheze customer wikis until further notice. We apologize for any inconvenience. Thank you</td>
 			</tr></tbody></table>
 EOF;
-} */
+}
