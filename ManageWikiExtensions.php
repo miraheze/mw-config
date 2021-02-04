7300e94483b0f92711d699a0594b1be7a19d68eb
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-02-04 11:36:49 -0700

    DynamicPageList3: run script on install

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -615,6 +615,11 @@ $wgManageWikiExtensions = [
 			'var' => 'wmgUseDynamicPageList3',
 			'conflicts' => 'dynamicpagelist',
 			'requires' => [],
+			'install' => [
+				'mwscript' => [
+ 					"$IP/extensions/DynamicPageList3/classes/db/CreateTemplateUpdateMaintenance.php" => [],
+				],
+			],
 		],
 		'dynamicsidebar' => [
 			'name' => 'DynamicSidebar',
