488193084893ccefd6ef6a4f7fb491de4095a8e5
paladox <paladox@users.noreply.github.com>
2021-02-05 17:50:53 +0000

    Switch over wikis beginning with v to new file storage

--- LocalWiki.php
+++ LocalWiki.php
@@ -3,7 +3,7 @@
 // All group of wikis/tag specific things should go at the top. Below the file, custom wiki config starts.
 
 // Support migration to new volume
-if ( preg_match( '/^(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|x|y|z|0|1|2|3|4|5|6|7|8|9)(.+)wiki$/', $wi->dbname ) ) {
+if ( preg_match( '/^(a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|p|q|r|s|t|u|v|x|y|z|0|1|2|3|4|5|6|7|8|9)(.+)wiki$/', $wi->dbname ) ) {
 	$wgUploadDirectory = "/mnt/mediawiki-static-new/$wgDBname";
 	$wi->config->settings['wgDataDumpDirectory']['default'] = "/mnt/mediawiki-static-new/{$wi->dbname}/dumps/";
 }
