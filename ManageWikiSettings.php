354f0c9887873c0ff905656f794a8cf5e7034126
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-02-28 09:39:14 -0700

    Update comment

--- ManageWikiSettings.php
+++ ManageWikiSettings.php
@@ -2,7 +2,7 @@
 /**
  * ManageWiki settings are added using the variable below.
  *
- * name: the displayed name of the setting on Special:ManageWiki.
+ * name: the displayed name of the setting on Special:ManageWiki/settings.
  * from: a text entry of which extension is required for this setting to work. If added by MediaWiki or a 'global' extension, use 'mediawiki'.
  * type: configuration type. See below for available options.
  * overridedefault: a string/array override default when no existing value exist.
