2aaf16747cca7c223b8a480c5b1ad3927bb1d5b9
paladox <thomasmulhall410@yahoo.com>
2021-03-15 13:21:20 +0000

    Revert "Switch to nutcracker for memcached"
    This reverts commit 7737ff33f7ae6e8471dd205f5c826f865ee69cda.

--- LocalSettings.php
+++ LocalSettings.php
@@ -3423,7 +3423,7 @@ $wi->config->settings += [
 	'wmgCacheSettings' => [
 		'default' => [
 			'memcached' => [
-				'server' => '127.0.0.1:11212',
+				'server' => '51.195.236.245:11211',
 			],
 			'jobrunner' => [
 				'server' => '51.195.236.220:6379',
