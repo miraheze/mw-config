27a7341f5abef0bb577a6176b9f76501cae6b9b2
paladox <paladox@users.noreply.github.com>
2021-03-17 18:46:08 +0000

    Update GlobalCache.php

--- GlobalCache.php
+++ GlobalCache.php
@@ -5,11 +5,12 @@ $wgMemCachedPersistent = false;
 // Set timeout to 500ms (in microseconds)
 $wgMemCachedTimeout = 0.5 * 1e6;
 
-$wgObjectCaches['memcached'] = [
+
+$wgObjectCaches['memcached-mem-1'] = [
 	'class'                => 'MemcachedPhpBagOStuff',
 	'serializer'           => 'php',
 	'persistent'           => false,
-	'servers'              => [ $wmgCacheSettings['memcached']['server'] ],
+	'servers'              => [ $wmgCacheSettings['memcached']['server'][0] ],
 	// Effectively disable the failure limit (0 is invalid)
 	'server_failure_limit' => 1e9,
 	// Effectively disable the retry timeout
@@ -18,14 +19,27 @@ $wgObjectCaches['memcached'] = [
 	'timeout'              => $wgMemCachedTimeout,
 ];
 
-$wgMainCacheType = 'memcached';
-$wgSessionCacheType = 'memcached';
+$wgObjectCaches['memcached-mem-2'] = [
+	'class'                => 'MemcachedPhpBagOStuff',
+	'serializer'           => 'php',
+	'persistent'           => false,
+	'servers'              => [ $wmgCacheSettings['memcached']['server'][1] ],
+	// Effectively disable the failure limit (0 is invalid)
+	'server_failure_limit' => 1e9,
+	// Effectively disable the retry timeout
+	'retry_timeout'        => -1,
+	'loggroup'             => 'memcached',
+	'timeout'              => $wgMemCachedTimeout,
+];
+
+$wgMainCacheType = 'memcached-mem-1';
+$wgSessionCacheType = 'memcached-mem-1';
 $wgSessionsInObjectCache = true;
 
-$wgMessageCacheType = CACHE_DB;
+$wgMessageCacheType = 'memcached-mem-2';
 $wgUseLocalMessageCache = true;
-$wgParserCacheType = CACHE_DB;
-$wgLanguageConverterCacheType = CACHE_DB;
+$wgParserCacheType = 'memcached-mem-2';
+$wgLanguageConverterCacheType = 'memcached-mem-2';
 
 $jobrunnerSettings = $wmgCacheSettings['jobrunner'];
 $wgJobTypeConf['default'] = [
