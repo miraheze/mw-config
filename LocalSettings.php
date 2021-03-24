8ae76e283ed245b8d9c0b4d98fc8962c54a77a00
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-03-24 17:42:24 -0600

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -3056,7 +3056,7 @@ $wi->config->settings += [
 		'idolish7wiki' => true,
 	],
 	'wgShellRestrictionMethod' => [
-		'default' => 'autodetect',
+		'default' => 'firejail',
 	],
 	'wmgWhitelistRead' => [
 		'default' => false,
