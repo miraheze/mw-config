c1a9abbfa882b33dde478bac53c9b87aee5e54b4
Reception123 <utilizator.receptie123@gmail.com>
2021-04-29 22:00:26 +0200

    re-enable managewiki backups (#3867)

--- LocalWiki.php
+++ LocalWiki.php
@@ -109,6 +109,23 @@ $wi->config->settings['wgDataDump']['default'] = [
 			'delete' => 'delete-dump',
 		],
 	],
+	'managewiki_backup' => [
+		'file_ending' => '.json',
+		'generate' => [
+			'type' => 'mwscript',
+			'script' => "$IP/extensions/MirahezeMagic/maintenance/generateManageWikiBackup.php",
+			'options' => [
+				'--filename',
+				'${filename}'
+			],
+		],
+		'limit' => 1,
+		'permissions' => [
+			'view' => 'view-dump',
+			'generate' => 'generate-dump',
+			'delete' => 'delete-dump',
+		],
+	],
 ];
 
 // CookieWarning exempt ElectronPdfService
