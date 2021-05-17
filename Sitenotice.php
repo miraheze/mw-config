1a17998d97e581ae49dab2616198d2c2214ff556
paladox <paladox@users.noreply.github.com>
2021-05-17 20:41:49 +0100

    Add site notice for file storage maintenance (#3908)
    * Add site notice for file storage maintenance
    
    * Update Sitenotice.php
    
    * Update Sitenotice.php
    
    * Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -8,9 +8,9 @@ if ( $wmgSiteNoticeOptOut ) {
 // Global SiteNotice
 // Increment this version number whenever you change the site notice
 // and don't comment it out
-$wgMajorSiteNoticeID = 56;
+$wgMajorSiteNoticeID = 57;
 
-/* if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter'; // show to all users
 
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
@@ -18,11 +18,11 @@ $wgMajorSiteNoticeID = 56;
 
 		$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td style="font-size:125%">We are aware of issues where some wikis are unable to upload files, or are experiencing issues in thumbnail rendering. We are trying to resolve this as fast as we can, but we don't currently have an estimated time as to when this will be resolved. We apologise for the inconvenience this may cause, and thank you for your patience.</td>
+			<td style="font-size:125%">Miraheze will be performing maintenance on May 20th, 2021 between 20:00 UTC and 20:30 UTC. During this maintenance window, you will not be able to view or upload images or videos.</td>
 			</tr></tbody></table>
 		EOF;
 	}
-} */
+}
 
 // Wrap your sitenotice with <div data-nosnippet>(sitenotice)</div>
 // or Google will use the sitenotice for their search result snippet.
