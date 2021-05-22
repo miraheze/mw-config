18e907dbd4cbbea5fd8cde27de1f3b155169546d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-21 22:22:57 -0600

    Fix Sitenotice.php formatting

--- Sitenotice.php
+++ Sitenotice.php
@@ -1,15 +1,20 @@
 <?php
 
 if ( $wmgSiteNoticeOptOut ) {
-	// only show important notices when optout
+	# Only show important notices when optout
 	$wi->config->settings['wgNoticeProject']['default'] = 'optout';
 }
 
-// Global SiteNotice
-// Increment this version number whenever you change the site notice
-// and don't comment it out
+
+# Increment this version number whenever you change the site notice
 $wgMajorSiteNoticeID = 57;
 
+/**
+* Wrap your sitenotice with <div data-nosnippet>(sitenotice)</div>
+* or Google will use the sitenotice for their search result snippet.
+*/
+
+# Global SiteNotice
 /* if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter'; // show to all users
 
@@ -24,21 +29,17 @@ $wgMajorSiteNoticeID = 57;
 	}
 } */
 
-// Wrap your sitenotice with <div data-nosnippet>(sitenotice)</div>
-// or Google will use the sitenotice for their search result snippet.
-// Specific wiki sitenotice
-/*
-if ( $wgDiscordIncomingWebhookUrl || $wgSlackIncomingWebhookUrl ) {
-$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
-function onSiteNoticeAfter( &$siteNotice, $skin ) {
-	global $wmgSiteNoticeOptOut, $snImportant;
+# Specific wiki SiteNotice
+/* if ( $wgDiscordIncomingWebhookUrl || $wgSlackIncomingWebhookUrl ) {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 
-	$siteNotice .= <<<EOF
-			<div data-nosnippet><table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td>Unfortunately, between 3 January 2021 and 28 April 2021, Discord & Slack Webhook URLs were available via the MediaWiki API due to <a href="https://github.com/miraheze/ManageWiki/security/advisories/GHSA-jmc9-rv2f-g8vv">GHSA-jmc9-rv2f-g8vv</a>. We advise you to consider resetting and replacing your Discord or Slack webhook via Special:ManageWiki/settings.</td>
-			</tr></tbody></table></div>
-EOF;
-		return true;
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
+		global $wmgSiteNoticeOptOut, $snImportant;
+
+		$siteNotice .= <<<EOF
+				<div data-nosnippet><table class="wikitable" style="text-align:center;"><tbody><tr>
+				<td>Unfortunately, between 3 January 2021 and 28 April 2021, Discord & Slack Webhook URLs were available via the MediaWiki API due to <a href="https://github.com/miraheze/ManageWiki/security/advisories/GHSA-jmc9-rv2f-g8vv">GHSA-jmc9-rv2f-g8vv</a>. We advise you to consider resetting and replacing your Discord or Slack webhook via Special:ManageWiki/settings.</td>
+				</tr></tbody></table></div>
+		EOF;
 	}
-}
-*/
+} */
