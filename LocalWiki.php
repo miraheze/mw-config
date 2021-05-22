97384bde56bc0f00d462a94b4c782ee952f614d9
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-05-21 20:32:10 -0600

    Update LocalWiki.php

--- LocalWiki.php
+++ LocalWiki.php
@@ -75,7 +75,7 @@ $wi->config->settings['wgDataDump']['default'] = [
 				'--namespaces'
 			]
 		],
-		'limit' => 0, 
+		'limit' => 1, 
 		'permissions' => [
 			'view' => 'view-dump',
 			'generate' => 'generate-dump',
