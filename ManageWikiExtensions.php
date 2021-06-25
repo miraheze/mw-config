5c75ac4a01184e13fc0821f7abe883587288f980
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-06-25 17:55:52 -0600

    Update ManageWikiExtensions.php

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -1911,6 +1911,14 @@ $wgManageWikiExtensions = [
 		'requires' => [],
 		'section' => 'skins',
 	],
+	'snap' => [
+		'name' => 'Snap!',
+		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Skin:Snap!_Wiki_Skin',
+		'var' => 'wmgUseSnap!',
+		'conflicts' => false,
+		'requires' => [],
+		'section' => 'skins',
+	],
 	'truglass' => [
 		'name' => 'Truglass',
 		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Skin:Truglass',
