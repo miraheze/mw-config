4df2088133ec12725c4a467f8b07e0c60cfcaa77
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-07-19 16:44:34 -0600

    Add viewport for libertygamewiki (#4005)

--- LocalWiki.php
+++ LocalWiki.php
@@ -233,6 +233,14 @@ if ( $wgDBname === 'erislywiki' ) {
 	}
 }
 
+if ( $wgDBname === 'libertygamewiki' ) {
+	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
+	
+	function onBeforePageDisplay( OutputPage $out ) {
+		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' );
+	}
+}
+
 if ( $wgDBname === 'metawiki' ) {
 	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
 
@@ -255,18 +263,18 @@ if ( $wgDBname === 'metawiki' ) {
 }
 
 if ( $wgDBname === 'pokemundowiki') {
-	$wgHooks['BeforePageDisplay'][] = 'loadFonts';
+	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
 
-	function loadFonts ( OutputPage $out ) {
-		$out->addLink( ['rel' => 'preconnect', 'href' => 'https://fonts.gstatic.com'] );
-		$out->addLink( ['rel' => 'stylesheet', 'href' => 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap'] );
+	function onBeforePageDisplay( OutputPage $out ) {
+		$out->addLink( [ 'rel' => 'preconnect', 'href' => 'https://fonts.gstatic.com' ] );
+		$out->addLink( [ 'rel' => 'stylesheet', 'href' => 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' ] );
 	}
 }
 
 if ( $wgDBname === 'snapwikiwiki' ) {
-	$wgHooks['BeforePageDisplay'][] = 'addViewport';
+	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
 	
-	function addViewport( OutputPage $out ) {
+	function onBeforePageDisplay( OutputPage $out ) {
 		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1' );
 	}
 }
