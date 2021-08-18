56a841a72b5c94b01bc6d1ad7ac3d97c59489583
RhinosF1 <rhinosf1@miraheze.org>
2021-08-18 17:01:07 +0100

    Database: All wikis RO for DB restart

--- Database.php
+++ Database.php
@@ -44,10 +44,10 @@ $wi->config->settings['wgLBFactoryConf']['default'] = [
 		],
 	],
 	'readOnlyBySection' => [
-		// 'DEFAULT' => 'Maintenance ongoing on the database server.',
-		//'c1' => 'Maintenance ongoing on the database server.',
-		//'c2' => 'Maintenance ongoing on the database server.',
-		//'c3' => 'Maintenance ongoing on the database server.',
-		//'c4' => 'Maintenance ongoing on the database server.',
+		'DEFAULT' => 'Maintenance ongoing on the database server.',
+		'c1' => 'Maintenance ongoing on the database server.',
+		'c2' => 'Maintenance ongoing on the database server.',
+		'c3' => 'Maintenance ongoing on the database server.',
+		'c4' => 'Maintenance ongoing on the database server.',
 	],
 ];
