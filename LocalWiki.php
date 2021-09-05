c312a76747ca310b8b0fb10615be27a939cdf1d8
paladox <paladox@users.noreply.github.com>
2021-09-05 19:57:14 -0400

    Fix setting apiThumbCacheExpiry for commons (#4072)
    * Fix setting apiThumbCacheExpiry for commons
    
    * Update LocalWiki.php
    
    * Update LocalWiki.php

--- LocalWiki.php
+++ LocalWiki.php
@@ -206,9 +206,17 @@ if ( $wgDBname !== 'commonswiki' && $wgMirahezeCommons ) {
 	];
 }
 
-foreach ( $wgForeignFileRepos as $key => $value ) {
-	if ( isset( $value['name'] ) && $value['name'] === 'wikimediacommons' ) {
-		$wgForeignFileRepos[$key]['apiThumbCacheExpiry'] = 86400;
+if ( $wgUseInstantCommons ) {
+	$wgHooks['SetupAfterCache'][] = 'onSetupAfterCache';
+
+	function onSetupAfterCache() {
+		global $wgForeignFileRepos;
+
+		foreach ( $wgForeignFileRepos as $key => $value ) {
+			if ( isset( $value['name'] ) && $value['name'] === 'wikimediacommons' ) {
+				$wgForeignFileRepos[$key]['apiThumbCacheExpiry'] = 86400;
+			}
+		}
 	}
 }
 
