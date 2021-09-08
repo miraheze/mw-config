886cf2a967f473eea499072650402e27ccb934a3
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-09-08 16:13:24 -0600

    Set Scribunto configs (#4087)
    * Set Scribunto configs
    
    * Update LocalSettings.php
    
    * Update LocalWiki.php

--- LocalWiki.php
+++ LocalWiki.php
@@ -405,3 +405,7 @@ $wi->config->settings['wgDiscordNotificationWikiUrl']['default'] = $wgServer . '
 // Slack
 $wi->config->settings['wgSlackFromName']['default'] = $wgSitename;
 $wi->config->settings['wgSlackNotificationWikiUrl']['default'] = $wgServer . '/w/';
+
+// Scribunto
+$wgScribuntoEngineConf['luasandbox']['cpuLimit'] = 10;
+$wgScribuntoEngineConf['luasandbox']['maxLangCacheSize'] = 200;
