c45336aa8fc102847a2ba317d45a0e28a8001f9f
paladox <paladox@users.noreply.github.com>
2021-09-08 21:39:09 +0100

    Switch to MediaWikiServices hook (#4085)

--- LocalWiki.php
+++ LocalWiki.php
@@ -206,9 +206,9 @@ if ( $wgDBname !== 'commonswiki' && $wgMirahezeCommons ) {
 }
 
 if ( $wgUseInstantCommons ) {
-	$wgHooks['SetupAfterCache'][] = 'onSetupAfterCache';
+	$wgHooks['MediaWikiServices'][] = 'onMediaWikiServices';
 
-	function onSetupAfterCache() {
+	function onMediaWikiServices() {
 		global $wgForeignFileRepos;
 
 		foreach ( $wgForeignFileRepos as $key => $value ) {
