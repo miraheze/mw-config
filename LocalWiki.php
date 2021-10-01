71e4bca076a1c6ae869a23de98e1b961fb06a5cf
paladox <thomasmulhall410@yahoo.com>
2021-10-01 20:04:02 +0100

    Revert "Convert LocalWiki.php to use a `switch` (#4118)"
    This reverts commit 7023e5d07928e961edf9de1b05a69561a5a4a737.

--- LocalWiki.php
+++ LocalWiki.php
@@ -1,91 +1,88 @@
 <?php
 
-# Per-wiki settings that are incompatible with LocalSettings.php
-switch ( $wi->dbname ) {
-	case 'erislywiki':
-		$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
+// Per-wiki settings that are incompatible with LocalSettings.php
 
-		function onBeforePageDisplay( OutputPage $out ) {
-			$out->addMeta( 'PreMiD_Presence', 'Erisly' );
-		}
+if ( $wgDBname === 'erislywiki' ) {
+	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
 
-		break;
-	case 'libertygamewiki':
-		$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
+	function onBeforePageDisplay( OutputPage $out ) {
+		$out->addMeta( 'PreMiD_Presence', 'Erisly' );
+	}
+}
 
-		function onBeforePageDisplay( OutputPage $out ) {
-			$out->addMeta( 'viewport', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' );
-		}
+if ( $wgDBname === 'libertygamewiki' ) {
+	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
 
-		break;
-	case 'metawiki':
-		$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
+	function onBeforePageDisplay( OutputPage $out ) {
+		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' );
+	}
+}
 
-		function onBeforePageDisplay( OutputPage $out ) {
-			$out->addMeta( 'description', 'Miraheze is an open source project that offers free MediaWiki hosting, for everyone. Request your free wiki today!' );
-			$out->addMeta( 'revisit-after', '2 days' );
-			$out->addMeta( 'keywords', 'miraheze, free, wiki hosting, mediawiki, mediawiki hosting, open source, hosting' );
-		}
+if ( $wgDBname === 'metawiki' ) {
+	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
 
-		$wgHooks['SkinBuildSidebar'][] = 'onSkinBuildSidebar';
+	function onBeforePageDisplay( OutputPage $out ) {
+		$out->addMeta( 'description', 'Miraheze is an open source project that offers free MediaWiki hosting, for everyone. Request your free wiki today!' );
+		$out->addMeta( 'revisit-after', '2 days' );
+		$out->addMeta( 'keywords', 'miraheze, free, wiki hosting, mediawiki, mediawiki hosting, open source, hosting' );
+	}
 
-		function onSkinBuildSidebar( $skin, &$bar ) {
-			$bar['miraheze-sidebar-donate'][] = [
-				'text' => $skin->msg( 'miraheze-donate' ),
-				'href' => '/wiki/Special:MyLanguage/Donate',
-				'title' => $skin->msg( 'miraheze-donate' ),
-				'id' => 'n-donate',
-			];
-		}
+	$wgHooks['SkinBuildSidebar'][] = 'onSkinBuildSidebar';
 
-		break;
-	case 'newusopediawiki':
-		$wgFilterLogTypes['comments'] = false;
-
-		break;
-	case 'pokemundowiki':
-		$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
-
-		function onBeforePageDisplay( OutputPage $out ) {
-			$out->addLink( [ 'rel' => 'preconnect', 'href' => 'https://fonts.gstatic.com' ] );
-			$out->addLink( [ 'rel' => 'stylesheet', 'href' => 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' ] );
-		}
-
-		break;
-	case '402611wiki':
-	case 'ballmediawiki':
-	case 'partyballwiki':
-	case 'polandballfanonwiki':
-	case 'polandballwikisongcontestwiki':
-	case 'polandsmallswiki':
-		$wgForeignFileRepos[] = [
-			'class' => 'ForeignDBViaLBRepo',
-			'name' => 'shared-polcomwiki',
-			'directory' => '/mnt/mediawiki-static/polcomwiki',
-			'url' => 'https://static.miraheze.org/polcomwiki',
-			'hashLevels' => $wgHashedSharedUploadDirectory ? 2 : 0,
-			'fetchDescription' => true,
-			'descriptionCacheExpiry' => 86400 * 7,
-			'thumbScriptUrl' => false,
-			'transformVia404' => !$wgGenerateThumbnailOnParse,
-			'hasSharedCache' => false,
-			'wiki' => 'polcomwiki',
-			'descBaseUrl' => 'https://commons.ballmedia.org/wiki/File:',
+	function onSkinBuildSidebar( $skin, &$bar ) {
+		$bar['miraheze-sidebar-donate'][] = [
+			'text' => $skin->msg( 'miraheze-donate' ),
+			'href' => '/wiki/Special:MyLanguage/Donate',
+			'title' => $skin->msg( 'miraheze-donate' ),
+			'id' => 'n-donate',
 		];
+	}
+}
 
-		break;
-	case 'snapwikiwiki':
-		$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
+if ( $wgDBname === 'newusopediawiki' ) {
+	$wgFilterLogTypes['comments'] = false;
+}
 
-		function onBeforePageDisplay( OutputPage $out ) {
-			$out->addMeta( 'viewport', 'width=device-width, initial-scale=1' );
-		}
+if ( $wgDBname === 'pokemundowiki' ) {
+	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
 
-		break;
-	case 'traceprojectwikiwiki':
-	case 'vgportdbwiki':
-		$wgDplSettings['allowUnlimitedCategories'] = true;
-		$wgDplSettings['allowUnlimitedResults'] = true;
+	function onBeforePageDisplay( OutputPage $out ) {
+		$out->addLink( [ 'rel' => 'preconnect', 'href' => 'https://fonts.gstatic.com' ] );
+		$out->addLink( [ 'rel' => 'stylesheet', 'href' => 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap' ] );
+	}
+}
 
-		break;
+if ( in_array( $wgDBname, [ 'polandballwikisongcontestwiki', 'polandsmallswiki', 'ballmediawiki', 'polandballfanonwiki', 'partyballwiki', '402611wiki' ] ) ) {
+	$wgForeignFileRepos[] = [
+		'class' => 'ForeignDBViaLBRepo',
+		'name' => 'shared-polcomwiki',
+		'directory' => '/mnt/mediawiki-static/polcomwiki',
+		'url' => 'https://static.miraheze.org/polcomwiki',
+		'hashLevels' => $wgHashedSharedUploadDirectory ? 2 : 0,
+		'fetchDescription' => true,
+		'descriptionCacheExpiry' => 86400 * 7,
+		'thumbScriptUrl' => false,
+		'transformVia404' => !$wgGenerateThumbnailOnParse,
+		'hasSharedCache' => false,
+		'wiki' => 'polcomwiki',
+		'descBaseUrl' => 'https://commons.ballmedia.org/wiki/File:',
+	];
+}
+
+if ( $wgDBname === 'snapwikiwiki' ) {
+	$wgHooks['BeforePageDisplay'][] = 'onBeforePageDisplay';
+
+	function onBeforePageDisplay( OutputPage $out ) {
+		$out->addMeta( 'viewport', 'width=device-width, initial-scale=1' );
+	}
+}
+
+if ( $wgDBname === 'traceprojectwikiwiki' ) {
+	$wgDplSettings['allowUnlimitedCategories'] = true;
+	$wgDplSettings['allowUnlimitedResults'] = true;
+}
+
+if ( $wgDBname === 'vgportdbwiki' ) {
+	$wgDplSettings['allowUnlimitedCategories'] = true;
+	$wgDplSettings['allowUnlimitedResults'] = true;
 }
