a5bdfa0fcaaaad2dd83d0988690f948e8ce29e0d
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-11 14:51:33 -0600

    Set wgProfiler early (#4141)

--- PhpAutoPrepend.php
+++ PhpAutoPrepend.php
@@ -1,11 +1,23 @@
 <?php
 
 function wfSetupProfiler() {
-	$forceprofile = $_GET['forceprofile'] ?? 0;
+	global $wmgProfiler;
+	$wmgProfiler = [];
 
+	/**
+	 * When using ?forceprofile=1, a profile can be found as an HTML comment
+	 * Disabled on production hosts because it seems to be causing performance issues (how ironic)
+	 */
+	$forceprofile = $_GET['forceprofile'] ?? 0;
 	if ( ( $forceprofile == 1 || PHP_SAPI === 'cli' ) && extension_loaded( 'tideways_xhprof' ) ) {
 		$xhprofFlags = TIDEWAYS_XHPROF_FLAGS_CPU | TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_NO_BUILTINS;
 		tideways_xhprof_enable( $xhprofFlags );
+
+		$wmgProfiler = [
+			'class' => 'ProfilerXhprof',
+			'flags' => $xhprofFlags,
+			'output' => 'text',
+		];
 	}
 }
 
