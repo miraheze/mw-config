ab417c3536e2d3619ba3143e50795fda2c4f9956
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-18 20:52:46 -0600

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -484,11 +484,6 @@ $wi->config->settings += [
 		'default' => '#131a21',
 	],
 
-	// Citoid
-	'wgCitoidFullRestbaseURL' => [
-		'wmgUseCitoid' => "https://{$wi->hostname}/{$wi->hostname}/",
-	],
-
 	// Comments
 	'wgCommentsDefaultAvatar' => [
 		'default' => '/w/extensions/SocialProfile/avatars/default_ml.gif',
