2aac23a49e056c1c64f467f0781c414c4a500faa
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-10-19 19:43:39 -0600

    Update ManageWikiExtensions.php

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -2069,6 +2069,14 @@ $wgManageWikiExtensions = [
 		'requires' => [],
 		'section' => 'other',
 	],
+	'articlecreationworkflow' => [
+		'name' => 'ArticleCreationWorkflow',
+		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:ArticleCreationWorkflow',
+		'var' => 'wmgUseArticleCreationWorkflow',
+		'conflicts' => false,
+		'requires' => [],
+		'section' => 'other',
+	],
 	'articleplaceholder' => [
 		'name' => 'ArticlePlaceholder',
 		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:ArticlePlaceholder',
