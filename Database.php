3efd415b507d8e87fd5247c30e8ab0bfc4d46772
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-11-24 00:44:07 -0700

    Set `beta` cluster to c3 / db12, where `test3wiki` is located (#4232)

--- Database.php
+++ Database.php
@@ -43,7 +43,7 @@ $wi->config->settings['wgLBFactoryConf']['default'] = [
 			'db11' => 1, // should echo c1
 		],
 		'beta' => [
-			'db11' => 1, // should echo c1 (for now)
+			'db12' => 1, // should echo c3 (where test3wiki is located)
 		],
 	],
 	'readOnlyBySection' => [
