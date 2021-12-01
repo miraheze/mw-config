841e470ae274d63f77cb21aa649db07dae747932
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-01 10:08:38 -0700

    Remove Southparkfan from staffwiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -1311,7 +1311,6 @@ $wi->config->settings += [
 	'wgMirahezeStaffAccessIds' => [
 		'default' => [
 			1, // John (SRE)
-			2, // Southparkfan (Board)
 			19, // Reception123 (SRE)
 			5258, // Void (SRE and Board)
 			13554, // Paladox (SRE)
