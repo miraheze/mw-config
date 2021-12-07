1f09b3e4603256aa3cc0e333176ee52e014dff5a
RhinosF1 <rhinosf1@miraheze.org>
2021-12-07 21:40:17 +0000

    Remove test3 files hack

--- LocalWiki.php
+++ LocalWiki.php
@@ -122,25 +122,6 @@ switch ( $wi->dbname ) {
 		}
 
 		break;
-	case 'test3wiki':
-		$wgLocalFileRepo = [
-			'class' => LocalRepo::class,
-			'name' => 'local',
-			'directory' => $wgUploadDirectory,
-			'scriptDirUrl' => $wgScriptPath,
-			'url' => $wgUploadBaseUrl ? $wgUploadBaseUrl . $wgUploadPath : $wgUploadPath,
-			'hashLevels' => $wgHashedUploadDirectory ? 2 : 0,
-			'thumbScriptUrl' => $wgThumbnailScriptPath,
-			'transformVia404' => !$wgGenerateThumbnailOnParse,
-			'deletedDir' => $wgDeletedDirectory,
-			'deletedHashLevels' => $wgHashedUploadDirectory ? 3 : 0,
-			'updateCompatibleMetadata' => $wgUpdateCompatibleMetadata,
-			'reserializeMetadata' => $wgUpdateCompatibleMetadata,
-			'useJsonMetadata' => true,
-			'useSplitMetadata' => true,
-		];
-
-		break;
 	case 'traceprojectwikiwiki':
 	case 'vgportdbwiki':
 		$wgDplSettings['allowUnlimitedCategories'] = true;
