1e83cdaebbc8730ceb62e36ce4ec79c3b6815739
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-10 12:22:14 -0700

    Revert "profile-mw8: directly set $wgProfiler"

--- PhpAutoPrepend.php
+++ PhpAutoPrepend.php
@@ -25,7 +25,7 @@ function wfSetupProfiler() {
 		$xhprofFlags = TIDEWAYS_XHPROF_FLAGS_CPU | TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_NO_BUILTINS;
 		tideways_xhprof_enable( $xhprofFlags );
 
-		$wgProfiler = [
+		$wmgProfiler = [
 			'class' => 'ProfilerXhprof',
 			'flags' => $xhprofFlags,
 			'running' => true,
