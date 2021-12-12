41362bc0d1380ed9bb0cab69bec0b352d3974f0d
RhinosF1 <rhinosf1@miraheze.org>
2021-12-12 11:22:07 +0000

    Update PhpAutoPrepend.php

--- PhpAutoPrepend.php
+++ PhpAutoPrepend.php
@@ -21,7 +21,7 @@ function wfSetupProfiler() {
 		];
 	}
 
-	if ( php_uname( 'n' ) === 'mw8.miraheze.org' ) {
+	if ( php_uname( 'n' ) === 'mw8' ) {
 		$xhprofFlags = TIDEWAYS_XHPROF_FLAGS_CPU | TIDEWAYS_XHPROF_FLAGS_MEMORY | TIDEWAYS_XHPROF_FLAGS_NO_BUILTINS;
 		tideways_xhprof_enable( $xhprofFlags );
 
