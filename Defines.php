3e9fd3b381daa817b98d2a23fc3d28a2702f1c0a
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-16 15:37:36 -0700

    Prevent Special:Preferences from ever being added to `$wgWhitelistRead` (#4287)

--- Defines.php
+++ Defines.php
@@ -103,6 +103,11 @@ if ( !$cwPrivate ) {
 	// Unset wgDataDumpDownloadUrl so private wikis stream the download via Special:DataDump/download
 	$wi->config->settings['wgDataDumpDownloadUrl']['default'] = '';
 	$wgWhitelistRead = explode( "\n", $wmgWhitelistRead );
+
+	// Never allow Special:Preferences to be whitelisted (T8448)
+	if ( $wgWhitelistRead['Special:Preferences'] ?? false ) {
+		unset( $wgWhitelistRead['Special:Preferences'] );
+	}
 }
 
 // Experimental Wikis
