0db01948e5ed1f20c6e86b33f5670fd334c2d45a
RhinosF1 <rhinosf1@miraheze.org>
2021-12-20 09:58:39 +0000

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -42,7 +42,7 @@ $wgPasswordSender = 'noreply@miraheze.org';
 $wmgUploadHostname = 'static.miraheze.org';
 
 $betaWikis = ['betawiki'];  // not test3 as that will be binned
-if ( in_array($wi->dbname, $betaWikis ) {
+if ( in_array($wi->dbname, $betaWikis) ) {
 	require_once '/srv/mediawiki/config/BetaSettings.php';
 } else {
 	require_once '/srv/mediawiki/config/ProductionSettings.php';
