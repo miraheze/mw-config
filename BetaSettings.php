2869169d7ea449e6330e9e92c51e2bccd663fc8b
RhinosF1 <rhinosf1@miraheze.org>
2021-12-20 09:57:11 +0000

    Create BetaSettings.php

---
+++ BetaSettings.php
@@ -0,0 +1,45 @@
+/**
+ * BetaSettings.php for Miraheze.
+ */
+
+// AbuseFilter
+$wgAbuseFilterCentralDB = 'betawiki';
+
+// BotPasswords
+$wgBotPasswordsDatabase = 'betawiki';
+
+// CentralNotice
+$wgCentralSelectedBannerDispatcher = 'https://beta.betaheze.org/w/index.php/Special:BannerLoader';
+$wgCentralBannerRecorder = 'https://beta.betaheze.org/w/index.php/Special:RecordImpression';
+$wgCentralDBname = 'betawiki';
+$wgCentralHost = 'https://beta.betaheze.org';
+
+// CheckUser
+$wgCheckUserGBtoollink['centralDB'] = 'betawiki';
+
+// Echo
+$wgEchoSharedTrackingCluster = 'beta';
+$wgEchoSharedTrackingDB = 'betawiki';
+
+// GlobalBlocking
+$wgGlobalBlockingDatabase = 'testglobal';
+
+// GlobalCssJS
+$wgGlobalCssJsConfig['source'] = 'betawiki';
+$wgGlobalCssJsConfig['wiki'] = 'betawiki';
+
+// GlobalUserPage
+$wgGlobalUserPageAPIUrl = 'https://beta.betaheze.org/w/api.php';
+$wgGlobalUserPageDBname = 'betawiki';
+
+// Interwiki
+$wgInterwikiCentralDB = 'betawiki';
+
+// OAuth
+$wgMWOAuthCentralWiki = 'betawiki';
+
+// UrlShortner
+$wgUrlShortenerDBName = 'betawiki';
+
+// Logging
+$wgDeprecationReleaseLimit = false
