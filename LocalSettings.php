f0c0443db0f1b1af81b89e251a303536b6447dd4
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-12-21 16:52:03 -0700

    Drop test3wiki from config + configure beta

--- LocalSettings.php
+++ LocalSettings.php
@@ -84,13 +84,11 @@ $wi->config->settings += [
 	],
 	'wgAbuseFilterCentralDB' => [
 		'default' => 'metawiki',
-		'test3wiki' => 'test3wiki',
-		'betawiki' => 'betawiki',
+		'beta' => 'betawiki',
 	],
 	'wgAbuseFilterIsCentral' => [
 		'default' => false,
 		'metawiki' => true,
-		'test3wiki' => true,
 		'betawiki' => true,
 	],
 	'wgAbuseFilterBlockDuration' => [
@@ -258,8 +256,7 @@ $wi->config->settings += [
 	// Bot passwords
 	'wgBotPasswordsDatabase' => [
 		'default' => 'mhglobal',
-		'test3wiki' => 'test3wiki',
-		'betawiki' => 'betawiki',
+		'beta' => 'betawiki',
 	],
 
 	// Cache
@@ -426,28 +423,23 @@ $wi->config->settings += [
 	'wgNoticeInfrastructure' => [
 		'default' => false,
 		'metawiki' => true,
-		'test3wiki' => true,
 		'betawiki' => true,
 	],
 	'wgCentralSelectedBannerDispatcher' => [
 		'default' => 'https://meta.miraheze.org/w/index.php/Special:BannerLoader',
-		'test3wiki' => 'https://test3.miraheze.org/w/index.php/Special:BannerLoader',
-		'betawiki' => 'https://beta.betaheze.org/w/index.php/Special:BannerLoader',
+		'beta' => 'https://beta.betaheze.org/w/index.php/Special:BannerLoader',
 	],
 	'wgCentralBannerRecorder' => [
 		'default' => 'https://meta.miraheze.org/w/index.php/Special:RecordImpression',
-		'test3wiki' => 'https://test3.miraheze.org/w/index.php/Special:RecordImpression',
-		'betawiki' => 'https://beta.betaheze.org/w/index.php/Special:RecordImpression',
+		'beta' => 'https://beta.betaheze.org/w/index.php/Special:RecordImpression',
 	],
 	'wgCentralDBname' => [
 		'default' => 'metawiki',
-		'test3wiki' => 'test3wiki',
-		'betawiki' => 'betawiki',
+		'beta' => 'betawiki',
 	],
 	'wgCentralHost' => [
 		'default' => 'https://meta.miraheze.org',
-		'test3wiki' => 'https://test3.miraheze.org',
-		'betawiki' => 'https://beta.betaheze.org',
+		'beta' => 'https://beta.betaheze.org',
 	],
 	'wgNoticeProject' => [
 		'default' => 'all',
@@ -488,13 +480,7 @@ $wi->config->settings += [
 				'steward',
 			],
 		],
-		'test3wiki' => [
-			'centralDB' => 'test3wiki',
-			'groups' => [
-				'steward',
-			],
-		],
-		'betawiki' => [
+		'beta' => [
 			'centralDB' => 'betawiki',
 			'groups' => [
 				'steward',
@@ -1109,13 +1095,11 @@ $wi->config->settings += [
 	],
 	'wgEchoSharedTrackingCluster' => [
 		'default' => 'echo',
-		'test3wiki' => 'beta',
-		'betawiki' => 'beta',
+		'beta' => 'beta',
 	],
 	'wgEchoSharedTrackingDB' => [
 		'default' => 'metawiki',
-		'test3wiki' => 'test3wiki',
-		'betawiki' => 'betawiki',
+		'beta' => 'betawiki',
 	],
 	'wgEchoUseCrossWikiBetaFeature' => [
 		'default' => true,
@@ -1398,12 +1382,10 @@ $wi->config->settings += [
 	'wgApplyGlobalBlocks' => [
 		'default' => true,
 		'metawiki' => false,
-		'betawiki' => true,
 	],
 	'wgGlobalBlockingDatabase' => [
 		'default' => 'mhglobal', // use mhglobal for global blocks
-		'test3wiki' => 'testglobal',
-		'betawiki' => 'testglobal',
+		'beta' => 'testglobal',
 	],
 
 	// GlobalCssJs
@@ -1412,11 +1394,7 @@ $wi->config->settings += [
 			'wiki' => 'metawiki',
 			'source' => 'metawiki',
 		],
-		'test3wiki' => [
-			'wiki' => 'test3wiki',
-			'source' => 'test3wiki',
-		],
-		'betawiki' => [
+		'beta' => [
 			'wiki' => 'betawiki',
 			'source' => 'betawiki',
 		],
@@ -1450,13 +1428,11 @@ $wi->config->settings += [
 	// GlobalUserPage
 	'wgGlobalUserPageAPIUrl' => [
 		'default' => 'https://login.miraheze.org/w/api.php',
-		'test3wiki' => 'https://test3.miraheze.org/w/api.php',
-		'betawiki' => 'https://beta.betaheze.org/w/api.php',
+		'beta' => 'https://beta.betaheze.org/w/api.php',
 	],
 	'wgGlobalUserPageDBname' => [
 		'default' => 'loginwiki',
-		'test3wiki' => 'test3wiki',
-		'betawiki' => 'betawiki',
+		'beta' => 'betawiki',
 	],
 
 	// Grant Permissions for BotPasswords and OAuth
@@ -1606,8 +1582,7 @@ $wi->config->settings += [
 	],
 	'wgInterwikiCentralDB' => [
 		'default' => 'metawiki',
-		'test3wiki' => 'test3wiki',
-		'betawiki' => 'betawiki',
+		'beta' => 'betawiki',
 	],
 	'wgExtraInterlanguageLinkPrefixes' => [
 		'default' => [
@@ -2540,7 +2515,6 @@ $wi->config->settings += [
 	'wgAllowGlobalMessaging' => [
 		'default' => false,
 		'metawiki' => true,
-		'test3wiki' => true,
 		'betawiki' => true,
 	],
 
@@ -2965,8 +2939,7 @@ $wi->config->settings += [
 	'wgMWOAuthCentralWiki' => [
 		'default' => 'metawiki',
 		'ldapwikiwiki' => false,
-		'test3wiki' => 'test3wiki',
-		'betawiki' => 'betawiki',
+		'beta' => 'betawiki',
 	],
 	'wgOAuth2GrantExpirationInterval' => [
 		'default' => 'PT4H',
@@ -3340,7 +3313,6 @@ $wi->config->settings += [
 	'wgRemovePIIAllowedWikis' => [
 		'default' => [
 			'metawiki',
-			'test3wiki',
 			'betawiki',
 		],
 	],
@@ -3976,8 +3948,7 @@ $wi->config->settings += [
 	],
 	'wgUrlShortenerDBName' => [
 		'default' => 'metawiki',
-		'test3wiki' => 'test3wiki',
-		'betawiki' => 'betawiki',
+		'beta' => 'betawiki',
 	],
 	'wgUrlShortenerAllowedDomains' => [
 		'default' => [
@@ -4392,8 +4363,7 @@ $wi->config->settings += [
 	// Control MediaWiki Deprecation Warnings
 	'wgDeprecationReleaseLimit' => [
 		'default' => '1.34',
-		'test3wiki' => false,
-		'betawiki' => false,
+		'beta' => false,
 	],
 
 	// Email notifications on privileged actions configuration
