94dc03c0283bfcddcd4f32d8597998ca6aa47f89
RhinosF1 <rhinosf1@miraheze.org>
2021-12-21 11:23:44 +0000

    PageForms: disable

--- LocalSettings.php
+++ LocalSettings.php
@@ -4416,7 +4416,7 @@ $wi->readCache();
 
 // ManageWiki settings
 require_once __DIR__ . '/ManageWikiExtensions.php';
-$wi->disabledExtensions = [];
+$wi->disabledExtensions = ['pageforms'];
 
 $wi->config->extractAllGlobals( $wi->dbname );
 $wi->loadExtensions();
