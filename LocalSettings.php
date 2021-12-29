b94b78ad6ca1b6e5a7265400c7165eeca2956410
RhinosF1 <rhinosf1@miraheze.org>
2021-12-29 16:06:51 +0000

    set $egChameleonLayoutFile on koopacabanawiki?
    Lakelimbo — Today at 16:00
    could you set $egChameleonLayoutFile= __DIR__ . '/skins/chameleon/layouts/navhead.xml'; on koopacabanawiki?

--- LocalSettings.php
+++ LocalSettings.php
@@ -482,6 +482,7 @@ $wi->config->settings += [
 	'egChameleonLayoutFile' => [
 		'default' => '/srv/mediawiki/w/skins/chameleon/layouts/standard.xml',
 		'lakehubwiki' => '/srv/mediawiki/w/skins/chameleon/layouts/fixedhead.xml',
+		'koopacabanawiki' => '/srv/mediawiki/w/skins/chameleon/layouts/navhead.xml',
 	],
 
 	// CheckUser
