db43bb2e09b7b8574de1a4717c5b80965b42da67
RhinosF1 <rhinosf1@miraheze.org>
2021-12-30 10:35:33 +0000

    Update GlobalCache.php

--- GlobalCache.php
+++ GlobalCache.php
@@ -6,7 +6,7 @@ $wgMemCachedPersistent = false;
 $wgMemCachedTimeout = 0.5 * 1e6;
 
 $ovlon = ['test3', 'mw8', 'mw9', 'mw10', 'mw11', 'mw12', 'mw13', 'mwtask1'];
-if (in_array(wfHostname(), $ovlon) {
+if (in_array(wfHostname(), $ovlon)) {
 	$wmgJobrunnerServer = '51.195.236.215:6379';
 	$wmgMem1Server = '51.195.236.223:11211';
 	$wmgMem2Server = '51.195.236.245:11211';
