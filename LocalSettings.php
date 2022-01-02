dd1398a8558ad2d9d0bf8dc599996ec7e53fbaf7
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-01 17:08:16 -0700

    test111 -> test101

--- LocalSettings.php
+++ LocalSettings.php
@@ -4537,7 +4537,7 @@ if ( $wi->missing ) {
 	require_once '/srv/mediawiki/ErrorPages/MissingWiki.php';
 }
 
-if ( in_array( wfHostname(), [ 'test3', 'test111' ] ) ) {
+if ( in_array( wfHostname(), [ 'test3', 'test101' ] ) ) {
 	// Prevent cache (better be safe than sorry)
 	$wi->config->settings['wgUseCdn']['default'] = false;
 }
