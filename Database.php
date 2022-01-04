cf6f7ae4482d48e82065f998daff27d64c9c36fa
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-04 14:58:21 -0700

    Add note about why the 'echo' cluster echo's c1

--- Database.php
+++ Database.php
@@ -49,7 +49,7 @@ $wi->config->settings['wgLBFactoryConf']['default'] = [
 	],
 	'externalLoads' => [
 		'echo' => [
-			$db1 => 1, // should echo c1
+			$db1 => 1, // should echo c1 (where metawiki is located)
 		],
 		'beta' => [
 			$db3 => 1, // should echo c4 (where betawiki is located)
