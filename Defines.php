19ad03400ba75e316cf05a627cc7331a21199d56
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-07 20:39:33 -0700

    SCSVG only

--- Defines.php
+++ Defines.php
@@ -393,15 +393,15 @@ $wgFooterIcons['copyright']['copyright'] = [
 	'alt' => $wi->config->get( 'wgRightsText', $wi->dbname ),
 ];
 
-$wgLocalisationUpdateRepositories['github'] = [
-	'mediawiki' => 'https://raw.github.com/wikimedia/mediawiki/master/%PATH%',
-	'extension' => false,
-	'skin' => false,
-];
-
 $ovlon = [ 'test3', 'mw8', 'mw9', 'mw10', 'mw11', 'mw12', 'mw13', 'mwtask1' ];
 if ( !in_array( wfHostname(), $ovlon ) ) {
 	$wgLocalisationUpdateHttpRequestOptions['proxy'] = 'http://bast.miraheze.org:8080';
+
+	$wgLocalisationUpdateRepositories['github'] = [
+		'mediawiki' => 'https://raw.github.com/wikimedia/mediawiki/master/%PATH%',
+		'extension' => false,
+		'skin' => false,
+	];
 }
 
 // Discord
