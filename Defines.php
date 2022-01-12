73fe0a48f78b545d15cf1d5d41f8db4f232aaf81
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-11 18:22:51 -0700

    fix port for `$wgShellboxUrl`

--- Defines.php
+++ Defines.php
@@ -64,7 +64,7 @@ if (
 $wgAllowedCorsHeaders[] = 'X-Miraheze-Debug';
 
 if ( wfHostname() === 'test3' ) {
-	$wgShellboxUrl = 'http://shellbox.internal/shellbox';
+	$wgShellboxUrl = 'http://shellbox.internal:8080/shellbox';
 }
 
 // Closed Wikis
