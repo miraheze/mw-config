a3a5fb1b9555666d895f4515e6f31d2fb41b4d99
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-13 13:36:26 -0700

    Remove CategoryBrowser site notice

--- Sitenotice.php
+++ Sitenotice.php
@@ -28,14 +28,14 @@ $wgMajorSiteNoticeID = 66;
 // }
 
 # Specific wiki SiteNotice
-if ( $wgUseCategoryBrowser ?? false ) {
-	$wgHooks['SiteNoticeAfter'][] = 'onCategoryBrowserSiteNotice';
+/* if ( $wgUseCategoryBrowser ?? false ) {
+	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 
-	function onCategoryBrowserSiteNotice( &$siteNotice, $skin ) {
+	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
 			<td style="font-size:125%"><div data-nosnippet>MediaWiki developers are considering removing <a href="https://www.mediawiki.org/wiki/Manual:&#36;wgUseCategoryBrowser">Category Browser (&#36;wgUseCategoryBrowser)</a>. <b>Miraheze is requesting your feedback on this so we can forward it to MediaWiki developers!</b> Let us know what you think <a href="https://meta.miraheze.org/wiki/Community_noticeboard#Request_for_Feedback:_Removal_of_$wgUseCategoryBrowser_in_MediaWiki_1.38">here</a>.</div></td>
 			</tr></tbody></table>
 		EOF;
 	}
-}
+} */
