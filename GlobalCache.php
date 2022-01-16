f9bd249a57c16753c675de4ef758649c34137b18
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-16 14:37:36 -0700

    Modify cache

--- GlobalCache.php
+++ GlobalCache.php
@@ -1,8 +1,22 @@
 <?php
 
-$wgMemCachedServers = [];
-$wgMemCachedPersistent = false;
+$wgMemCachedServers = [
+    '127.0.0.1:11212',
+    '127.0.0.1:11213',
+];
 
+$wgMainCacheType = 'memcached-pecl';
+
+$wgUseLocalMessageCache = true;
+
+$wgParserCacheType = 'db-replicated';
+$wgParserCacheExpireTime = 86400 * 7;
+
+$wgLanguageConverterCacheType = CACHE_ACCEL;
+
+$wgInvalidateCacheOnLocalSettingsChange = false;
+
+// session cache needs to be flipped for betaheze to avoid session conflicts
 $wgObjectCaches['memcached-mem-1'] = [
 	'class'                => 'MemcachedPeclBagOStuff',
 	'serializer'           => 'php',
@@ -29,27 +43,9 @@ $wgObjectCaches['memcached-mem-2'] = [
 	'timeout'              => 0.5 * 1e6, // 500ms, in microseconds
 ];
 
-$wi->config->settings['wgMainCacheType']['default'] = 'memcached-mem-2';
-$wi->config->settings['wgMainCacheType']['betaheze'] = 'memcached-mem-1';
-
 $wi->config->settings['wgSessionCacheType']['default'] = 'memcached-mem-2';
 $wi->config->settings['wgSessionCacheType']['betaheze'] = 'memcached-mem-1';
-
 $wgSessionsInObjectCache = true;
-
-$wi->config->settings['wgMessageCacheType']['default'] = 'memcached-mem-1';
-$wi->config->settings['wgMessageCacheType']['betaheze'] = 'memcached-mem-2';
-
-$wgUseLocalMessageCache = true;
-
-$wi->config->settings['wgParserCacheType']['default'] = 'memcached-mem-2';
-$wi->config->settings['wgParserCacheType']['betaheze'] = CACHE_ACCEL;
-$wgParserCacheExpireTime = 86400 * 5;
-
-$wi->config->settings['wgLanguageConverterCacheType']['default'] = 'memcached-mem-1';
-$wi->config->settings['wgLanguageConverterCacheType']['betaheze'] = 'memcached-mem-2';
-
-$wgInvalidateCacheOnLocalSettingsChange = false;
 
 $wgJobTypeConf['default'] = [
 	'class' => 'JobQueueRedis',
@@ -65,6 +61,5 @@ $wgJobTypeConf['default'] = [
 
 if ( PHP_SAPI === 'cli' ) {
 	# APC not available in CLI mode
-	$wi->config->settings['wgLanguageConverterCacheType']['default'] = CACHE_NONE;
-	$wi->config->settings['wgLanguageConverterCacheType']['betaheze'] = CACHE_NONE;
+	$wgLanguageConverterCacheType = CACHE_NONE;
 }
