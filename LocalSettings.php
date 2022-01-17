915ff10382b888a1beb21d2d477a7c4b47ed4702
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-17 15:33:51 -0700

    Set `$wgCopyUploadProxy`

--- LocalSettings.php
+++ LocalSettings.php
@@ -1339,6 +1339,9 @@ $wi->config->settings += [
 	'wgCopyUploadsFromSpecialUpload' => [
 		'default' => false,
 	],
+	'wgCopyUploadProxy' => [
+		'default' => 'http://bast.miraheze.org:8080',
+	],
 	'wgGenerateThumbnailOnParse' => [
 		'default' => false,
 	],
