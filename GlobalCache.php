9c1fd2391f3e5a4fe218b351570f17517d8d5b96
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-01-20 20:20:06 -0700

    Update GlobalCache.php

--- GlobalCache.php
+++ GlobalCache.php
@@ -9,7 +9,8 @@ $wgMainCacheType = 'memcached-pecl';
 $wgParserCacheType = 'db-replicated';
 $wgLanguageConverterCacheType = CACHE_ACCEL;
 
-$wgParserCacheExpireTime = 86400 * 10;
+$wgParserCacheExpireTime = 86400 * 10; // 10 days
+$wgRevisionCacheExpiry = 86400 * 2; // 2 days
 $wgDLPQueryCacheTime = 120;
 
 // Currently we can't set this if GroupsSidebar us used.
