d5d9124e6e946554eacaf376fd8367827528bcad
RhinosF1 <rhinosf1@miraheze.org>
2022-01-25 00:04:49 +0000

    Sitenotice: add notice about ES

--- Sitenotice.php
+++ Sitenotice.php
@@ -6,7 +6,7 @@ if ( $wmgSiteNoticeOptOut ) {
 }
 
 # Increment this version number whenever you change the site notice
-$wgMajorSiteNoticeID = 66;
+$wgMajorSiteNoticeID = 67;
 
 /**
  * Wrap your sitenotice with <div data-nosnippet>(sitenotice)</div>
@@ -28,14 +28,14 @@ $wgMajorSiteNoticeID = 66;
  } */
 
 # Specific wiki SiteNotice
-/* if ( $wgUseCategoryBrowser ?? false ) {
+if ( $wgDisableSearchUpdate ?? false ) {
 	$wgHooks['SiteNoticeAfter'][] = 'onSiteNoticeAfter';
 
 	function onSiteNoticeAfter( &$siteNotice, $skin ) {
 		$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
-			<td style="font-size:125%"><div data-nosnippet>MediaWiki developers are considering removing <a href="https://www.mediawiki.org/wiki/Manual:&#36;wgUseCategoryBrowser">Category Browser (&#36;wgUseCategoryBrowser)</a>. <b>Miraheze is requesting your feedback on this so we can forward it to MediaWiki developers!</b> Let us know what you think <a href="https://meta.miraheze.org/wiki/Community_noticeboard#Request_for_Feedback:_Removal_of_$wgUseCategoryBrowser_in_MediaWiki_1.38">here</a>.</div></td>
+			<td style="font-size:125%"><div data-nosnippet>Your wiki is currently being migrated to ElasticSearch. We've disabled updates so new pages won't show up in search while we migrate. The new search experience will be available soon./div></td>
 			</tr></tbody></table>
 		EOF;
 	}
-} */
+}
