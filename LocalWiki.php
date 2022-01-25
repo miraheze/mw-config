6126130b39ae28a0b221414dcfd68a032913c5af
RhinosF1 <rhinosf1@gmail.com>
2022-01-25 15:59:18 +0000

    Revert "Update LocalWiki.php"
    This reverts commit 56a75e62ff117ba1a8695b4fd76d0e631d0dde81.

--- LocalWiki.php
+++ LocalWiki.php
@@ -3,7 +3,7 @@
 # Per-wiki settings that are incompatible with LocalSettings.php
 switch ( $wi->dbname ) {
 	case 'betawiki':
-		wfLoadExtensions( [
+		wfLoadExtension( [
 			'GlobalWatchlist',
 			'Elastica',
 			'CirrusSearch',
