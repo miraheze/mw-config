21959bea9cca031d8185deead30c1671a9a7026e
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-03-28 11:48:54 -0600

    Try changing parsoid url to localhost

--- Defines.php
+++ Defines.php
@@ -64,7 +64,7 @@ if (
 ) {
 	wfLoadExtension( 'Parsoid', "$IP/vendor/wikimedia/parsoid/extension.json" );
 	$wgVirtualRestConfig['modules']['parsoid'] = [
-		'url' => "{$wi->server}/w/rest.php",
+		'url' => 'http://localhost/w/rest.php',
 		'domain' => $wi->server,
 		'prefix' => $wi->dbname,
 		'forwardCookies' => true,
