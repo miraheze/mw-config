57104a89c7739dce192c6fb4658ae5f56cacfcbe
Reception123 <utilizator.receptie123@gmail.com>
2022-03-28 17:50:17 +0200

    add group to restrict users from requesting wikis on Meta (#4529)
    * add group to restrict users from requesting wikis on Meta
    
    * CI: lint code to MediaWiki standards
    
    Check commit and GitHub actions for more details
    
    Co-authored-by: github-actions <github-actions@users.noreply.github.com>

--- LocalSettings.php
+++ LocalSettings.php
@@ -2435,6 +2435,9 @@ $wi->config->settings += [
 				'globalblock' => true,
 				'centralauth-lock' => true,
 			],
+			'requestwikiblocked' => [
+				'read' => true,
+			],
 			'steward' => [
 				'abusefilter-modify-global' => true,
 				'centralauth-lock' => true,
@@ -2957,10 +2960,10 @@ $wi->config->settings += [
 		],
 	],
 	'wgMFQueryPropModules' => [
-		'default' => [ 
-			'pageprops', 
-		],
-		'gratisdatawiki' => [ 
+		'default' => [
+			'pageprops',
+ ],
+		'gratisdatawiki' => [
 			'entityterms',
 		],
 	],
@@ -3242,6 +3245,11 @@ $wi->config->settings += [
 	],
 	'wgRevokePermissions' => [
 		'default' => [],
+		'+metawiki' => [
+			'requestwikiblocked' => [
+				'requestwiki' => true,
+			],
+		],
 		'+simulatorwiki' => [
 			'moderated' => [
 				'skip-moderation' => true,
