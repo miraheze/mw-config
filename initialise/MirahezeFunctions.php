01741b1b0f92d15081e050fa7ea72502ea27499c
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-11 16:54:47 -0600

    Update MirahezeFunctions.php

--- initialise/MirahezeFunctions.php
+++ initialise/MirahezeFunctions.php
@@ -80,7 +80,11 @@ class MirahezeFunctions {
 
 			return $databases;
 		} else {
-			$databasesArray = json_decode( file_get_contents( self::CACHE_DIRECTORY . "/{$dblist}.json" ), true );
+			static $databasesArray = null;
+
+			if ( $databasesArray === null ) {
+				$databasesArray = json_decode( file_get_contents( self::CACHE_DIRECTORY . "/{$dblist}.json" ), true );
+			}
 		}
 
 		if ( $database ) {
