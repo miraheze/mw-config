1a813fcadab09262400431134312da9bdc3279d2
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-15 17:32:51 -0600

    Update SemanticMediaWiki.php

--- SemanticMediaWiki.php
+++ SemanticMediaWiki.php
@@ -1,4 +1,8 @@
 <?php
 
+require_once '/srv/mediawiki/w/extensions/SemanticMediaWiki/includes/GlobalFunctions.php';
+require_once '/srv/mediawiki/w/extensions/SemanticMediaWiki/includes/SemanticMediaWiki.php';
+require_once '/srv/mediawiki/w/extensions/SemanticMediaWiki/src/ConfigPreloader.php';
+
 wfLoadExtension( 'SemanticMediaWiki' );
 enableSemantics( 'semantic-mediawiki.betaheze.org' );
