951ee9b6ed7a64c59f34e88bcd98bd40c94c128e
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-04-18 21:09:59 -0600

    Don't manually enable (#4606)

--- LocalSettings.php
+++ LocalSettings.php
@@ -5013,7 +5013,7 @@ $wi->readCache();
 
 // ManageWiki settings
 require_once __DIR__ . '/ManageWikiExtensions.php';
-$wi->disabledExtensions = [ 'semanticmediawiki', 'createredirect', 'regexfunctions' ];
+$wi->disabledExtensions = [ 'createredirect', 'regexfunctions' ];
 
 $wgConf->extractAllGlobals( $wi->dbname );
 $wi->loadExtensions();
