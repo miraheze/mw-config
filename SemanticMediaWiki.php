2a4bdbb50759e29d8d05eb09686c5d0d8c929df5
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-29 11:18:31 -0600

    Update SemanticMediaWiki.php

--- SemanticMediaWiki.php
+++ SemanticMediaWiki.php
@@ -1,6 +1,26 @@
 <?php
 
 $smwgUpgradeKey = 'smw:2022-04-15';
+
+$smwgNamespacesWithSemanticLinks => [
+	NS_MAIN => true,
+	NS_TALK => false,
+	NS_USER => true,
+	NS_USER_TALK => false,
+	NS_PROJECT => true,
+	NS_PROJECT_TALK => false,
+	NS_FILE => true,
+	NS_FILE_TALK => false,
+	NS_MEDIAWIKI => false,
+	NS_MEDIAWIKI_TALK => false,
+	NS_TEMPLATE => false,
+	NS_TEMPLATE_TALK => false,
+	NS_HELP => true,
+	NS_HELP_TALK => false,
+	NS_CATEGORY => true,
+	NS_CATEGORY_TALK => false,
+];
+
 $smwgPageSpecialProperties = [
 	'_MDAT',
 	'_MIME',
