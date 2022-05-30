5b34257c6657642d5bdb7dadd3fd64a08e96133c
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-05-30 15:28:37 -0600

    Update SemanticMediaWiki.php

--- SemanticMediaWiki.php
+++ SemanticMediaWiki.php
@@ -31,5 +31,4 @@ $smwgPageSpecialProperties = [
 if ( !class_exists( SMW\Setup::class ) ) {
 	require_once "$IP/extensions/SemanticMediaWiki/src/MediaWiki/HookDispatcherAwareTrait.php";
 	require_once "$IP/extensions/SemanticMediaWiki/src/Setup.php";
-
 }
