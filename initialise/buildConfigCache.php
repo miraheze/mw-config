2b023492a5fcaccd0bcfad368b6f289506dea3eb
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-14 14:58:37 -0600

    Update buildConfigCache.php

--- initialise/buildConfigCache.php
+++ initialise/buildConfigCache.php
@@ -4,6 +4,8 @@ require_once __DIR__ . '/../ManageWikiExtensions.php';
 require_once __DIR__ . '/MirahezeFunctions.php';
 require_once __DIR__ . '/StaticSiteConfiguration.php';
 
+$wi = new MirahezeFunctions();
+
 foreach ( MirahezeFunctions::getLocalDatabases() as $wiki ) {
 	$conf = new StaticSiteConfiguration();
 	MirahezeFunctions::getConfigGlobals( $wiki, $conf );
