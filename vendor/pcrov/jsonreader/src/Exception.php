fa48f8b22a9d6df6100a66e8381d06c41506fb20
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-14 23:07:13 -0600

    Delete vendor directory

--- vendor/pcrov/jsonreader/src/Exception.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace pcrov\JsonReader;
-
-class Exception extends \Exception
-{
-
-}
--- vendor/pcrov/jsonreader/src/InvalidArgumentException.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace pcrov\JsonReader;
-
-class InvalidArgumentException extends Exception
-{
-
-}
