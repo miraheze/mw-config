02dbfee7ea109d6e5890207089a7d661f70be1f2
Universal-Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-14 19:49:31 -0600

    Revert "CI: lint code to MediaWiki standards"
    This reverts commit 8b67623a56681aed5c45dec0d7dbbfc14bfa6f02.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,15 +25,38 @@ class ComposerAutoloaderInite6d5d8a02ce70f28695a65c0af0ed809
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInite6d5d8a02ce70f28695a65c0af0ed809', 'loadClassLoader'), true, false);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInite6d5d8a02ce70f28695a65c0af0ed809', 'loadClassLoader'));
 
-        require __DIR__ . '/autoload_static.php';
-        call_user_func(\Composer\Autoload\ComposerStaticInite6d5d8a02ce70f28695a65c0af0ed809::getInitializer($loader));
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
+        if ($useStaticLoader) {
+            require __DIR__ . '/autoload_static.php';
+
+            call_user_func(\Composer\Autoload\ComposerStaticInite6d5d8a02ce70f28695a65c0af0ed809::getInitializer($loader));
+        } else {
+            $map = require __DIR__ . '/autoload_namespaces.php';
+            foreach ($map as $namespace => $path) {
+                $loader->set($namespace, $path);
+            }
+
+            $map = require __DIR__ . '/autoload_psr4.php';
+            foreach ($map as $namespace => $path) {
+                $loader->setPsr4($namespace, $path);
+            }
+
+            $classMap = require __DIR__ . '/autoload_classmap.php';
+            if ($classMap) {
+                $loader->addClassMap($classMap);
+            }
+        }
 
         $loader->register(false);
 
-        $includeFiles = \Composer\Autoload\ComposerStaticInite6d5d8a02ce70f28695a65c0af0ed809::$files;
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInite6d5d8a02ce70f28695a65c0af0ed809::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequiree6d5d8a02ce70f28695a65c0af0ed809($fileIdentifier, $file);
         }
@@ -42,16 +65,11 @@ class ComposerAutoloaderInite6d5d8a02ce70f28695a65c0af0ed809
     }
 }
 
-/**
- * @param string $fileIdentifier
- * @param string $file
- * @return void
- */
 function composerRequiree6d5d8a02ce70f28695a65c0af0ed809($fileIdentifier, $file)
 {
     if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
         require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
