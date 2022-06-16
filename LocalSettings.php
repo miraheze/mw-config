1f0f2d91eb3f843daf49eef16824f52bea287430
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2022-06-15 20:22:00 -0600

    Remove $wgVectorSkinMigrationMode

--- LocalSettings.php
+++ LocalSettings.php
@@ -4421,9 +4421,6 @@ $wgConf->settings += [
 	],
 
 	// Vector
-	'wgVectorSkinMigrationMode' => [
-		'default' => true, // Remove with 1.39
-	],
 	'wgVectorDefaultSkinVersion' => [
 		'default' => '2', // Remove with 1.39
 	],
