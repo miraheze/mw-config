a13476bf44eb7fd7794d2a15ef4619f8da6604f5
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-06-18 18:41:26 -0600

    T9415: set Chameleon layout to fixedhead for obeymewiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -450,6 +450,7 @@ $wgConf->settings += [
 	'egChameleonLayoutFile' => [
 		'default' => '/srv/mediawiki/w/skins/chameleon/layouts/standard.xml',
 		'lakehubwiki' => '/srv/mediawiki/w/skins/chameleon/layouts/fixedhead.xml',
+		'obeymewiki' => '/srv/mediawiki/w/skins/chameleon/layouts/fixedhead.xml',
 		'koopacabanawiki' => '/srv/mediawiki/w/skins/chameleon/layouts/navhead.xml',
 	],
 
