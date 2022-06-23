3ea89d7e78830bbf0b2e07eacb3cc4e1235141db
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-06-23 16:50:34 -0600

    $wgEnablePartialBlocks -> `$wgEnablePartialActionBlocks`

--- LocalSettings.php
+++ LocalSettings.php
@@ -245,7 +245,7 @@ $wgConf->settings += [
 	'wgEnableBlockNoticeStats' => [
 		'default' => false,
 	],
-	'wgEnablePartialBlocks' => [
+	'wgEnablePartialActionBlocks' => [
 		'default' => true,
 	],
 
