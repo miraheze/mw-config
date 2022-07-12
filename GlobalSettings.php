ee558e0e5d0e637b5764052bb3284140afe7555d
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-07-12 17:08:10 -0600

    Update GlobalSettings.php

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -386,7 +386,6 @@ $vectorVersion = $wgDefaultSkin === 'vector-2022' ? '2' : '1';
 
 $wgVectorDefaultSkinVersion = $vectorVersion; // Remove with 1.39
 $wgVectorDefaultSkinVersionForExistingAccounts = $vectorVersion;
-$wgVectorDefaultSkinVersionForNewAccounts = $vectorVersion;
 
 // Don't need a global here
 unset( $vectorVersion );
