8e6fed3ea420b9db419130183be614ead83a084b
Chima Ugo <81777765+ugochimobi@users.noreply.github.com>
2022-08-30 03:17:11 +0100

    fix sitenotice (#4881)
    I presume this is how it should look originally?

--- Sitenotice.php
+++ Sitenotice.php
@@ -21,7 +21,7 @@ if ( !$wmgSiteNoticeOptOut ) {
 		$siteNotice .= <<<EOF
 			<table style="width: 100% !important;">
 			<tbody><tr>
-			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px; color: black;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="MediaWiki 1.38" src="https://upload.wikimedia.org/wikipedia/commons/8/8c/MediaWiki-2020-large-icon.svg" decoding="async" width="50" height="50"></div> Miraheze will be performing necessary database maintenace tomorrow (August 30th 2022) between 2000-2030 UTC. During this period wikis will be inaccessible for a period of up to 10 minutes while restarts are done.</div></div>
+			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px; color: black;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="MediaWiki 1.38" src="https://upload.wikimedia.org/wikipedia/commons/8/8c/MediaWiki-2020-large-icon.svg" decoding="async" width="50" height="50"></div> Miraheze will be performing necessary database maintenace tomorrow (August 30th 2022) between 20:00-20:30 UTC. During this period wikis will be inaccessible for a period of up to 10 minutes while restarts are done.</div></div>
 			</td></tr>
 			</tbody></table>
 		EOF;
