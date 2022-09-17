80a311f069543f1398a14ef21db8adb8711a3129
Reception123 <utilizator.receptie123@gmail.com>
2022-09-17 10:05:08 +0200

    wikiforum disable notice

--- Sitenotice.php
+++ Sitenotice.php
@@ -29,15 +29,12 @@ $wgMajorSiteNoticeID = 72;
 } */
 
 // Specific wiki SiteNotice
-/* if ( $wi->isAnyOfExtensionsActive( 'Vector' ) ) {
+if ( $wi->isAnyOfExtensionsActive( 'WikiForum' ) ) {
 	$wgHooks['SiteNoticeAfter'][] = static function ( &$siteNotice, $skin ) {
 		$siteNotice .= <<<EOF
-			<table style="width: 100% !important;">
-			<tbody><tr>
-			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="Advisory" src="https://upload.wikimedia.org/wikipedia/commons/c/ca/OOjs_UI_icon_info.svg" decoding="async" width="50" height="50"></div>MediaWiki 1.38 intoduced new changes to Vector which includes a new design. If you wish to revert to go back to Legacy Vector, go to Special:Preferences and select Legacy Vector. To rollback your wiki, go to Special:ManageWiki/settings -> Styling -> Default skin and set the default skin to 'vector' (not 'vector-2022').</div></div>
-			<div style="text-align:center;"><a href="https://meta.miraheze.org/wiki/Special:MyLanguage/MediaWiki/1.38" title="MediaWiki 1.38"><span class="mw-ui-button mw-ui-progressive mw-ui-small">Click here to learn more about the upgrade!</span></a></div></div>
-			</td></tr>
-			</tbody></table>
+			<table class="wikitable" style="text-align:center;"><tbody><tr>
+			<td style="font-size:125%"><div data-nosnippet>Due to a security vulnerability, WikiForum has been temporarily disabled on all wikis. </div></td>
+			</tr></tbody></table>
 		EOF;
 	};
-} */
+} 
