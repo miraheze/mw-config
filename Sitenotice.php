c2429724b318845deac2ab673f9f29317d828cb3
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-19 09:26:42 -0600

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -16,14 +16,13 @@ $wgMajorSiteNoticeID = 74;
 
 // Global SiteNotice
 // if ( !$wmgSiteNoticeOptOut ) {
-	// show to all users
-	$wgHooks['SiteNoticeAfter'][] = 'wfOnSiteNoticeAfter';
+	$wgHooks['SiteNoticeAfter'][] = 'wfGlobalSiteNotice';
 
-	function wfOnSiteNoticeAfter( &$siteNotice, $skin ) {
+	function wfGlobalSiteNotice( &$siteNotice, $skin ) {
 		$siteNotice .= <<<EOF
 			<table style="width: 100% !important;">
 			<tbody><tr>
-			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px; color: black;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div> Miraheze will be performing server maintenance on Monday, 26 September 2022 between approximately 18:00-21:00 UTC. During this period wikis may be inaccessible.</div></div>
+			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px; color: black;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div> Miraheze will be performing server maintenance on Monday, 26 September 2022 between approximately 18:00-21:00 UTC. During this period wikis may be intermittently inaccessible.</div></div>
 			</td></tr>
 			</tbody></table>
 		EOF;
@@ -33,9 +32,9 @@ $wgMajorSiteNoticeID = 74;
 
 // Specific wiki SiteNotice
 if ( $wi->isExtensionActive( 'WikiForum' ) ) {
-	$wgHooks['SiteNoticeAfter'][] = 'wfOnConditionalSiteNoticeAfter';
+	$wgHooks['SiteNoticeAfter'][] = 'wfConditionalSiteNotice';
 
-	function wfOnConditionalSiteNoticeAfter( &$siteNotice, $skin ) {
+	function wfConditionalSiteNotice( &$siteNotice, $skin ) {
 		$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
 			<td style="font-size:125%"><div data-nosnippet>Due to a security vulnerability, WikiForum has been temporarily disabled on all wikis. </div></td>
