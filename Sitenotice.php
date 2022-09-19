75c22701a82c0a4aea7720c690bf8cdb8a6b6731
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-09-19 09:53:09 -0600

    Add maintenance sitenotice (#4906)

--- Sitenotice.php
+++ Sitenotice.php
@@ -7,7 +7,7 @@ if ( $wmgSiteNoticeOptOut ) {
 }
 
 // Increment this version number whenever you change the site notice
-$wgMajorSiteNoticeID = 73;
+$wgMajorSiteNoticeID = 74;
 
 /**
  * Wrap your sitenotice with <div data-nosnippet>(sitenotice)</div>
@@ -15,26 +15,30 @@ $wgMajorSiteNoticeID = 73;
  */
 
 // Global SiteNotice
-/* if ( !$wmgSiteNoticeOptOut ) {
-	// show to all users
-	$wgHooks['SiteNoticeAfter'][] = static function ( &$siteNotice, $skin ) {
+// if ( !$wmgSiteNoticeOptOut ) {
+	$wgHooks['SiteNoticeAfter'][] = 'wfGlobalSiteNotice';
+
+	function wfGlobalSiteNotice( &$siteNotice, $skin ) {
 		$siteNotice .= <<<EOF
 			<table style="width: 100% !important;">
 			<tbody><tr>
-			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px; color: black;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="MediaWiki 1.38" src="https://upload.wikimedia.org/wikipedia/commons/8/8c/MediaWiki-2020-large-icon.svg" decoding="async" width="50" height="50"></div> Miraheze will be performing necessary database maintenace tomorrow (August 30th 2022) between 20:00-20:30 UTC. During this period wikis will be inaccessible for a period of up to 10 minutes while restarts are done.</div></div>
+			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px; color: black;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div> Miraheze will be performing server maintenance on Monday, 26 September 2022 between approximately 18:00-21:00 UTC. During this period wikis may be intermittently inaccessible.</div></div>
 			</td></tr>
 			</tbody></table>
 		EOF;
-	};
-} */
+	}
+
+// }
 
 // Specific wiki SiteNotice
 if ( $wi->isExtensionActive( 'WikiForum' ) ) {
-	$wgHooks['SiteNoticeAfter'][] = static function ( &$siteNotice, $skin ) {
+	$wgHooks['SiteNoticeAfter'][] = 'wfConditionalSiteNotice';
+
+	function wfConditionalSiteNotice( &$siteNotice, $skin ) {
 		$siteNotice .= <<<EOF
 			<table class="wikitable" style="text-align:center;"><tbody><tr>
 			<td style="font-size:125%"><div data-nosnippet>Due to a security vulnerability, WikiForum has been temporarily disabled on all wikis. </div></td>
 			</tr></tbody></table>
 		EOF;
-	};
+	}
 }
