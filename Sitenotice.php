be51d44bc2667dfc24a2b5287bc6646737bc0a4c
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-10-06 17:08:02 -0600

    Minor grammatical correction

--- Sitenotice.php
+++ Sitenotice.php
@@ -22,7 +22,7 @@ $wgMajorSiteNoticeID = 76;
 		$siteNotice .= <<<EOF
 			<table style="width: 100% !important;">
 			<tbody><tr>
-			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px; color: black;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div>Due to a security issue all users have been logged out, and you will need to login again. We apologise if this is inconvenience. If you experience any login issues, please let us know at <a href="https://meta.miraheze.org/wiki/Phabricator">Phabricator</a>, <a href="https://meta.miraheze.org/wiki/Discord">Discord</a>, or <a href="https://meta.miraheze.org/wiki/IRC">IRC</a>. Thank you!</div></div>
+			<td style="font-size: 120%; border-left: 4px solid #67440F; background-color: #FFF2F6; padding: 10px 15px; color: black;"><div style="padding-top:0.3em; padding-bottom:0.1em;"><div data-nosnippet><div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div>Due to a security issue all users have been logged out, and you will need to login again. We apologise if this is an inconvenience. If you experience any login issues, please let us know at <a href="https://meta.miraheze.org/wiki/Phabricator">Phabricator</a>, <a href="https://meta.miraheze.org/wiki/Discord">Discord</a>, or <a href="https://meta.miraheze.org/wiki/IRC">IRC</a>. Thank you!</div></div>
 			</td></tr>
 			</tbody></table>
 		EOF;
