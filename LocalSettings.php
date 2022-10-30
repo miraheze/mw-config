3e2163136f96b23feab8b1444dd978a84b601003
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-10-30 15:39:08 -0600

    Revert "Use static.miraheze.org for default $wgLogo" (#4958)
    Reverts miraheze/mw-config#4942

--- LocalSettings.php
+++ LocalSettings.php
@@ -4267,7 +4267,7 @@ $wgConf->settings += [
 		'default' => '/favicon.ico',
 	],
 	'wgLogo' => [
-		'default' => "https://static.miraheze.org/metawiki/3/35/Miraheze_Logo.svg",
+		'default' => "https://$wmgUploadHostname/metawiki/3/35/Miraheze_Logo.svg",
 	],
 	'wgIcon' => [
 		'default' => false,
