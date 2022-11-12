77576258eae4ccb9a1f454137473c36ac7e587e8
CosmicAlpha <cosmic.alpha24@gmail.com>
2022-11-12 11:09:46 -0700

    Update FileBackend.php

--- FileBackend.php
+++ FileBackend.php
@@ -19,6 +19,10 @@ $wgFileBackends[] = [
 	'reqTimeout'          => 900,
 ];
 
+if ( !$wmgEnableSwift ) {
+	$wgThumbPath = '/w/thumb_handler.php';
+}
+
 // Only use swift as the backend if enabled.
 if ( $wmgEnableSwift ) {
 	$wgGenerateThumbnailOnParse = false;
