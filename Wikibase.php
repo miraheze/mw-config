2489085e05d683bc3fe212f9a69edf3cb64571dd
Chima Ugo <81777765+ugochimobi@users.noreply.github.com>
2022-12-03 23:04:09 +0100

    disable localClientDatabases, enable allowDataAccessInUserLanguage on gratisdatawiki (#4998)

--- Wikibase.php
+++ Wikibase.php
@@ -237,11 +237,6 @@ if ( $wgDBname === 'gratisdatawiki' ) {
 		'http://gratisdata.miraheze.org/entity/Q982' => 'triton',
 		'http://gratisdata.miraheze.org/entity/Q2123' => 'pluto',
 	];
-	$wgWBRepoSettings['localClientDatabases'] = [
-		'gratispaideia' => 'gratispaideiawiki',
-		'gpcommons' => 'gpcommonswiki',
-		'benpedia' => 'benpediawiki',
-	];
 	$wgWBRepoSettings['updateRepoTags'] = [
 		'client-automatic-update'
 	];
@@ -269,6 +264,8 @@ if ( $wgDBname === 'gratisdatawiki' ) {
 	$wgWBRepoSettings['useKartographerGlobeCoordinateFormatter'] = true;
 	// Data-Bridge
 	$wgWBRepoSettings['dataBridgeEnabled'] = true;
+	$wgWBRepoSettings['allowDataAccessInUserLanguage'] = true;
+	$wgWBRepoSettings['entityAccessLimit'] = 500;
 }
 
 if ( $wgDBname === 'gratispaideiawiki' ) {
