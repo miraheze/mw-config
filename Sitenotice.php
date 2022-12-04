0c372753f7898f88c0a2b2d218223e5777873862
paladox <paladox@users.noreply.github.com>
2022-12-04 21:57:28 +0000

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -48,39 +48,33 @@ $wgMajorSiteNoticeID = 78;
 	}
 }*/
 
-// Specific wiki SiteNotice
-if (
-	$wmgEnableSwift &&
-	!preg_match( '/^([0-9]|a|b|c|d|e|f|g|h|i|j|k|l|m|n|o|q)/', $wgDBname )
-) {
-	$wgHooks['SiteNoticeAfter'][] = 'wfConditionalSiteNotice';
+$wgHooks['SiteNoticeAfter'][] = 'wfConditionalSiteNotice';
 
-	function wfConditionalSiteNotice( &$siteNotice, $skin ) {
-		$skin->getOutput()->enableOOUI();
-		$skin->getOutput()->addInlineStyle(
-			'.mw-dismissable-notice .mw-dismissable-notice-body { margin: unset; }' .
-			'.skin-cosmos #sitenotice-learnmore-button { margin-left: 50px; }'
-		);
+function wfConditionalSiteNotice( &$siteNotice, $skin ) {
+	$skin->getOutput()->enableOOUI();
+	$skin->getOutput()->addInlineStyle(
+		'.mw-dismissable-notice .mw-dismissable-notice-body { margin: unset; }' .
+		'.skin-cosmos #sitenotice-learnmore-button { margin-left: 50px; }'
+	);
 
-		$siteNotice .= <<<EOF
-			<table style="width: 100%;">
-				<tbody><tr><td style="font-size: 120%; border-left: 4px solid #ff1e00; background-color: #ff5200cf; padding: 10px 15px; color: whitesmoke;">
-					<div data-nosnippet style="padding-top:0.3em; padding-bottom:0.1em;">
-						<div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div>
-						<div style="padding-bottom: 15px; font-size: 13pt; font-weight: bold;">
-							This wiki has been migrated to our new file storage software (Swift). If files appear missing for this wiki, please let us know by <a href="https://phabricator.miraheze.org/maniphest/task/edit/form/1">creating a task on Phabricator</a>. Thank you.
-						</div>
-
-						<span id="sitenotice-learnmore-button" class="oo-ui-widget oo-ui-widget-enabled oo-ui-buttonElement oo-ui-buttonElement-framed oo-ui-iconElement oo-ui-labelElement oo-ui-buttonWidget">
-							<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="https://meta.miraheze.org/wiki/Community_noticeboard#Note_from_SRE_Regarding_the_Swift_Migration">
-								<span class="oo-ui-iconElement-icon oo-ui-icon-notice"></span>
-								<span class="oo-ui-labelElement-label">{$skin->msg( 'miraheze-sitenotice-learnmore' )->escaped()}</span>
-								<span class="oo-ui-indicatorElement-indicator oo-ui-indicatorElement-noIndicator"></span>
-							</a>
-						</span>
+	$siteNotice .= <<<EOF
+		<table style="width: 100%;">
+			<tbody><tr><td style="font-size: 120%; border-left: 4px solid #ff1e00; background-color: #ff5200cf; padding: 10px 15px; color: whitesmoke;">
+				<div data-nosnippet style="padding-top:0.3em; padding-bottom:0.1em;">
+					<div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/b/b7/Miraheze-Logo.svg" decoding="async" width="50" height="50"></div>
+					<div style="padding-bottom: 15px; font-size: 13pt; font-weight: bold;">
+						This wiki has been migrated to our new file storage software (Swift). If files appear missing for this wiki, please let us know by <a href="https://phabricator.miraheze.org/maniphest/task/edit/form/1">creating a task on Phabricator</a>. Thank you.
 					</div>
-				</td></tr></tbody>
-			</table>
-		EOF;
-	}
+
+					<span id="sitenotice-learnmore-button" class="oo-ui-widget oo-ui-widget-enabled oo-ui-buttonElement oo-ui-buttonElement-framed oo-ui-iconElement oo-ui-labelElement oo-ui-buttonWidget">
+						<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="https://meta.miraheze.org/wiki/Community_noticeboard#Note_from_SRE_Regarding_the_Swift_Migration">
+							<span class="oo-ui-iconElement-icon oo-ui-icon-notice"></span>
+							<span class="oo-ui-labelElement-label">{$skin->msg( 'miraheze-sitenotice-learnmore' )->escaped()}</span>
+							<span class="oo-ui-indicatorElement-indicator oo-ui-indicatorElement-noIndicator"></span>
+						</a>
+					</span>
+				</div>
+			</td></tr></tbody>
+		</table>
+	EOF;
 }
