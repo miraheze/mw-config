660f23b93210358618dd0be148309209b77328ee
paladox <paladox@users.noreply.github.com>
2022-12-18 21:11:04 +0000

    Set default read only for db101

--- Database.php
+++ Database.php
@@ -68,7 +68,7 @@ $wgLBFactoryConf = [
 		],
 	],
 	'readOnlyBySection' => [
-		// 'DEFAULT' => 'DC Switchover in progress. Please try again in a few minutes.',
+		'DEFAULT' => 'DC Switchover in progress. Please try again in a few minutes.',
 		// 'c1' => 'DC Switchover in progress. Please try again in a few minutes.',
 		'c2' => 'DC Switchover in progress. Please try again in a few minutes.',
 		// 'c3' => 'DC Switchover in progress. Please try again in a few minutes.',
