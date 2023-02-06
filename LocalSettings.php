8fa2af5bb3a97042e9266b7dc9fc22fd6a12f889
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-02-05 23:46:35 -0700

    Update LocalSettings.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -1101,6 +1101,9 @@ $wgConf->settings += [
 	'wgDiscordEnableExperimentalCVTFeatures' => [
 		'default' => true,
 	],
+	'wgDiscordExperimentalCVTMatchFilter' => [
+		'default' => [ '.*' ],
+	],
 
 	// Description2
 	'wgEnableMetaDescriptionFunctions' => [
