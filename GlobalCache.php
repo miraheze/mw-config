521f1184b2da6a2b21826d875c58583b0c127d3e
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-03-07 01:17:30 -0700

    Set session cache to CACHE_DB

--- GlobalCache.php
+++ GlobalCache.php
@@ -92,7 +92,7 @@ $wgMWOAuthSessionCacheType = 'db-replicated';
 $redisServerIP = '[2a10:6740::6:306]:6379';
 
 $wgMainCacheType = 'memcached-mem-2';
-$wgMessageCacheType = 'memcached-mem-2';
+$wgMessageCacheType = CACHE_DB;
 
 $wgParserCacheType = 'mysql-multiwrite';
 
