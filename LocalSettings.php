33f6d30ba7debcca0c000a65d11f226256330ee7
MacFan4000 <paulfkeffer@gmail.com>
2023-06-15 15:21:58 -0400

    remove Reception from staff wiki

--- LocalSettings.php
+++ LocalSettings.php
@@ -1554,8 +1554,6 @@ $wgConf->settings += [
 	// DO NOT ADD UNAUTHORISED USERS
 	'wgMirahezeStaffAccessIds' => [
 		'default' => [
-			/** Reception123 (SRE) */
-			19,
 			/** Void (SRE and Board) */
 			5258,
 			/** Paladox (SRE) */
