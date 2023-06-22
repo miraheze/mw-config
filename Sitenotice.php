926bacd6f4d92c4739024e6d64923765b3b15e18
Agent_Isai <43097272+AgentIsai@users.noreply.github.com>
2023-06-21 21:19:44 -0500

    Sitenotice for Swift outage

--- Sitenotice.php
+++ Sitenotice.php
@@ -15,7 +15,7 @@ $wgMajorSiteNoticeID = 84;
  */
 
 // Global SiteNotice
-/* if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'wfGlobalSiteNotice';
 
 	function wfGlobalSiteNotice( &$siteNotice, $skin ) {
@@ -31,11 +31,11 @@ $wgMajorSiteNoticeID = 84;
 					<div data-nosnippet style="padding-top:0.3em; padding-bottom:0.1em;">
 						<div class="floatleft"><img alt="Miraheze Logo" src="https://upload.wikimedia.org/wikipedia/commons/a/ab/OOjs_UI_icon_unLock-ltr.svg" decoding="async" width="50" height="50"></div>
 						<div style="padding-bottom: 15px; font-size: 13pt; font-weight: bold;">
-							Out of an abundance of precaution, all user sessions have been reset due to a security vulnerabiltity in the Cargo extension. You may be logged out and will need to re-login.
+							Image uploads, deletions, and import requests are temporarily unavailable due to no space left on file servers.
 						</div>
 
 						<span id="sitenotice-learnmore-button" class="oo-ui-widget oo-ui-widget-enabled oo-ui-buttonElement oo-ui-buttonElement-framed oo-ui-iconElement oo-ui-labelElement oo-ui-buttonWidget">
-							<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="https://meta.miraheze.org/wiki/Tech:SRE_noticeboard#Cargo_disabled">
+							<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="https://meta.miraheze.org/wiki/Tech:SRE_noticeboard#Swift_outage">
 								<span class="oo-ui-iconElement-icon oo-ui-icon-notice"></span>
 								<span class="oo-ui-labelElement-label">{$skin->msg( 'miraheze-sitenotice-learnmore' )->escaped()}</span>
 								<span class="oo-ui-indicatorElement-indicator oo-ui-indicatorElement-noIndicator"></span>
@@ -47,7 +47,7 @@ $wgMajorSiteNoticeID = 84;
 		EOF;
 	}
 
- }*/
+ }
 
 // Specific wiki SiteNotice
 if ( $wi->isExtensionActive( 'Graph' ) ) {
