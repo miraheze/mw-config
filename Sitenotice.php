6527dcc65414a75b172cd5c8833d2a73326c34a6
paladox <paladox@users.noreply.github.com>
2023-07-01 22:10:46 +0100

    Update Sitenotice.php

--- Sitenotice.php
+++ Sitenotice.php
@@ -7,7 +7,7 @@ if ( $wmgSiteNoticeOptOut ) {
 }
 
 // Increment this version number whenever you change the site notice
-$wgMajorSiteNoticeID = 85;
+$wgMajorSiteNoticeID = 86;
 
 /**
  * Wrap your sitenotice with <div data-nosnippet>(sitenotice)</div>
