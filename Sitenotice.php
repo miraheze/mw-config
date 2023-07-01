59578c777c60fdecf12dcc13d7ed3b7f934429d0
paladox <paladox@users.noreply.github.com>
2023-07-01 22:11:25 +0100

    Site notice for maintenance on servers (#5268)

--- Sitenotice.php
+++ Sitenotice.php
@@ -7,7 +7,7 @@ if ( $wmgSiteNoticeOptOut ) {
 }
 
 // Increment this version number whenever you change the site notice
-$wgMajorSiteNoticeID = 85;
+$wgMajorSiteNoticeID = 86;
 
 /**
  * Wrap your sitenotice with <div data-nosnippet>(sitenotice)</div>
@@ -15,7 +15,7 @@ $wgMajorSiteNoticeID = 85;
  */
 
 // Global SiteNotice
-/* if ( !$wmgSiteNoticeOptOut ) {
+if ( !$wmgSiteNoticeOptOut ) {
 	$wgHooks['SiteNoticeAfter'][] = 'wfGlobalSiteNotice';
 
 	function wfGlobalSiteNotice( &$siteNotice, $skin ) {
@@ -29,25 +29,17 @@ $wgMajorSiteNoticeID = 85;
 			<table style="width: 100%;">
 				<tbody><tr><td style="font-size: 120%; border-left: 4px solid #fc3; background-color: #fef6e7; padding: 10px 15px; color: black;">
 					<div data-nosnippet style="padding-top:0.3em; padding-bottom:0.1em;">
-						<div class="floatleft"><img alt="Files unavailable" src="https://upload.wikimedia.org/wikipedia/commons/d/d0/OOjs_UI_icon_imageLock-ltr.svg" decoding="async" width="50" height="50"></div>
+						<div class="floatleft"><img alt="Maintenance" src="https://upload.wikimedia.org/wikipedia/commons/d/d0/OOjs_UI_icon_imageLock-ltr.svg" decoding="async" width="50" height="50"></div>
 						<div style="padding-bottom: 15px; font-size: 13pt; font-weight: bold;">
-							File uploads, deletions, and import requests have been reenabled. Thank you for bearing with us and apologize for the inconvenience caused.
+							Miraheze will be doing maintenance on the servers on the 6th of July at 22:00 UTC. Some wiki's will be unavailable, whilst others will be read-only during the period of 3 hours (ends at 01:00 UTC). Less if we complete it before the 3 hour window is up. We apologize for the inconvenience caused.
 						</div>
-
-						<span id="sitenotice-learnmore-button" class="oo-ui-widget oo-ui-widget-enabled oo-ui-buttonElement oo-ui-buttonElement-framed oo-ui-iconElement oo-ui-labelElement oo-ui-buttonWidget">
-							<a class="oo-ui-buttonElement-button" role="button" tabindex="0" href="https://meta.miraheze.org/wiki/Tech:SRE_noticeboard#Swift_outage">
-								<span class="oo-ui-iconElement-icon oo-ui-icon-notice"></span>
-								<span class="oo-ui-labelElement-label">{$skin->msg( 'miraheze-sitenotice-learnmore' )->escaped()}</span>
-								<span class="oo-ui-indicatorElement-indicator oo-ui-indicatorElement-noIndicator"></span>
-							</a>
-						</span>
 					</div>
 				</td></tr></tbody>
 			</table>
 		EOF;
 	}
 
-} */
+}
 
 // Specific wiki SiteNotice
 if ( $wi->isExtensionActive( 'Graph' ) ) {
