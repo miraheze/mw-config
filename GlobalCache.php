03a4027da76e5b4442362c60091989e297e10f8c
paladox <thomasmulhall410@yahoo.com>
2023-08-12 01:38:47 -0400

    Revert "Switch $wgMainCacheType to memcached-mem-1"
    This reverts commit 4dc3d9f435124e5b316308dbdaf048f11eaeefce.

--- GlobalCache.php
+++ GlobalCache.php
@@ -88,7 +88,7 @@ $wgMWOAuthSessionCacheType = 'db-replicated';
 
 $redisServerIP = '[2a10:6740::6:306]:6379';
 
-$wgMainCacheType = 'memcached-mem-1';
+$wgMainCacheType = 'memcached-mem-2';
 $wgMessageCacheType = 'memcached-mem-1';
 
 $wgParserCacheType = 'mysql-multiwrite';
