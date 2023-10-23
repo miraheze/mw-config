0c4cb11ce564620745ed3391ea147a2d79fe29ba
paladox <paladox@users.noreply.github.com>
2023-10-23 13:23:40 +0100

    Set mirabeta to use SCHEMA_COMPAT_READ_NEW

--- LocalSettings.php
+++ LocalSettings.php
@@ -73,13 +73,13 @@ $wgConf->settings += [
 	// and to run migrateRevisionCommentTemp.php. After set to SCHEMA_COMPAT_READ_NEW.
 	'wgCommentTempTableSchemaMigrationStage' => [
 		'default' => MIGRATION_OLD,
-		'mirabeta' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+		'mirabeta' => SCHEMA_COMPAT_READ_NEW,
 	],
 	// Migrating requires SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD
 	// and to run migrateExternallinks. After set to SCHEMA_COMPAT_READ_NEW.
 	'wgExternalLinksSchemaMigrationStage' => [
 		'default' => MIGRATION_OLD,
-		'mirabeta' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+		'mirabeta' => SCHEMA_COMPAT_READ_NEW,
 	],
 
 	// 3D
