dd42905238658efd957ed52eef023fa72fcbbfe0
Rob Kam <robkam@gmx.com>
2023-10-26 23:52:35 +0100

    Get LinkTarget to open iwlinks in new tab

--- LocalSettings.php
+++ LocalSettings.php
@@ -2502,16 +2502,16 @@ $wgConf->settings += [
 			'_self' => [ 'sametablinks' ]
 		],
 		'scruffywiki' => [
-			'_blank' => [ 'extiw' ],
+			'_blank' => [ '' ]
 		],
 		'simpleelectronicswiki' => [
-			'_blank' => [ 'extiw' ]
+			'_blank' => [ '' ]
 		],
 		'sdiywikiwiki' => [
-			'_blank' => [ 'extiw' ]
+			'_blank' => [ '' ]
 		],
 	],
-
+	
 	// LinkTitles
 	'wgLinkTitlesFirstOnly' => [
 		'default' => true,
