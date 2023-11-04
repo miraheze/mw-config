a0ec5a79450c99c3acade7b1dc19cae60f65dc2d
paladox <paladox@users.noreply.github.com>
2023-11-04 16:10:33 +0000

    Switch warmCache off for wgParsoidCacheConfig (#5359)

--- GlobalCache.php
+++ GlobalCache.php
@@ -81,7 +81,7 @@ $wgParsoidCacheConfig = [
 	'StashType' => 'db-replicated',
 	'StashDuration' => 86400 * 10,
 	'CacheThresholdTime' => 0.0,
-	'WarmParsoidParserCache' => true,
+	'WarmParsoidParserCache' => false,
 ];
 
 $wgLanguageConverterCacheType = CACHE_ACCEL;
