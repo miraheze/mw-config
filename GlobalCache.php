f302e7d169db609433caf1d913a5abbecf22c36f
paladox <paladox@users.noreply.github.com>
2023-11-05 18:10:17 +0000

    Enable warmup for wikis beginning with g (#5365)

--- GlobalCache.php
+++ GlobalCache.php
@@ -109,7 +109,7 @@ $disableWarmupArray = [
 	'xedwiki',
 ];
 // $disableWarmup = in_array( $wgDBname, $disableWarmupArray );
-$enableWarmup = ( $beta || preg_match( '/^([0-9]|a|b|c|d|e|f)/', $wgDBname ) );
+$enableWarmup = ( $beta || preg_match( '/^([0-9]|a|b|c|d|e|f|g)/', $wgDBname ) );
 $wgParsoidCacheConfig = [
 	// Defaults to MainStash
 	'StashType' => null,
