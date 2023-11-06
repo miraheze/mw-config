3cfc6e10edbc12fe243f95b079f9592f0d9393eb
paladox <paladox@users.noreply.github.com>
2023-11-06 19:46:19 +0000

    Set $wgPoolCounterConf and use redis

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -606,3 +606,88 @@ $mcpMessageCachePerformanceMsgPrefixes = [
 	'accesskey-',
 	'nstab-'
 ];
+
+$wgPoolCounterConf = [
+	'ArticleView' => [
+		'class' => 'PoolCounterRedis',
+		'timeout' => 15,
+		'workers' => 2,
+		'maxqueue' => 100,
+		'fastStale' => true,
+                'servers' => [
+			'[2a10:6740::6:306]:6379'
+		],
+                'redisConfig' => [
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		],
+	],
+	'FileRender' => [
+		'class' => 'PoolCounterRedis',
+		'timeout' => 8,
+		'workers' => 2,
+		'maxqueue' => 100,
+                'servers' => [
+			'[2a10:6740::6:306]:6379'
+		],
+                'redisConfig' => [
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		],
+	],
+	'FileRenderExpensive' => [
+		'class' => 'PoolCounterRedis',
+		'timeout' => 8,
+		'workers' => 2,
+		'slots' => 8,
+		'maxqueue' => 100,
+                'servers' => [
+			'[2a10:6740::6:306]:6379'
+		],
+                'redisConfig' => [
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		],
+	],
+	'SpecialContributions' => [
+		'class' => 'PoolCounterRedis',
+		'timeout' => 15,
+		'workers' => 2,
+		'maxqueue' => 25,
+                'servers' => [
+			'[2a10:6740::6:306]:6379'
+		],
+                'redisConfig' => [
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		],
+	],
+	'TranslateFetchTranslators' => [
+		'class' => 'PoolCounterRedis',
+		'timeout' => 8,
+		'workers' => 1,
+		'slots' => 16,
+		'maxqueue' => 20,
+                'servers' => [
+			'[2a10:6740::6:306]:6379'
+		],
+                'redisConfig' => [
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		],
+	],
+	'WikiLambdaFunctionCall' => [
+		'class' => 'PoolCounterRedis',
+		'timeout' => 1,
+		'workers' => 2,
+		'maxqueue' => 5,
+		'slots' => 50,
+                'servers' => [
+			'[2a10:6740::6:306]:6379'
+		],
+                'redisConfig' => [
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		],
+	],
+];
