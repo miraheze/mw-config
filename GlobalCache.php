63481546523a5ed4d8ff394db3865d42f4dba96f
paladox <paladox@users.noreply.github.com>
2023-11-14 01:00:43 +0000

    Enable warmup on all

--- GlobalCache.php
+++ GlobalCache.php
@@ -101,7 +101,7 @@ $wgMessageCacheType = 'memcached-mem-1';
 
 $wgParserCacheType = 'mysql-multiwrite';
 
-$enableWarmup = ( $beta || preg_match( '/^[0-9a-p]/', $wgDBname ) ) && $wgDBname !== 'commonswiki';
+$enableWarmup = ( $beta || preg_match( '/^[0-9a-z]/', $wgDBname ) ) && $wgDBname !== 'commonswiki';
 $wgParsoidCacheConfig = [
 	// Defaults to MainStash
 	'StashType' => null,
