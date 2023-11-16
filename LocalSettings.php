00cb143f50517dd7101a39956cdab2c54c26c5f8
paladox <paladox@users.noreply.github.com>
2023-11-16 17:22:16 -0500

    Set wgExternalLinksSchemaMigrationStage to READ_NEW in addition to still writing to both tables (#5376)

--- LocalSettings.php
+++ LocalSettings.php
@@ -98,7 +98,7 @@ $wgConf->settings += [
 	// Migrating requires SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD
 	// and to run migrateExternallinks. After set to SCHEMA_COMPAT_WRITE_NEW | SCHEMA_COMPAT_READ_NEW.
 	'wgExternalLinksSchemaMigrationStage' => [
-		'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_OLD,
+		'default' => SCHEMA_COMPAT_WRITE_BOTH | SCHEMA_COMPAT_READ_NEW,
 		'mirabeta' => SCHEMA_COMPAT_WRITE_NEW | SCHEMA_COMPAT_READ_NEW,
 	],
 
