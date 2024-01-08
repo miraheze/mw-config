3986c2f274b56b0f1671b41488995896474397ee
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-08 14:22:06 -0700

    Update path

--- LocalSettings.php
+++ LocalSettings.php
@@ -524,7 +524,7 @@ $wgConf->settings += [
 
 	// Chameleon
 	'egChameleonLayoutFile' => [
-		'default' => '/srv/mediawiki/' . $wi->version . '/skins/chameleon/layouts/standard.xml',
+		'default' => '/srv/mediawiki/config/chameleon-layouts/standard.xml',
 	],
 	'egChameleonEnableExternalLinkIcons' => [
 		'default' => false,
