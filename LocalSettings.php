e461263342ce5635cffee6316055e1d5a461dc14
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-10 12:47:14 -0700

    Remove mwtask141

--- LocalSettings.php
+++ LocalSettings.php
@@ -22,10 +22,7 @@ ini_set( 'memory_limit', 1400 * 1024 * 1024 );
 // Configure PHP request timeouts.
 if ( PHP_SAPI === 'cli' ) {
 	$wgRequestTimeLimit = 0;
-} elseif (
-	( $_SERVER['HTTP_HOST'] ?? '' ) === 'mwtask141.miraheze.org' ||
-	( $_SERVER['HTTP_HOST'] ?? '' ) === 'mwtask181.wikitide.net'
-) {
+} elseif ( ( $_SERVER['HTTP_HOST'] ?? '' ) === 'mwtask181.wikitide.net' ) {
 	$wgRequestTimeLimit = 1200;
 } elseif ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
 	$wgRequestTimeLimit = 200;
@@ -2647,8 +2644,6 @@ $wgConf->settings += [
 			'2a10:6740::6:503' => true,
 			/** mw143 */
 			'2a10:6740::6:513' => true,
-			/** mwtask141 */
-			'2a10:6740::6:504' => true,
 			/** test131 */
 			'2a10:6740::6:406' => true,
 			/** mw181 */
