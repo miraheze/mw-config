c9b53b0108a4f51b82bf5b7b87014b9c91f41fda
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-01-30 15:43:43 -0700

    T11758: also check for title when viewing diffs

--- initialise/entrypoints/decode.php
+++ initialise/entrypoints/decode.php
@@ -35,6 +35,10 @@ if ( $wgArticlePath === '/wiki/$1' && isset( $_GET['diff'] ) ) {
 	if ( isset( $_GET['oldid'] ) ) {
 		$queryParameters['oldid'] = $_GET['oldid'];
 	}
+
+	if ( isset( $_GET['title'] ) ) {
+		$queryParameters['title'] = $_GET['title'];
+	}
 }
 
 if ( $queryString || isset( $queryParameters ) ) {
