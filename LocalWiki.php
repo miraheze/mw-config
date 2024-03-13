ca51fb674741112a542b0a8d975b834ba9ec09d6
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-13 12:38:39 -0600

    LocalWiki: use namespaced classes (#5499)

--- LocalWiki.php
+++ LocalWiki.php
@@ -1,5 +1,12 @@
 <?php
 
+use MediaWiki\Linker\Linker;
+use MediaWiki\Output\OutputPage;
+use MediaWiki\Request\WebRequest;
+use MediaWiki\SpecialPage\DisabledSpecialPage;
+use MediaWiki\Title\Title;
+use MediaWiki\User\User;
+
 // Per-wiki settings that are incompatible with LocalSettings.php
 switch ( $wi->dbname ) {
 	case 'arquivopkmnwiki':
