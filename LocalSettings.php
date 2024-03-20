e11655d347d05aa4b31e59714302f0537fc965e1
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-19 19:57:45 -0600

    Set $wgStatsdServer

--- LocalSettings.php
+++ LocalSettings.php
@@ -84,6 +84,8 @@ $wmgUploadHostname = 'static.miraheze.org';
 
 $wgStatsFormat = 'dogstatsd';
 $wgStatsTarget = 'udp://localhost:9125';
+// graphite151
+$wgStatsdServer = '10.0.15.145';
 
 $wgConf->settings += [
 	// invalidates user sessions - do not change unless it is an emergency.
