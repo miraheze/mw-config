9195d6ea621714adb8af607d05efed59dc27073a
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-03-24 16:44:25 -0600

    Configure $wgEventRelayerConfig

--- GlobalCache.php
+++ GlobalCache.php
@@ -1,5 +1,6 @@
 <?php
 
+use MediaWiki\Extension\EventBus\Adapters\EventRelayer\CdnPurgeEventRelayer;
 use MediaWiki\Extension\EventBus\Adapters\JobQueue\JobQueueEventBus;
 use MediaWiki\Extension\EventBus\Adapters\RCFeed\EventBusRCFeedEngine;
 use MediaWiki\Extension\EventBus\Adapters\RCFeed\EventBusRCFeedFormatter;
@@ -163,6 +164,16 @@ if ( $beta ) {
 		'eventgate' => [
 			'url' => 'http://10.0.18.147:8192/v1/events',
 			'timeout' => 5,
+		],
+	];
+
+	$wgEventRelayerConfig = [
+		'cdn-url-purges' => [
+			'class' => CdnPurgeEventRelayer::class,
+			'stream' => 'resource-purge',
+		],
+		'default' => [
+			'class' => EventRelayerNull::class,
 		],
 	];
 
