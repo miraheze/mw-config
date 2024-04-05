002162eff452e6319b3a48d87e5fe1e4b93c7523
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-04-05 14:52:22 -0600

    thumb_handler: redirect default server to primary domain

--- initialise/entrypoints/thumb_handler.php
+++ initialise/entrypoints/thumb_handler.php
@@ -1,3 +1,17 @@
 <?php
+
 require_once '/srv/mediawiki/config/initialise/MirahezeFunctions.php';
+
+if (
+	MirahezeFunctions::getPrimaryDomain( MirahezeFunctions::getCurrentDatabase() ) !== MirahezeFunctions::getDefaultServer() &&
+	str_contains( strtoupper( $_SERVER['REQUEST_URI'] ), strtoupper( MirahezeFunctions::getDefaultServer() ) )
+) {
+	header( 'Location: ' . str_replace(
+		MirahezeFunctions::getDefaultServer(),
+		MirahezeFunctions::getPrimaryDomain( MirahezeFunctions::getCurrentDatabase() ),
+		$_SERVER['REQUEST_URI']
+	), true, 301 );
+	exit();
+}
+
 require MirahezeFunctions::getMediaWiki( 'thumb_handler.php' );
