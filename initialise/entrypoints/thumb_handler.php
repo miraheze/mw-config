1cac6ca761ac40bd7b4adf08aa1f94ce59a140b4
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-04-06 11:28:51 -0600

    Use ignorePrimary

--- initialise/entrypoints/thumb_handler.php
+++ initialise/entrypoints/thumb_handler.php
@@ -3,12 +3,12 @@
 require_once '/srv/mediawiki/config/initialise/MirahezeFunctions.php';
 
 if (
-	MirahezeFunctions::getPrimaryDomain( MirahezeFunctions::getCurrentDatabase() ) !== MirahezeFunctions::getDefaultServer() &&
+	MirahezeFunctions::getPrimaryDomain( MirahezeFunctions::getCurrentDatabase( true ) ) !== MirahezeFunctions::getDefaultServer() &&
 	str_contains( strtoupper( $_SERVER['REQUEST_URI'] ), strtoupper( MirahezeFunctions::getDefaultServer() ) )
 ) {
 	header( 'Location: ' . str_replace(
 		MirahezeFunctions::getDefaultServer(),
-		MirahezeFunctions::getPrimaryDomain( MirahezeFunctions::getCurrentDatabase() ),
+		MirahezeFunctions::getPrimaryDomain( MirahezeFunctions::getCurrentDatabase( true ) ),
 		$_SERVER['REQUEST_URI']
 	), true, 301 );
 	exit();
