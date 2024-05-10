36c4e6c0580d83b73efda4fe2244c7ad17be4b4f
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-05-10 16:47:59 -0600

    Re-add ReplaceText (restricted for now)

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -1715,6 +1715,26 @@ $wgManageWikiExtensions = [
 		],
 		'section' => 'specialpages',
 	],
+	'replacetext' => [
+		'name' => 'Replace Text',
+		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:Replace_Text',
+		'conflicts' => false,
+		'requires' => [
+			'permissions' => [
+				'managewiki-restricted',
+			],
+		],
+		'install' => [
+			'permissions' => [
+				'sysop' => [
+					'permissions' => [
+						'replacetext',
+					],
+				],
+			],
+		],
+		'section' => 'specialpages',
+	],
 	'report' => [
 		'name' => 'Report',
 		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:Report',
