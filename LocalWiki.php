58a4c18910c24710c95c631e80ce50c9a9b19729
BrandonWM <74372157+brandon-wm@users.noreply.github.com>
2024-05-13 14:07:35 -0700

    T12108

--- LocalWiki.php
+++ LocalWiki.php
@@ -352,7 +352,8 @@ switch ( $wi->dbname ) {
 
 		break;
 	case 'metzowiki':
-		$wgDplSettings['maxCategoryCount'] = 10;
+		$wgDplSettings['allowUnlimitedCategories'] = true;
+	        $wgDplSettings['allowUnlimitedResults'] = true;
 
 		break;
 	case 'newusopediawiki':
