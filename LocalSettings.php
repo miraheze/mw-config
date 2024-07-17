d471e2d954480302005a97f5daa5be03ca69f6a5
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2024-07-17 17:47:13 -0400

    disable eveskin T12349

--- LocalSettings.php
+++ LocalSettings.php
@@ -6643,6 +6643,7 @@ $wi::$disabledExtensions = [
 if ( $wi->version >= '1.42' ) {
 	array_push( $wi::$disabledExtensions, 'chameleon' );
 	array_push( $wi::$disabledExtensions, 'evelution' );
+	array_push( $wi::$disabledExtensions, 'eveskin' );
 	array_push( $wi::$disabledExtensions, 'femiwiki' );
 	array_push( $wi::$disabledExtensions, 'ratepage' );
 	array_push( $wi::$disabledExtensions, 'rightfunctions' );
