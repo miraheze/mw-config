c02ca1698d8a0a6601771ae493f51dcf730714e2
Luke <123885349+OAuthority@users.noreply.github.com>
2024-08-26 04:13:49 +0100

    Add back for now

--- LocalSettings.php
+++ LocalSettings.php
@@ -4838,6 +4838,9 @@ $wgConf->settings += [
 	'wgRemovePIIAutoPrefix' => [
 		'default' => 'MirahezeGDPR_',
 	],
+	'wgRemovePIIDPAValidationEndpoint' => [
+		'default' => 'https://reports.miraheze.org/api/dpa/{dpa_id}/{username}',
+	],
 	'wgRemovePIIHashPrefixOptions' => [
 		'default' => [
 			'Trust and Safety' => 'MirahezeGDPR_',
