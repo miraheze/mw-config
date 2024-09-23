d2caa6d1a3722710e86d149c2a8af439168f614b
paladox <paladox@users.noreply.github.com>
2024-09-23 16:41:52 +0100

    Use new jobrunner/jobqueue on beta (#5676)
    Co-authored-by: github-actions <github-actions@users.noreply.github.com>

--- GlobalCache.php
+++ GlobalCache.php
@@ -149,22 +149,24 @@ $wgResourceLoaderUseObjectCacheForDeps = true;
 
 $wgCdnMatchParameterOrder = false;
 
-$redisServerIP = $beta ?
-	'10.0.15.118:6379' :
-	'10.0.17.120:6379';
+if ( !$beta ) {
+	$redisServerIP = $beta ?
+		'10.0.15.118:6379' :
+		'10.0.17.120:6379';
 
-$wgJobTypeConf['default'] = [
-	'class' => JobQueueRedis::class,
-	'redisServer' => $redisServerIP,
-	'redisConfig' => [
-		'connectTimeout' => 2,
-		'password' => $wmgRedisPassword,
-		'compression' => 'gzip',
-	],
-	'daemonized' => true,
-];
+	$wgJobTypeConf['default'] = [
+		'class' => JobQueueRedis::class,
+		'redisServer' => $redisServerIP,
+		'redisConfig' => [
+			'connectTimeout' => 2,
+			'password' => $wmgRedisPassword,
+			'compression' => 'gzip',
+		],
+		'daemonized' => true,
+	];
 
-unset( $redisServerIP );
+	unset( $redisServerIP );
+}
 
 if ( PHP_SAPI === 'cli' ) {
 	// APC not available in CLI mode
