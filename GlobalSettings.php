23a3dae002a0f9a729ee5fc650420f1fa1b7db21
paladox <paladox@users.noreply.github.com>
2024-10-13 01:39:50 +0100

    Add cirrus config to poolcounter config

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -783,6 +783,57 @@ $wgPoolCounterConf = [
 		'maxqueue' => 100,
 		'fastStale' => true,
 	],
+	'CirrusSearch-Search' => [
+		'class' => 'PoolCounter_Client',
+		'timeout' => 15,
+		'workers' => 200,
+		'maxqueue' => 200,
+	],
+	// Software tries to recognize sources of external automation, such as GAE,
+	// AWS, browser automation, etc. and give them a separate pool so they
+	// can cap out without interfering with interactive users.
+	'CirrusSearch-Automated' => [
+		'class' => 'PoolCounter_Client',
+		'timeout' => 15,
+		'workers' => 30,
+		'maxqueue' => 35,
+	],
+	// Super common and mostly fast
+	'CirrusSearch-Prefix' => [
+		'class' => 'PoolCounter_Client',
+		'timeout' => 15,
+		'workers' => 32,
+		'maxqueue' => 40,
+	],
+	// Super common and mostly fast, replaces Prefix (eventually)
+	'CirrusSearch-Completion' => [
+		'class' => 'PoolCounter_Client',
+		'timeout' => 15,
+		'workers' => 432,
+		'maxqueue' => 450,
+	],
+	// Pool counter for expensive full text searches such as regex
+	// and deepcat.
+	'CirrusSearch-ExpensiveFullText' => [
+		'class' => 'PoolCounter_Client',
+		'timeout' => 60,
+		'workers' => 10,
+		'maxqueue' => 15,
+	],
+	// These should be very very fast
+	'CirrusSearch-NamespaceLookup' => [
+		'class' => 'PoolCounter_Client',
+		'timeout' => 5,
+		'workers' => 100,
+		'maxqueue' => 120,
+	],
+	// These are very expensive and incredibly common.
+	'CirrusSearch-MoreLike' => [
+		'class' => 'PoolCounter_Client',
+		'timeout' => 5,
+		'workers' => 150,
+		'maxqueue' => 175,
+	],
 	'FileRender' => [
 		'class' => 'PoolCounter_Client',
 		'timeout' => 8,
