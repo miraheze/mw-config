3ac7e0d1c08a991573d95c439e61b9f9676354ff
CosmicAlpha <cosmic.alpha24@gmail.com>
2024-10-16 17:05:17 -0600

    Remove old config

--- LocalSettings.php
+++ LocalSettings.php
@@ -1007,21 +1007,6 @@ $wgConf->settings += [
 	'wgCreateWikiShowBiographicalOption' => [
 		'default' => true,
 	],
-	'wgCreateWikiSQLfiles' => [
-		'default' => [
-			"$IP/maintenance/tables-generated.sql",
-			"$IP/extensions/AbuseFilter/db_patches/mysql/tables-generated.sql",
-			"$IP/extensions/AntiSpoof/sql/mysql/tables-generated.sql",
-			"$IP/extensions/BetaFeatures/sql/tables-generated.sql",
-			"$IP/extensions/CheckUser/schema/mysql/tables-generated.sql",
-			"$IP/extensions/DataDump/sql/data_dump.sql",
-			"$IP/extensions/Echo/sql/mysql/tables-generated.sql",
-			"$IP/extensions/GlobalBlocking/sql/mysql/tables-generated-global_block_whitelist.sql",
-			"$IP/extensions/OAuth/schema/mysql/tables-generated.sql",
-			"$IP/extensions/RottenLinks/sql/rottenlinks.sql",
-			"$IP/extensions/UrlShortener/schemas/mysql/tables-generated.sql",
-		],
-	],
 	'wgCreateWikiSQLFiles' => [
 		'default' => [
 			"$IP/maintenance/tables-generated.sql",
