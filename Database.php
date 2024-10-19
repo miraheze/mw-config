bddd11d14ea04c53231399423529411b5ce343f2
Alex <alex@blueselene.com>
2024-10-19 12:18:02 +0200

    Add db172

--- Database.php
+++ Database.php
@@ -1,62 +1,100 @@
 <?php
 
-$wgLBFactoryConf = [
-	'class' => \Wikimedia\Rdbms\LBFactoryMulti::class,
-	'secret' => $wgSecretKey,
-	'sectionsByDB' => $wi->wikiDBClusters,
-	'sectionLoads' => [
-		'DEFAULT' => [
-			'db171' => 0,
+if ( strpos( wfHostname(), 'test' ) ) {
+	// Mirabeta database configuration
+	$wgLBFactoryConf = [
+		'class' => \Wikimedia\Rdbms\LBFactoryMulti::class,
+		'secret' => $wgSecretKey,
+		'sectionsByDB' => $wi->wikiDBClusters,
+		'sectionLoads' => [
+			'DEFAULT' => [
+				'db172' => 0,
+			],
+			'c1' => [
+				'db172' => 0,
+			],
 		],
-		'c1' => [
-			'db151' => 0,
+		'serverTemplate' => [
+			'dbname' => $wgDBname,
+			'user' => $wgDBuser,
+			'password' => $wgDBpassword,
+			'type' => 'mysql',
+			'flags' => DBO_DEFAULT | ( MW_ENTRY_POINT === 'cli' ? DBO_DEBUG : 0 ),
+			'variables' => [
+				// https://mariadb.com/docs/reference/mdb/system-variables/innodb_lock_wait_timeout
+				'innodb_lock_wait_timeout' => 120,
+			],
 		],
-		'c2' => [
-			'db161' => 0,
+		'hostsByName' => [
+			'db172' => '10.0.17.158',
 		],
-		'c3' => [
-			'db171' => 0,
+		'externalLoads' => [
+			'beta' => [
+				/** where the metawikibeta database is located */
+				'db172' => 0,
+			],
 		],
-		'c4' => [
-			'db181' => 0,
+		'readOnlyBySection' => [
+			// 'DEFAULT' => 'Maintenance is in progress. Please try again in a few minutes.',
+			// 'c1' => 'Maintenance is in progress. Please try again in a few minutes.',
 		],
-	],
-	'serverTemplate' => [
-		'dbname' => $wgDBname,
-		'user' => $wgDBuser,
-		'password' => $wgDBpassword,
-		'type' => 'mysql',
-		'flags' => DBO_DEFAULT | ( MW_ENTRY_POINT === 'cli' ? DBO_DEBUG : 0 ),
-		'variables' => [
-			// https://mariadb.com/docs/reference/mdb/system-variables/innodb_lock_wait_timeout
-			'innodb_lock_wait_timeout' => 120,
+	];
+} else {
+	// Production database configuration
+	$wgLBFactoryConf = [
+		'class' => \Wikimedia\Rdbms\LBFactoryMulti::class,
+		'secret' => $wgSecretKey,
+		'sectionsByDB' => $wi->wikiDBClusters,
+		'sectionLoads' => [
+			'DEFAULT' => [
+				'db171' => 0,
+			],
+			'c1' => [
+				'db151' => 0,
+			],
+			'c2' => [
+				'db161' => 0,
+			],
+			'c3' => [
+				'db171' => 0,
+			],
+			'c4' => [
+				'db181' => 0,
+			],
 		],
-	],
-	'hostsByName' => [
-		'db151' => '10.0.15.110',
-		'db161' => '10.0.16.128',
-		'db171' => '10.0.17.119',
-		'db181' => '10.0.18.102',
-	],
-	'externalLoads' => [
-		'beta' => [
-			/** where the metawikibeta database is located */
-			'db161' => 0,
+		'serverTemplate' => [
+			'dbname' => $wgDBname,
+			'user' => $wgDBuser,
+			'password' => $wgDBpassword,
+			'type' => 'mysql',
+			'flags' => DBO_DEFAULT | ( MW_ENTRY_POINT === 'cli' ? DBO_DEBUG : 0 ),
+			'variables' => [
+				// https://mariadb.com/docs/reference/mdb/system-variables/innodb_lock_wait_timeout
+				'innodb_lock_wait_timeout' => 120,
+			],
 		],
-		'echo' => [
-			/** where the metawiki database is located */
-			'db171' => 0,
+		'hostsByName' => [
+			'db151' => '10.0.15.110',
+			'db161' => '10.0.16.128',
+			'db171' => '10.0.17.119',
+			'db181' => '10.0.18.102',
 		],
-	],
-	'readOnlyBySection' => [
-		// 'DEFAULT' => 'Maintenance is in progress. Please try again in a few minutes.',
-		// 'c1' => 'Maintenance is in progress. Please try again in a few minutes.',
-		// 'c2' => 'Maintenance is in progress. Please try again in a few minutes.',
-		// 'c3' => 'Maintenance is in progress. Please try again in a few minutes.',
-		// 'c4' => 'Maintenance is in progress. Please try again in a few minutes.',
-		// 's1' => 'Maintenance is in progress. Please try again in a few minutes.',
-	],
-];
+		'externalLoads' => [
+			'echo' => [
+				/** where the metawiki database is located */
+				'db171' => 0,
+			],
+		],
+		'readOnlyBySection' => [
+			// 'DEFAULT' => 'Maintenance is in progress. Please try again in a few minutes.',
+			// 'c1' => 'Maintenance is in progress. Please try again in a few minutes.',
+			// 'c2' => 'Maintenance is in progress. Please try again in a few minutes.',
+			// 'c3' => 'Maintenance is in progress. Please try again in a few minutes.',
+			// 'c4' => 'Maintenance is in progress. Please try again in a few minutes.',
+			// 's1' => 'Maintenance is in progress. Please try again in a few minutes.',
+		],
+	];
+}
 
 // Disable LoadMonitor in CLI, it doesn't provide much value in CLI.
 if ( PHP_SAPI === 'cli' ) {
