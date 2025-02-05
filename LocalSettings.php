08c377b084ff8b86ac826506b8cc10e18ab5d046
Rob Kam <robkam@gmx.com>
2025-02-05 16:08:09 +0000

    Open interwiki links in new tab (#5722)
    Get interwiki links to open in a new tab for the Popular Music Studies
    Wiki.

--- LocalSettings.php
+++ LocalSettings.php
@@ -3044,6 +3044,9 @@ $wgConf->settings += [
 			[ 'newtablinks', 'wikiwalk' ],
 			'_self' => [ 'sametablinks' ]
 		],
+		'randrwiki' => [
+			'_blank' => [ '' ]
+		],
 		'scruffwiki' => [
 			'_blank' => [ '' ]
 		],
