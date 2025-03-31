a14ae3acb7cbf23160ac707440c36ab4d319284f
Aeywoo <65846594+Aeywoo@users.noreply.github.com>
2025-03-31 22:48:38 +1030

    Undeploy Skin:Eveskin per T13411
    Per [T13411](https://issue-tracker.miraheze.org/T13411), [Skin:Eveskin](https://www.mediawiki.org/wiki/Skin:Eveskin) was archived on March 25th as it hasn't been updated to work with MediaWiki 1.42 and the repository for the skin was archived by the creator, therefore it needs to be removed as it is already in a disabled state on Miraheze.

--- LocalSettings.php
+++ LocalSettings.php
@@ -7305,7 +7305,6 @@ $wi::$disabledExtensions = [
 
 	'chameleon' => 'Incompatible with MediaWiki 1.42',
 	'evelution' => 'Incompatible with MediaWiki 1.42',
-	'eveskin' => 'Incompatible with MediaWiki 1.42',
 	'femiwiki' => 'Incompatible with MediaWiki 1.42',
 	'snapwikiskin' => 'Incompatible with MediaWiki 1.42',
 ];
