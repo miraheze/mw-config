485bcf1866a6745604543a9ad6ce640233749c19
Aeywoo <65846594+Aeywoo@users.noreply.github.com>
2025-03-31 22:49:12 +1030

    Undeploy Skin:Eveskin per T13411
    Removing the ManageWikiExtensions.php entry for Skin:Eveskin.

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -3779,13 +3779,6 @@ $wgManageWikiExtensions = [
 		'requires' => [],
 		'section' => 'skins',
 	],
-	'eveskin' => [
-		'name' => "Eveskin",
-		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Skin:Eveskin',
-		'conflicts' => false,
-		'requires' => [],
-		'section' => 'skins',
-	],
 	'femiwiki' => [
 		'name' => 'Femiwiki',
 		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Skin:Femiwiki',
