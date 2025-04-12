11860845456359107817a0236f7b27e3fc6114ce
Aeywoo <65846594+Aeywoo@users.noreply.github.com>
2025-04-12 23:07:42 +1000

    Revert "Updating SemanticMediaWiki.php settings for Constantnoblewiki per T13503" (#5905)
    Reverts miraheze/mw-config#5904 I can't stop breaking prod!!!!

--- SemanticMediaWiki.php
+++ SemanticMediaWiki.php
@@ -53,8 +53,6 @@ if ( !class_exists( SMW\Setup::class ) ) {
 if ( $wgDBname === 'constantnoblewiki' ) {
 	array_push( $smwgPageSpecialProperties, '_CDAT' );
 
-	$smwgDVFeatures = $smwgDVFeatures | SMW_DV_PVUC;
-
 	$smwgNamespacesWithSemanticLinks = [
 		NS_MAIN => true,
 		NS_TALK => false,
