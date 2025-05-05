a756ac81b120334f0f6e6230d2939f3876a25f3a
RhinosF1 <rhinosf1@miraheze.org>
2025-05-05 10:20:04 +0100

    Revert "Update LocalSettings.php" (#5945)
    Reverts miraheze/mw-config#5944

--- LocalSettings.php
+++ LocalSettings.php
@@ -75,7 +75,7 @@ if ( $forceprofile == 1 && extension_loaded( 'xhprof' ) ) {
 }
 
 // Show custom database maintenance error page on these clusters.
-$wgDatabaseClustersMaintenance = ['c4'];
+$wgDatabaseClustersMaintenance = [];
 
 require_once '/srv/mediawiki/config/initialise/MirahezeFunctions.php';
 $wi = new MirahezeFunctions();
