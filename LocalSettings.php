4399551955d1993f461e210dd806f10bcdb7cca2
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-05-21 10:30:36 -0700

    Add comment

--- LocalSettings.php
+++ LocalSettings.php
@@ -3328,6 +3328,9 @@ $wgConf->settings += [
 		'default' => false,
 	],
 	'wgManageWikiHandledUnknownContentModels' => [
+		// Only add content models here that is not possible to get working on new wikis
+		// Content models that can should be setup when doing imports etc...
+		// to avoid potential content model mismatch issues.
 		'default' => [
 			// Flow is being removed and no longer enabled no new wikis
 			'flow-board',
