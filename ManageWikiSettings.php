4bd1a335ee92882f0d7b0bf283e383e68f06cf77
Aeywoo <65846594+Aeywoo@users.noreply.github.com>
2025-05-25 00:49:52 +1000

    Update ManageWikiSettings.php
    Goodbye MobileUrlCallback.

--- ManageWikiSettings.php
+++ ManageWikiSettings.php
@@ -3726,15 +3726,6 @@ $wgManageWikiSettings = [
 			]
 		],
 	],
-	'wgMobileUrlCallback' => [
-		'name' => 'MobileFrontend Mobile URL Callback',
-		'from' => 'mobilefrontend',
-		'type' => 'text',
-		'overridedefault' => '',
-		'section' => 'styling',
-		'help' => 'A callback that takes a domain name, and changes it into a mobile domain name. When that is not possible, it should return its input unchanged. On wikifarms, the domain name might belong to another wiki.',
-		'requires' => [],
-	],
 	'wgWordmark' => [
 		'name' => 'Wordmark',
 		'from' => 'mediawiki',
