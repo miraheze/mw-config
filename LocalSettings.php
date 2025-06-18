fa03f83346b64a2ccd930170b5ca4525faf83744
Asko <49131864+asko1@users.noreply.github.com>
2025-06-19 01:52:03 +0300

    Correct Semantic MediaWiki url
    URL is more specific now (/wiki/Semantic_MediaWiki), like the default. Nothing wrong with the earlier, but it's nice to be specific.

--- LocalSettings.php
+++ LocalSettings.php
@@ -2034,7 +2034,7 @@ $wgConf->settings += [
 				],
 				'semanticmediawiki' => [
 					'src' => 'https://static.wikitide.net/universalunionwiki/7/77/SemanticMediaWiki.svg',
-					'url' => 'https://www.semantic-mediawiki.org',
+					'url' => 'https://www.semantic-mediawiki.org/wiki/Semantic_MediaWiki',
 					'alt' => 'Powered by Semantic MediaWiki',
 				],
 			],
