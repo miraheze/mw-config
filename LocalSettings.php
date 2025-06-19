368dd595429479a1f2a9192653e52768a97400b1
Asko <49131864+asko1@users.noreply.github.com>
2025-06-19 13:45:13 +0300

    T13868: Change Semantic MediaWiki footer icon for universalunionwiki (#5993)
    Resolves [T13868](https://issue-tracker.miraheze.org/T13868).

--- LocalSettings.php
+++ LocalSettings.php
@@ -2032,6 +2032,11 @@ $wgConf->settings += [
 					'url' => 'https://www.mediawiki.org',
 					'alt' => 'Powered by MediaWiki',
 				],
+				'semanticmediawiki' => [
+					'src' => 'https://static.wikitide.net/universalunionwiki/7/77/SemanticMediaWiki.svg',
+					'url' => 'https://www.semantic-mediawiki.org/wiki/Semantic_MediaWiki',
+					'alt' => 'Powered by Semantic MediaWiki',
+				],
 			],
 			'copyright' => [
 				'copyright' => [
