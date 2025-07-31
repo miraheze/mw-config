02eea63eea2f3c9084ec72406a07cdfc9e2511f0
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-07-31 11:08:15 -0600

    Update

--- tests/Mock/MirahezeFunctions.php
+++ tests/Mock/MirahezeFunctions.php
@@ -3,4 +3,25 @@
 namespace Miraheze\Config\Tests\Mock;
 
 class MirahezeFunctions {
+
+	public string $dbname = '';
+	public string $server = '';
+	public string $sitename = '';
+	public string $version = '';
+
+	public function getSettingValue(): array {
+		return [];
+	}
+
+	public function isAllOfExtensionsActive(): true {
+		return true;
+	}
+
+	public function isAnyOfExtensionsActive(): true {
+		return true;
+	}
+
+	public function isExtensionActive(): true {
+		return true;
+	}
 }
