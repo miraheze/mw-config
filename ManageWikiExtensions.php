0ac597bb159fd6714a32673cbdc72ffc8f3f5000
CosmicAlpha <cosmic.alpha24@gmail.com>
2025-09-07 14:21:55 -0600

    Only on 1.44

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -323,7 +323,6 @@ $wgManageWikiExtensions = [
 		'requires' => [],
 		'install' => [
 			'sql' => [
-				'cs_associated_pages' => "$IP/extensions/CommentStreams/sql/mysql/cs_associated_pages.sql",
 				'cs_comments' => "$IP/extensions/CommentStreams/sql/mysql/cs_comments.sql",
 				'cs_replies' => "$IP/extensions/CommentStreams/sql/mysql/cs_replies.sql",
 				'cs_votes' => "$IP/extensions/CommentStreams/sql/mysql/cs_votes.sql",
@@ -3905,6 +3904,8 @@ if ( $wi->version >= 1.44 ) {
 		],
 		'section' => 'parserhooks',
 	];
+
+	$wgManageWikiExtensions['commentstreams']['install']['sql']['cs_associated_pages'] = "$IP/extensions/CommentStreams/sql/mysql/cs_associated_pages.sql";
 
 	// table dropped in >= 1.44
 	unset( $wgManageWikiExtensions['wikibaserepository']['install']['sql']['wbt_type'] );
