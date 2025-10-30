8b336d7fc2aabcb2685bf1a733fde90ec7f0ef10
TheAstorPastor <theastorpastor@gmail.com>
2025-10-31 01:25:20 +0530

    Refactor wgParserEnableUserLanguage settings

--- ManageWikiSettings.php
+++ ManageWikiSettings.php
@@ -1987,15 +1987,15 @@ $wgManageWikiSettings = [
 		'help' => 'Load CSS and JavaScript to enhance the details and summary tags, causing them to behave like elements with the mw-collapsible class and be styled like wikitables.',
 		'requires' => [],
 	],
-    'wgParserEnableUserLanguage' => [
-	    'name' => 'Enable USERLANGUAGE Magic Word',
-	    'from' => 'mediawiki',
-	    'type' => 'check',
-	    'overridedefault' => false,
-	    'global' => true,
-	    'help' => 'If enabled, the <nowiki>{{USERLANGUAGE}}</nowiki> magic word will return the actual interface language of the viewer.',
-        'section' => 'parserfunctions',
-	    'requires' => [],
+	'wgParserEnableUserLanguage' => [
+		'name' => 'Enable USERLANGUAGE Magic Word',
+		'from' => 'mediawiki',
+		'type' => 'check',
+		'overridedefault' => false,
+		'global' => true,
+		'help' => 'If enabled, the <nowiki>{{USERLANGUAGE}}</nowiki> magic word will return the actual interface language of the viewer.',
+		'section' => 'parserfunctions',
+		'requires' => [],
 	],
         
 	// Media/File
