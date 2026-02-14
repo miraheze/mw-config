9a92116d9919ff4e66c6bb7a8ec36ea78e0ce3f7
Aeywoo <65846594+Aeywoo@users.noreply.github.com>
2026-02-15 02:26:53 +1030

    install RefreshSpecial per T12835 (again)
    Adding Extension:RefreshSpecial per [T12835](https://issue-tracker.miraheze.org/T12835), again.

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -1671,6 +1671,13 @@ $wgManageWikiExtensions = [
 		],
 		'section' => 'specialpages',
 	],
+	'refreshspecial' => [
+		'name' => 'RefreshSpecial',
+		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:RefreshSpecial',
+		'conflicts' => false,
+		'requires' => [],
+		'section' => 'specialpages',
+	],
 	'replacetext' => [
 		'name' => 'Replace Text',
 		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:Replace_Text',
