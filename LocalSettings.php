466369456d7ec2be4af1f7bcc26f536d653cb951
nukesme <116573940+Nukesme@users.noreply.github.com>
2026-05-21 19:30:23 +0200

    should be correct

--- LocalSettings.php
+++ LocalSettings.php
@@ -1905,6 +1905,27 @@ $wgConf->settings += [
 				],
 			],
 		],
+	'arsenalrefreshedwiki' => [
+			'poweredby' => [
+				'mediawiki' => [
+					'src' => 'https://static.wikitide.net/arsenalrefreshedwiki/b/b0/PoweredByMediaWiki.svg',
+					'url' => 'https://www.mediawiki.org/',
+					'alt' => 'Powered by MediaWiki',
+				],
+				'miraheze' => [
+					'src' => 'https://static.wikitide.net/arsenalrefreshedwiki/e/eb/Miraheze-badge.svg',
+					'url' => 'https://meta.miraheze.org/wiki/Special:MyLanguage/Miraheze_Meta',
+					'alt' => 'Hosted by Miraheze',
+				],
+			],
+			'copyright' => [
+				'copyright' => [
+					'src' => 'https://static.wikitide.net/arsenalrefreshedwiki/a/a3/Ccbysa.svg',
+					'url' => 'https://creativecommons.org/licenses/by-sa/4.0/',
+					'alt' => 'Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)',
+				],
+			],
+		],
 		'itemasylumwiki' => [
 			'poweredby' => [
 				'mediawiki' => [
@@ -2527,27 +2548,6 @@ $wgConf->settings += [
 				],
 			],
 		],
-		'arsenalrefreshedwiki' => [
-			'poweredby' => [
-				'mediawiki' => [
-					'src' => 'https://static.wikitide.net/arsenalrefreshedwiki/b/b0/PoweredByMediaWiki.svg',
-					'url' => 'https://www.mediawiki.org/',
-					'alt' => 'Powered by MediaWiki',
-				],
-				'miraheze' => [
-					'src' => 'https://static.wikitide.net/arsenalrefreshedwiki/e/eb/Miraheze-badge.svg',
-					'url' => 'https://meta.miraheze.org/wiki/Special:MyLanguage/Miraheze_Meta',
-					'alt' => 'Hosted by Miraheze',
-				],
-			],
-			'copyright' => [
-				'copyright' => [
-					'src' => 'https://static.wikitide.net/arsenalrefreshedwiki/a/a3/Ccbysa.svg',
-					'url' => 'https://creativecommons.org/licenses/by-sa/4.0/',
-					'alt' => 'Creative Commons Attribution-ShareAlike 4.0 International (CC BY-SA 4.0)',
-				],
-			],
-	],
 	'wmgWikiapiaryFooterPageName' => [
 		'default' => '',
 	],
