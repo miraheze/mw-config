1cf3b5ba4166b35611c1a972f3b890be413d5cbd
The-Voidwalker <voidwalker.wikipedia@gmail.com>
2026-07-06 02:42:05 -0400

    add processing for LocalWiki.php

--- cleanup/CleanupMissingWikis.php
+++ cleanup/CleanupMissingWikis.php
@@ -98,8 +98,38 @@ class CleanupMissingWikis {
 	}
 
 	/**
+	 * Contains logic for using `array_find_key` on an array of tokens.
+	 * 
+	 * @param mixed $token PHP lanugage token to compare.
+	 * @param int $key Integer offset of $token in the array being searched.
+	 * @param array|string $searchToken An array or exact string to match against the token.
+	 * @param int $offset Integer offset to being searching from.
+	 * @return bool True if $token and $searchToken are both exactly matching strings or if
+	 * the defined components of $searchToken match the $token.
+	 */
+	private static function tokenSearch( mixed $token, int $key, array|string $searchToken, int $offset = 0 ): bool {
+		if ( $key < $offset ) {
+			return false;
+		}
+		// Match strings exactly.
+		if ( is_string( $searchToken ) ) {
+			return $token === $searchToken;
+		}
+		if ( is_array( $searchToken ) && is_array( $token ) ) {
+			// All empty arrays are a match, in order to grab the next immediate block.
+			// Note: we will have skipped all non-array tokens, but this is expected!
+			if ( empty( $searchToken ) ) {
+				return true;
+			}
+			return $token[0] === $searchToken[0] && ( !isset( $searchToken[1] ) || $token[1] === $searchToken[1] );
+		}
+		return false;
+	}
+
+	/**
 	 * Searches a list of PHP lanugage tokens for a 'block' associated with the search token.
-	 * A valid block could be `'somewiki' => [ 'someconfig' ],`
+	 * 
+	 * A valid block could be `'somewiki' => [ 'someconfig' ],`.
 	 *
 	 * @param array $tokens List of PHP language tokens to search through.
 	 * @param array|string $searchToken Array of format [ T_*CONST, $value ] to search for,
@@ -110,22 +140,7 @@ class CleanupMissingWikis {
 	private static function seekTokenBlock( array $tokens, array|string $searchToken, int $offset = 0 ): array {
 		$block = [];
 		$blockStart = array_find_key( $tokens, static function ( mixed $token, int $key ) use ( $searchToken, $offset ) {
-			if ( $key < $offset ) {
-				return false;
-			}
-			// Match strings exactly.
-			if ( is_string( $searchToken ) ) {
-				return $token === $searchToken;
-			}
-			if ( is_array( $searchToken ) && is_array( $token ) ) {
-				// All empty arrays are a match, in order to grab the next immediate block.
-				// Note: we will have skipped all non-array tokens, but this is expected!
-				if ( empty( $searchToken ) ) {
-					return true;
-				}
-				return $token[0] === $searchToken[0] && ( !isset( $searchToken[1] ) || $token[1] === $searchToken[1] );
-			}
-			return false;
+			return self::tokenSearch( $token, $key, $searchToken, $offset );
 		} );
 		$blockEnd = 0;
 
@@ -210,10 +225,90 @@ class CleanupMissingWikis {
 	}
 
 	/**
+	 * Seeks a block of tokens explicitly defined by T_CASE and T_BREAK as the start and end of the block.
+	 * 
+	 * It is important to note that the logic for this function and {@see CleanupMissingWikis::seekTokenBlock()}
+	 * are not interchangable.
+	 * 
+	 * @param array $tokens Array of PHP language tokens to search.
+	 * @param int $offset Integer offset to begin searching from.
+	 * @return array May be empty if no block is found.
+	 */
+	private static function seekCaseBlock( array $tokens, int $offset = 0 ): array {
+		$block = [];
+		$blockStart = array_find_key( $tokens, static function ( mixed $token, int $key ) use ( $offset ) {
+			return self::tokenSearch( $token, $key, [ T_CASE ], $offset );
+		} );
+		$blockEnd = 0;
+
+		// The only keys we care about are integers, anything else is unexpected,
+		// or indicates we didn't find anything.
+		if ( is_int( $blockStart ) ) {
+			// We want to grab the proceeding whitespace token so we can delete blocks cleanly.
+			if (
+				$blockStart > array_key_first( $tokens )
+				&& is_array( $tokens[$blockStart - 1] )
+				&& $tokens[$blockStart - 1][0] === T_WHITESPACE
+			) {
+				$blockStart -= 1;
+			}
+			$blockEnd = array_find_key( $tokens, static function ( mixed $token, int $key ) use ( $blockStart ) {
+				return self::tokenSearch( $token, $key, [ T_BREAK ], $blockStart + 2 ) || self::tokenSearch( $token, $key, [ T_CASE ], $blockStart + 2 );
+			} ) ?? 0;
+			if ( $blockEnd > 0 ) {
+				// If we end on another case, don't include it (or the newline). If we end on a break, include the semicolon.
+				$blockEnd += $tokens[$blockEnd][0] === T_CASE ? -2 : 1;
+				// array_slice works on pure numerical index, ignoring any special keys.
+				$blockEnd = $blockEnd - array_key_first( $tokens );
+			}
+			// array_slice works on pure numerical index, ignoring any special keys.
+			$blockStart = $blockStart - array_key_first( $tokens );
+			$block = array_slice( $tokens, $blockStart, $blockEnd - $blockStart + 1, true );
+		}
+		return $block;
+	}
+
+	/**
+	 * Perform cleanup for LocalWiki.php.
+	 */
+	private static function processLocalWiki(): void{
+		$tokens = token_get_all( file_get_contents( __DIR__ . '/../LocalWiki.php' ) );
+		$case = [];
+		$offset = 0;
+		while( !empty( $case = self::seekCaseBlock( $tokens, $offset ) ) ) {
+			// Case statements are T_WHITESPACE T_CASE T_WHITESPACE 'wiki':
+			$wiki = $case[array_key_first( $case ) + 3][1];
+			$wiki = trim( $wiki, '\'"');
+			$offset += count( $case );
+			// Only search for wikis, beta not included.
+			if ( !str_ends_with( $wiki, 'wiki' ) ) {
+				continue;
+			}
+			// Delete tokens for any block where the wiki does not exist.
+			if ( !self::wikiExists( $wiki ) ) {
+				foreach ( $case as $index => $_ ) {
+					$tokens[$index] = '';
+				}
+			}
+		}
+		// Convert tokens back into source code.
+		$content = "";
+		foreach ( $tokens as $token ) {
+			if ( is_array( $token ) ) {
+				$content .= $token[1];
+			} else {
+				$content .= $token;
+			}
+		}
+		file_put_contents( __DIR__ . '/../LocalWiki.php', $content );
+	}
+
+	/**
 	 * Expected entry point.
 	 */
 	public static function process(): void {
 		self::processLocalSettings();
+		self::processLocalWiki();
 	}
 }
 
