8e1503afe1fd4dde731be58bb92a61a6c6f7aea9
SomeRandomDeveloper <git@srd.dev>
2026-07-08 14:39:37 +0200

    T15625: Undeploy WikiPoints

--- ManageWikiExtensions.php
+++ ManageWikiExtensions.php
@@ -1904,13 +1904,6 @@ $wgManageWikiExtensions = [
 		'requires' => [],
 		'section' => 'specialpages',
 	],
-	'wikipoints' => [
-		'name' => 'WikiPoints',
-		'linkPage' => 'https://www.mediawiki.org/wiki/Special:MyLanguage/Extension:WikiPoints',
-		'conflicts' => false,
-		'requires' => [],
-		'section' => 'specialpages',
-	],
 
 	// Other
 	'advancedsearch' => [
