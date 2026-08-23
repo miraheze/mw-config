774ad5a5e32253d42fe10439bbc02cb1a7aae55a
SomeRandomDeveloper <git@srd.dev>
2026-08-23 22:10:41 +0200

    Enable Speedscope on beta
    Part of T14537

--- LocalSettings.php
+++ LocalSettings.php
@@ -7,6 +7,15 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
+if ( str_starts_with( wfHostname(), 'test' ) ) {
+	$wgSpeedscopeEndpoint = 'https://speedscope.wikitide.net';
+	$wgSpeedscopeEnvironment = 'beta';
+	$wgSpeedscopeExposeCPUInfo = false;
+	$wgSpeedscopeSamplingRates = [];
+	require_once "$IP/extensions/Speedscope/bootstrap.php";
+	wfLoadExtension( 'Speedscope' );
+}
+
 if ( PHP_SAPI !== 'cli' ) {
 	header( "Cache-control: no-cache" );
 }
