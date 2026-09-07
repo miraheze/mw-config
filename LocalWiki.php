11045f64fa40305943c2ef8804b912be3dcb71b6
Aeywoo <Aeywoo@proton.me>
2026-09-08 03:24:52 +0930

    Moving changes
    Moving changes to LocalSettings.php instead, as requested by @pskyechology

--- LocalWiki.php
+++ LocalWiki.php
@@ -122,14 +122,6 @@ switch ( $wi->dbname ) {
 		}
 
 		break;
-	case 'doorswiki':
-		$wgRobloxAPICachingExpiries = [
-			'*' => 6000,
-			'gameData' => 120,
-			'groupRoles' => 180,
-		];
-
-		break;
 	case 'dragonquestxwiki':
 		$wgPopupsConf['contentPreviews'] = [
 			'image' => true,
