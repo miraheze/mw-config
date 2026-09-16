849d494941b1a4832dc034671f64bcb66918126f
CosmicAlpha <cosmic.alpha24@gmail.com>
2026-09-16 15:04:10 -0600

    Remove

--- GlobalSettings.php
+++ GlobalSettings.php
@@ -450,16 +450,6 @@ if ( $wi->isExtensionActive( 'UploadWizard' ) ) {
 	];
 }
 
-// FeaturedFeeds configuration
-
-if ( $wi->isExtensionActive( 'FeaturedFeeds' ) ) {
-	if ( $wmgMirahezeFeaturedFeedsInUserLanguage ) {
-		$wgFeaturedFeedsDefaults['inUserLanguage'] = true;
-	} else {
-		$wgFeaturedFeedsDefaults['inUserLanguage'] = false;
-	}
-}
-
 if ( $wi->isExtensionActive( 'Score' ) ) {
 	$wgScoreFileBackend = 'miraheze-swift';
 }
